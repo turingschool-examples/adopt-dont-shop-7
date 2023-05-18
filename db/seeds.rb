5b9853be3e51552b582de71ce68ce988ac042942
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-18 15:20:46 -0600

    Seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Pet.destroy_all
+Shelter.destroy_all
+Veterinarian.destroy_all
+VeterinaryOffice.destroy_all
