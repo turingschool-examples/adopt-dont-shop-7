9013bea28bce2cf7f0a1dde1f1481cfe4a68d072
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-19 14:10:24 -0600

    Added tests for new and show specs

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe '#new' do
       application = pet.applications.create!(applicant: "Sarah", address: "123 Sesame Street, Denver, CO 80212", description: "I am cool", status: "In Progress")
 
 
-      visit "/application/new"
+      visit "/applications/new"
       
-      expect(page).to have_field("name")
+      expect(page).to have_field("applicant")
       expect(page).to have_field("address")
       expect(page).to have_field("description")
     end
@@ -18,17 +18,20 @@ RSpec.describe '#new' do
     it "allows me to fill out form and takes me back to the new application's show page" do
       shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
       pet = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-      application = pet.applications.create!(applicant: "Sarah", address: "123 Sesame Street, Denver, CO 80212", description: "I am cool", status: "In Progress")
 
-      visit "/application/new"
+      visit "/applications/new"
 
 
-      fill_in("name", with: "Sarah")
+      fill_in("applicant", with: "Sarah")
       fill_in("address", with: "123 Sesame Street, Denver, CO 80212")
       fill_in("description", with: "I am cool")
 
       click_button "Submit"
-      expect(current_path).to eq("/application/#{application.id}")
+      expect(page).to have_content("Show Application")
+      expect(page).to have_content("Applicant Name: Sarah")
+      expect(page).to have_content("Address: 123 Sesame Street, Denver, CO 80212")
+      expect(page).to have_content("Why you? I am cool")
+      expect(page).to have_content("Application Status: In Progress") 
     end
   end
 end
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "application/show" do 
+RSpec.describe "applications/show" do 
   # 1. Application Show Page
   describe "when I visit an applications show page" do 
     it "shows name, address, description, pet names, and application status " do 
@@ -8,7 +8,7 @@ RSpec.describe "application/show" do
       pet = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
       application = pet.applications.create!(applicant: "Sarah", address: "123 Sesame Street, Denver, CO 80212", description: "I am cool", status: "In Progress")
 
-      visit "/application/#{application.id}"
+      visit "/applications/#{application.id}"
 
       expect(page).to have_content("Sarah")
       expect(page).to have_content("123 Sesame Street, Denver, CO 80212")
