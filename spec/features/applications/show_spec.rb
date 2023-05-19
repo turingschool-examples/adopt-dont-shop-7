46a6ab70e5111bee344cbebf67fcc650afb7a14f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-19 15:25:48 -0600

    update applications show page and tests to include application status

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,14 +8,17 @@ RSpec.describe '/applications/:id', type: :feature do
       city: 'Fargo', 
       state: 'MI', 
       zip: '45896', 
-      description: 'Loves alligators.')
+      description: 'Loves alligators.', 
+      status: 'Pending'
+    )
     @tom = Application.create!(
       name: 'Thomas', 
       street_address: '5234 S Jefferson', 
       city: 'Julian', 
       state: 'AL', 
       zip: '43896', 
-      description: 'Has owned a pet.'
+      description: 'Has owned a pet.', 
+      status: 'In Progress'
     )
   end
 
@@ -30,13 +33,15 @@ RSpec.describe '/applications/:id', type: :feature do
       expect(page).to have_content("State: #{@susie.state}")
       expect(page).to have_content("Zip Code: #{@susie.zip}")
       expect(page).to have_content("Description: #{@susie.description}")
+      expect(page).to have_content("Status: #{@susie.status}")
 
       expect(page).to_not have_content(@tom.name)
       expect(page).to_not have_content("Street Address: #{@tom.street_address}")
       expect(page).to_not have_content("City: #{@tom.city}")
       expect(page).to_not have_content("State: #{@tom.state}")
       expect(page).to_not have_content("Zip Code: #{@tom.zip}")
-      expect(page).to_not have_content("Description:#{@tom.description}")
+      expect(page).to_not have_content("Description: #{@tom.description}")
+      expect(page).to_not have_content("Status :#{@tom.status}")
     end
 
     it 'shows different applicant information' do 
@@ -47,6 +52,7 @@ RSpec.describe '/applications/:id', type: :feature do
       expect(page).to have_content("State: #{@tom.state}")
       expect(page).to have_content("Zip Code: #{@tom.zip}")
       expect(page).to have_content("Description: #{@tom.description}")
+      expect(page).to have_content("Status: #{@tom.status}")
 
       expect(page).to_not have_content(@susie.name)
       expect(page).to_not have_content("Street Address: #{@susie.street_address}")
@@ -54,6 +60,7 @@ RSpec.describe '/applications/:id', type: :feature do
       expect(page).to_not have_content("State: #{@susie.state}")
       expect(page).to_not have_content("Zip Code: #{@susie.zip}")
       expect(page).to_not have_content("Description:#{@susie.description}")
+      expect(page).to_not have_content("Status:#{@susie.status}")
     end
   end
 end
