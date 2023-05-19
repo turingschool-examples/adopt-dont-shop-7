d0a1bef167b0796df39acddacf535b1a97cb9807
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-18 19:39:53 -0600

    Test: applicationpet model

---
+++ spec/models/application_pet_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationPet, type: :model do 
+  describe 'relationship' do 
+    it { should belong_to :application }
+    it { should belong_to :pet }
+  end
+end
