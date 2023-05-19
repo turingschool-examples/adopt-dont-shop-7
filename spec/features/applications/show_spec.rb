3b9a726f30399c36f03b3d2441cd37b4969d20e5
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-18 20:48:00 -0600

    test: Add setup for application show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe Application, type: :feature do
   
   let!(:app_1) { pet_1.applications.create!(name: "Max Power", street_address: "456 Main St", city: "Broomfield", state: "CO", zip_code: 80211, description: "Love animals", status: "in progress") }
   
-  let!(:pet_2) { app_1.pets.create!(adoptable: true, age: 4, breed: "mutt", name: "Chaco")}
+  let!(:pet_2) { shelter_1.pets.create!(adoptable: true, age: 4, breed: "mutt", name: "Chaco")}
   
-  let!(:application_pets_1) { ApplicationPets.create!(application: app_1, pet: pet_2) }
+  let!(:application_pets_1) { ApplicationPet.create!(application: app_1, pet: pet_2) }
+  
   
   # User Story 1
   # As a visitor
@@ -22,20 +23,24 @@ RSpec.describe Application, type: :feature do
   # - Description of why the applicant says they'd be a good home for this pet(s)
   # - names of all pets that this application is for (all names of pets should be links to their show page)
   # - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected" 
-
+  # create shelters
+  # create pets that belong to shelters
+  # create application
+  # associates application with pets
+  
   describe 'as a visitor, when I open the applications show page' do
-    it should 'display a form' do
-      # create shelters
-      # create pets that belong to shelters
-      # create application
-      # associates application with pets
+    it 'display a form' do
+      visit "/applications/#{app_1.id}"
+      
+
+      within("#application-#{app_1.id}") do
+        expect(page).to have_content()
+      end
+
 
 
       
-      visit = 
+      
     end
   end
-
-
-
 end
