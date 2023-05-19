301ed3add8d455c9065ee00943f7bbcfdd37968d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-19 15:15:05 -0600

    Feat: first draft of application show page including routes, controller action view page and tests

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe '/applications/:id', type: :feature do 
+  before(:each) do 
+    @susie = Application.create!(
+      name: 'Susie', 
+      street_address: '5234 S Jamaica', 
+      city: 'Fargo', 
+      state: 'MI', 
+      zip: '45896', 
+      description: 'Loves alligators.')
+    @tom = Application.create!(
+      name: 'Thomas', 
+      street_address: '5234 S Jefferson', 
+      city: 'Julian', 
+      state: 'AL', 
+      zip: '43896', 
+      description: 'Has owned a pet.'
+    )
+  end
+
+  #User story 1
+
+  describe 'Application Show Page' do
+    it 'shows applicant information' do 
+      visit "/applications/#{@susie.id}"
+      expect(page).to have_content(@susie.name)
+      expect(page).to have_content("Street Address: #{@susie.street_address}")
+      expect(page).to have_content("City: #{@susie.city}")
+      expect(page).to have_content("State: #{@susie.state}")
+      expect(page).to have_content("Zip Code: #{@susie.zip}")
+      expect(page).to have_content("Description: #{@susie.description}")
+
+      expect(page).to_not have_content(@tom.name)
+      expect(page).to_not have_content("Street Address: #{@tom.street_address}")
+      expect(page).to_not have_content("City: #{@tom.city}")
+      expect(page).to_not have_content("State: #{@tom.state}")
+      expect(page).to_not have_content("Zip Code: #{@tom.zip}")
+      expect(page).to_not have_content("Description:#{@tom.description}")
+    end
+
+    it 'shows different applicant information' do 
+      visit "/applications/#{@tom.id}"
+      expect(page).to have_content(@tom.name)
+      expect(page).to have_content("Street Address: #{@tom.street_address}")
+      expect(page).to have_content("City: #{@tom.city}")
+      expect(page).to have_content("State: #{@tom.state}")
+      expect(page).to have_content("Zip Code: #{@tom.zip}")
+      expect(page).to have_content("Description: #{@tom.description}")
+
+      expect(page).to_not have_content(@susie.name)
+      expect(page).to_not have_content("Street Address: #{@susie.street_address}")
+      expect(page).to_not have_content("City: #{@susie.city}")
+      expect(page).to_not have_content("State: #{@susie.state}")
+      expect(page).to_not have_content("Zip Code: #{@susie.zip}")
+      expect(page).to_not have_content("Description:#{@susie.description}")
+    end
+  end
+end
