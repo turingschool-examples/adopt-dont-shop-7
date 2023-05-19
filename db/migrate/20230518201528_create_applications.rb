cb50bca0aa4bdb8fb566cd3b8f3cf843a9cc34fa
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-19 02:29:35 -0600

    Application - Add default value for attribute "application_status".

--- db/migrate/20230518201528_create_applications.rb
+++ db/migrate/20230518201528_create_applications.rb
@@ -2,9 +2,12 @@ class CreateApplications < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
       t.string :name
-      t.string :address
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
       t.string :description
-      t.string :application_status
+      t.string :application_status, default: "In Progress"
 
       t.timestamps
     end
