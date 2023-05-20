9ad4d3819509c96778a465bf571b50b52a22d959
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-19 18:13:21 -0600

    add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,20 @@ Pet.destroy_all
 Shelter.destroy_all
 Veterinarian.destroy_all
 VeterinaryOffice.destroy_all
+
+
+
+shelter_1 = Shelter.create!(foster_program: true, name:"Soul Dog Rescue", city:"Ft Lupton", rank:1)
+pet_1 = shelter_1.pets.create!(adoptable: true, age: 2, breed: "shepherd", name: "Frank")
+pet_2 = shelter_1.pets.create!(adoptable: false, age: 7, breed: "lab", name: "Rex")
+pet_3 = shelter_1.pets.create!(adoptable: true, age: 1, breed: "bulldog", name: "Daisy")
+
+# :application_1) = Application.create!(name: "John Smith", street_address: "123 Elm", city: "Denver", state: "CO", zip_code: 80205, description: "Responsible pet owner, fenced yard", status: "pending"  )}
+
+app_1 = Application.create!(name: "Max Power", street_address: "456 Main St", city: "Broomfield", state: "CO", zip_code: 80211, description: "Love animals", status: "in progress") 
+app_2 = Application.create!(name: "Jane Doe", street_address: "444 8th St", city: "Wheatridge", state: "CO", zip_code: 80231, description: "Outdoorsy, responsible", status: "accepted") 
+app_3 = Application.create!(name: "Clark Kent", street_address: "93428 Washington Ave", city: "Arvada", state: "CO", zip_code: 80411, description: "Family loves animals", status: "pending") 
+
+
+
+application_pets_1 = ApplicationPet.create!(application_id: app_1.id, pet_id: pet_2.id) 
