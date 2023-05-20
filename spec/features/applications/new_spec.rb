5c37d969cf01e0355450bac26529e86ecab54216
Steven Stackhouse <stackmm@gmail.com>
2023-05-20 17:07:43 -0600

    add test for successfully completed new application

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe "/applications/new", type: :feature do
   # User Story 2 (part 2)
   describe "New Application Creation" do
     it "can verify form elements" do
-      expect(page).to have_current_path("/applications/new")
+      visit "/applications/new"
+
       expect(page).to have_field("Name")
       expect(page).to have_field("Street Address")
       expect(page).to have_field("City")
       expect(page).to have_field("State")
-      expect(page).to have_field("Zip Code")
+      expect(page).to have_field("Zip")
       expect(page).to have_field("Description")
       expect(page).to have_button("Submit")
     end
@@ -22,11 +23,11 @@ RSpec.describe "/applications/new", type: :feature do
       fill_in("Street Address", with: "123 Peoples Square")
       fill_in("City", with: "Pyongyang")
       fill_in("State", with: "Florida")
-      fill_in("Zip Code", with: "12345")
+      fill_in("Zip", with: "12345")
       fill_in("Description", with: "very good applicant")
       click_button("Submit")
-
-      expect(page).to have_current_path("/applications/#{Application.last.id}")
+      save_and_open_page
+      expect(current_path).to eq("/applications/#{Application.last.id}")
     end
   end
 end
