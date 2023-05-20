015c791c2e93f33acd132e9dbfe6017d9b17ffec
Steven Stackhouse <stackmm@gmail.com>
2023-05-20 17:06:01 -0600

    add create method to handle incomplete applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,20 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    @application = Application.new(application_params)
+
+    if @application.save
+      redirect_to "/applications/#{@application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: You must complete all fields"
+    end
+  end
+
+private
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip, :description, :status)
+  end
 end
