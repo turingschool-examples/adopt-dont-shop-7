aee0640b499bd78542a97de1854ae27a0c90eff6
Javen Wilson <javenb022@gmail.com>
2023-05-20 11:51:57 -0400

    Migration to set status default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_014911) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_154315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_014911) do
     t.integer "zip_code"
     t.string "description"
     t.string "pets"
-    t.string "status"
+    t.string "status", default: "In Progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
