2fd8fccc7f573bf8c164b45fb8e6008517466621
Javen Wilson <javenb022@gmail.com>
2023-05-20 11:30:18 -0400

    Adds create route and action

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,18 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    @application = Application.create!(app_params)
+  end
+
+  private
+
+  def app_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description, :pets, :status, :shelter)
+  end
 end
