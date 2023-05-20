6be089150fbcbdfe65243cfb11c74635b303b16a
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-20 11:15:46 -0600

    Test: #find_applications pet_application model

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,39 @@ RSpec.describe PetApplication, type: :model do
     it { should belong_to(:pet) }
     it { should belong_to(:application) }
   end
+
+  describe "class methods" do
+    before(:each) do
+      @shelter1 = Shelter.create!(foster_program: true, name: "Bishop Animal Rescue", city: "Bishop", rank: 5)
+      @pet1 = @shelter1.pets.create!(adoptable: true, age: 3, breed: "Samoyed", name: "Fluffy")
+      @pet2 = @shelter1.pets.create!(adoptable: true, age: 6, breed: "Husky", name: "Hubert")
+      # ^ not applied by anyone
+      @pet3 = @shelter1.pets.create!(adoptable: true, age: 8, breed: "Shiba Inu", name: "Shishi")
+
+      @shelter2 = Shelter.create!(foster_program: true, name: "Chicago Animal Rescue", city: "Chicago", rank: 4)
+      @pet4 = @shelter2.pets.create!(adoptable: true, age: 1, breed: "Lab", name: "Lally")
+      @pet5 = @shelter2.pets.create!(adoptable: true, age: 10, breed: "Chihuahua", name: "Chewy")
+
+      @shelter3 = Shelter.create!(foster_program: true, name: "Los Angeles Animal Rescue", city: "Los Angeles", rank: 3)
+      @pet6 = @shelter3.pets.create!(adoptable: true, age: 5, breed: "Pitbull", name: "Bully")
+
+      @app1 = Application.create!(name: "Garrett", street_address: "123 Upland", city: "Bishop", state: "CA", zip_code: "12345", description: "I'm the best -DJ Khaled")
+      @petapp1 = PetApplication.create!(application_id: @app1.id, pet_id: @pet1.id, status: "In Progress")
+      @petapp2 = PetApplication.create!(application_id: @app1.id, pet_id: @pet4.id, status: "Pending")
+
+      @app2 = Application.create!(name: "Andy", street_address: "456 Downtown", city: "Anywhere", state: "HI", zip_code: "23456", description: "Anotha One -DJ Khaled")
+      @petapp3 = PetApplication.create!(application_id: @app2.id, pet_id: @pet1.id, status: "Pending")
+      @petapp4 = PetApplication.create!(application_id: @app2.id, pet_id: @pet3.id, status: "Pending")
+      @petapp5 = PetApplication.create!(application_id: @app2.id, pet_id: @pet6.id, status: "Accepted")
+
+      @app3 = Application.create!(name: "Jeff", street_address: "567 Sideways", city: "Somewhere", state: "DE", zip_code: "34567", description: "We the best -DJ Khaled")
+      @petapp6 = PetApplication.create!(application_id: @app3.id, pet_id: @pet5.id, status: "Rejected")
+    end
+
+    context "#find_applications" do
+      it "can find a specific application(s)" do
+        expect(PetApplication.find_applications(@app1.id)).to eq([@petapp1, @petapp2])
+      end
+    end
+  end
 end
