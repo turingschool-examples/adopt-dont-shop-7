ff9683c279fc8501dc5ead9328ae5ee692f2c420
Steven Stackhouse <stackmm@gmail.com>
2023-05-20 17:03:11 -0600

    update schema to include a default value for applications status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_211744) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_225833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_211744) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "In Progress"
   end
 
   create_table "pets", force: :cascade do |t|
