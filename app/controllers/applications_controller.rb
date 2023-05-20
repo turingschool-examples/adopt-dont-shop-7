57274f47f0f8d10e44142d01c029ce252850ac17
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-05-20 15:15:48 -0600

    complete user story 4

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pets = @application.pets
+    @application.status = 'In Progress' if params[:commit] == 'Submit'
+    @pets_search = Pet.search(params[:pet_name]) if params[:pet_name].present?
   end
 
   def new
@@ -8,17 +11,15 @@ class ApplicationsController < ApplicationController
 
   def create
     application = Application.new(application_params)
-      if application.save
-        application.update(status: 'In Progress')
-        redirect_to "/applications/#{application.id}"
-
-      else
-        redirect_to "/applications/new"
-        flash[:alert] = "Error: #{error_message(application.errors)}"
-      end
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
   end
-  
-private
+
+  private
 
   def application_params
     params.permit(:name, :street_address, :city, :state, :zip_code, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,4 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   post "/applications/new", to: "applications#create"
   get "/applications/:id", to: "applications#show"
-
 end
