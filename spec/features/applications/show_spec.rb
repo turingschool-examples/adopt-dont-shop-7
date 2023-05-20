af4a60588436c3fd00be7b636f19df29c0d41c26
Nick Sacco <nicksacco10@gmail.com>
2023-05-20 12:33:39 -0700

    test: adding a pet to an application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -56,9 +56,39 @@ RSpec.describe "Application Show Page", type: :feature do
           click_button("Search")
           
           expect(current_path).to eq("/applications/#{@application_1.id}")
-
+          
           expect(page).to have_content("#{@pet_5.name}")
         end
+        
+        visit "/applications/#{@application_2.id}"
+        within "#pet_search" do
+          expect(page).to have_content("Add a Pet to this Application")
+          expect(page).to have_field("Search Pet")
+          
+          fill_in("Search Pet", with: "Marshall")
+          click_button("Search")
+          
+          expect(current_path).to eq("/applications/#{@application_2.id}")
+          
+          expect(page).to have_content("#{@pet_4.name}")
+        end
+      end
+    end
+    
+    describe "Adding pet to application after search" do
+      it "can add a pet to an application" do
+        visit "/applications/#{@application_1.id}"
+        fill_in("Search Pet", with: "Pepper")
+        click_button("Search")
+        
+        expect(page).to have_content(@pet_5.name)
+        expect(page).to have_button("Adopt This Pet")
+        
+        click_button("Adopt This Pet")
+        
+        expect(page).to have_content(@pet_5.name)
+        expect(current_path).to eq("/applications/#{@application_1.id}")
+        expect(page).to_not have_button("Adopt This Pet")
       end
     end
   end
