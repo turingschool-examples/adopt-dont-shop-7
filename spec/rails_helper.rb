8d5f761476964f0adb2a4e052cfe3ffccecab785
Javen Wilson <javenb022@gmail.com>
2023-05-19 22:57:28 -0400

    Add test data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,49 @@
+def test_data
+  @shelter_1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+  @shelter_2 = Shelter.create!(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+  @shelter_3 = Shelter.create!(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+
+  @pet_1 = @shelter_1.pets.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald")
+  @pet_2 = @shelter_1.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster")
+
+  @pet_3 = @shelter_2.pets.create!(adoptable: true, age: 2, breed: "saint bernard", name: "Beethoven")
+  @pet_4 = @shelter_2.pets.create!(adoptable: false, age: 8, breed: "border collie", name: "Marshall")
+  @pet_5 = @shelter_2.pets.create!(adoptable: true, age: 4, breed: "tabby", name: "Pepper")
+
+  @application_1 = Application.create!(name: "Bob", street_address: "1234 Southeast St",
+    city: "San Francisco", state: "CA", zip_code: 12345,
+    description: "Wants a dog", status: "Pending")
+  @application_2 = Application.create!(name: "Sally", street_address: "4321 Bridge Way",
+    city: "San Francisco", state: "CA", zip_code: 54321,
+    description: "Would like a siamese cat", status: "In Progress")
+
+  @application_3 = Application.create!(name: "Fred", street_address: "376 Monroe St",
+    city: "Los Angeles", state: "CA", zip_code: 67890,
+    description: "My wife really wants this cat", status: "In Progress")
+  @application_4 = Application.create!(name: "Toki", street_address: "8395 Other St",
+    city: "Los Somewhere", state: "ZX", zip_code: 37163,
+    description: "We want this cat more than Fred", status: "In Progress")
+  @application_5 = Application.create!(name: "Jimbo", street_address: "45865 Turnaround St",
+    city: "NYC", state: "NY", zip_code: 84930,
+    description: "I want a big ol dog", status: "In Progress")
+
+  PetApplication.create!(pet: @pet_1, application: @application_2)
+  PetApplication.create!(pet: @pet_2, application: @application_1)
+
+  PetApplication.create!(pet: @pet_3, application: @application_5)
+  PetApplication.create!(pet: @pet_5, application: @application_3)
+  PetApplication.create!(pet: @pet_5, application: @application_4)
+end
+
 require "simplecov"
 SimpleCov.start
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+# This file is copied to spec/ when you run "rails generate rspec:install"
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -22,7 +59,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -32,10 +69,10 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # Remove this line if you"re not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # If you"re not using ActiveRecord, or you"d prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
