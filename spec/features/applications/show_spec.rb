2ae34b904763c600d7319b7461bf2a5d6b6bdbda
Nick Sacco <nicksacco10@gmail.com>
2023-05-20 10:08:38 -0700

    test: pet search on app show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Application Show Page", type: :feature do
     describe "I can see the following:" do
       it "can show applicant name and other attributes" do
         visit "/applications/#{@application_1.id}"
-
+        
         expect(page).to have_content("Applicant Name: #{@application_1.name}")
         expect(page).to have_content("Street Address: #{@application_1.street_address}")
         expect(page).to have_content("City: #{@application_1.city}")
@@ -17,9 +17,9 @@ RSpec.describe "Application Show Page", type: :feature do
         expect(page).to have_content("Zip Code: #{@application_1.zip_code}")
         expect(page).to have_content("Description: #{@application_1.description}")
         expect(page).to have_content("Status: #{@application_1.status}")
-
+        
         visit "/applications/#{@application_2.id}"
-
+        
         expect(page).to have_content("Applicant Name: #{@application_2.name}")
         expect(page).to have_content("Street Address: #{@application_2.street_address}")
         expect(page).to have_content("City: #{@application_2.city}")
@@ -28,19 +28,36 @@ RSpec.describe "Application Show Page", type: :feature do
         expect(page).to have_content("Description: #{@application_2.description}")
         expect(page).to have_content("Status: #{@application_2.status}")
       end
-
+      
       it "each pet name is a link to their show page" do
         visit "/applications/#{@application_1.id}"
         click_link("#{@pet_2.name}")
         expect(current_path).to eq("/pets/#{@pet_2.id}")
-
+        
         visit "/applications/#{@application_2.id}"
         click_link("#{@pet_1.name}")
         expect(current_path).to eq("/pets/#{@pet_1.id}")
-
+        
         visit "/applications/#{@application_3.id}"
         click_link("#{@pet_5.name}")
         expect(current_path).to eq("/pets/#{@pet_5.id}")
+      end
+    end
+    
+    describe "Searching for Pets for an Application" do
+      it "has a search function on page to 'Add a Pet to this Application'" do
+        visit "/applications/#{@application_1.id}"
+        
+        within "#pet_search" do
+          expect(page).to have_content("Add a Pet to this Application")
+          expect(page).to have_feild("Pet Name")
+
+          fill_in("Pet Name", with: "Pepper")
+          click_button("submit")
+
+          expect(current_path).to eq("/applications/#{@application_1.id}")
+          expect(page).to have_content("#{@pet_5.name}")
+        end
       end
     end
   end
