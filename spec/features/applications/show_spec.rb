5a6124bc9cc9985199460a80aed5ed6ac63ee9a8
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-21 12:16:26 -0600

    Application - add submit testing.

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe "the application show page" do
     end
 
     # User Story 5
-    it "displays pets applied to be adopted" do 
+    it "adds pets to the application" do 
         #application
         application_1 = Application.create!(name: "John Doe", street_address: "123 Sesame St", city: "Denver", state: "CO", zip_code: "80202", description: "I have big hands.")
   
@@ -72,7 +72,64 @@ RSpec.describe "the application show page" do
         click_button "Adopt this Pet"
 
         expect(current_path).to eq("/applications/#{application_1.id}")
-        expect(page).to have_content("Pets applied for: #{buddy.name}")
+        expect(page).to have_content("#{buddy.name}")
+        
+    end
+
+    # User Story 6
+    it "submits the application" do 
+      #application
+      application_1 = Application.create!(name: "John Doe", street_address: "123 Sesame St", city: "Denver", state: "CO", zip_code: "80202", description: "I have big hands.")
+
+      #shelter
+      shelter = Shelter.create!(foster_program: true, name: "Public Shelter", city: "Denver", rank: 1)
+  
+      #pet
+      buddy = shelter.pets.create!(adoptable: true, age: 3, breed: "Cockapoo", name: "Buddy Howly")
+  
+      visit "/applications/#{application_1.id}"
+      fill_in :search, with: "Buddy Howly"
+      click_button "Search"
+      expect(page).to have_content("Buddy Howly")
+
+      click_button "Adopt this Pet"
+
+      expect(current_path).to eq("/applications/#{application_1.id}")
+      expect(page).to have_content("#{buddy.name}")
+      
+      expect(page).to have_content("Submit my Application")
+      fill_in :pet_answer, with: "I really like this dog."
+
+      click_button "Submit"
+      expect(current_path).to eq("/applications/#{application_1.id}")
+      #  NOTE FOR MJ: try in pry the below and remove before final submission!!!
+
+      # expect(page).to have_content("Application Status: #{application_1.application_status}")
+      expect(page).to have_content("Application Status: Pending")
+
+      expect(page).to have_content("#{buddy.name}")
+      expect(page).to_not have_content("Add a Pet to this Application")
+      expect(page).to_not have_content("Why would you be a good owner for this pet(s)?")
+    end
+
+    # User Story 7
+    it "does not submit the application without a selected pet" do 
+      #application
+      application_1 = Application.create!(name: "John Doe", street_address: "123 Sesame St", city: "Denver", state: "CO", zip_code: "80202", description: "I have big hands.")
+
+      #shelter
+      shelter = Shelter.create!(foster_program: true, name: "Public Shelter", city: "Denver", rank: 1)
+  
+      #pet
+      buddy = shelter.pets.create!(adoptable: true, age: 3, breed: "Cockapoo", name: "Buddy Howly")
+  
+      visit "/applications/#{application_1.id}"
+      fill_in :search, with: "Buddy Howly"
+      click_button "Search"
+      expect(page).to have_content("Buddy Howly")
+
+      expect(page).to_not have_content("Submit my Application")
+      expect(page).to_not have_content("Why would you be a good owner for this pet(s)?")
     end
 
 end
