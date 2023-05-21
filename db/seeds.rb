57218ff4c25044d04f14681640718eeb5312a5aa
Jeff Nelson <jpnelson85@gmail.com>
2023-05-21 16:30:21 -0500

    delete seeds to fix render

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,3 @@ VeterinaryOffice.destroy_all
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    
-pet_1 = shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-
-applicant_1 = Applicant.create!(name: "Jimmy", street_address: "1234 road test", city: "Boca Raton", state: "FL", zip_code: "33498", qualification: "I love pets", application_status: "In Progress")
-
-app_1 = ApplicantPet.create!(applicant: applicant_1, pet: pet_1)
-
-
