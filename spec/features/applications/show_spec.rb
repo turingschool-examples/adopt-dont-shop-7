d27c137a24f755684cdefd465e5bb113569d529d
Jesse Thomas <jgthomas3@gmail.com>
2023-05-20 18:10:45 -0600

    Feat: begin story five seach button continued

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -31,31 +31,44 @@ RSpec.describe "Application show page" do
 
         expect(page).to have_content("Status: #{@joey.status}")
       end
-        #user story 4
-        # As a visitor
-        # When I visit an application's show page
-        # And that application has not been submitted,
-        # Then I see a section on the page to "Add a Pet to this Application"
-        # In that section I see an input where I can search for Pets by name
-        # When I fill in this field with a Pet's name
-        # And I click submit,
-        # Then I am taken back to the application show page
-        # And under the search bar I see any Pet whose name matches my search
 
-        it 'can search for a specific pet' do 
-          shelter = Shelter.create!(foster_program: true, name: "Rickys used pets", city: "Sunnyvale", rank: 1)
-          zappa = shelter.pets.create!(adoptable: true, age: 4, breed: "poodle", name: "Francesco Zappa")
-          # pet_app = PetsApplication.create!(pet_id: zappa.id, pet_status: )
+      #user story 4
+      # As a visitor
+      # When I visit an application's show page
+      # And that application has not been submitted,
+      # Then I see a section on the page to "Add a Pet to this Application"
+      # In that section I see an input where I can search for Pets by name
+      # When I fill in this field with a Pet's name
+      # And I click submit,
+      # Then I am taken back to the application show page
+      # And under the search bar I see any Pet whose name matches my search
 
-          visit "/applications/#{@joey.id}"
-          expect(page).to have_content("Add a Pet to this Application")
+      it 'can search for a specific pet and get redirected to show page' do
+        shelter = Shelter.create!(foster_program: true, name: "Rickys used pets", city: "Sunnyvale", rank: 1)
+        zappa = shelter.pets.create!(adoptable: true, age: 4, breed: "poodle", name: "Francesco Zappa")
+        # pet_app = PetsApplication.create!(pet_id: zappa.id, pet_status: )
 
-          fill_in :search, with: "Francesco Zappa"
-          click_button "Search"
-          expect(page).to have_content("Francesco Zappa")
-          click_button "Adopt This Pet"
-          expect(current_path).to eq("/applications/#{@joey.id}")
-        end
+        visit "/applications/#{@joey.id}"
+        expect(page).to have_content("Add a Pet to this Application")
+
+        fill_in :search, with: "Francesco Zappa"
+        click_button "Search"
+        expect(page).to have_content("Francesco Zappa")
+        click_button "Adopt This Pet"
+        expect(current_path).to eq("/applications/#{@joey.id}")
+      end
+
+      it "after we search for a pet, there's a button to adopt the pet" do
+        shelter = Shelter.create!(foster_program: true, name: "Rickys used pets", city: "Sunnyvale", rank: 1)
+        zappa = shelter.pets.create!(adoptable: true, age: 4, breed: "poodle", name: "Francesco Zappa")
+        # pet_app = PetsApplication.create!(pet_id: zappa.id, pet_status: )
+
+        visit "/applications/#{@joey.id}"
+        fill_in :search, with: "Francesco Zappa"
+        click_button "Search"
+        click_button "Adopt This Pet"
+        expect(page).to have_content("Francesco Zappa")
+      end
     end
   end
 end
