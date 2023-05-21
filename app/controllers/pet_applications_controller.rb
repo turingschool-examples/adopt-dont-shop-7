a5484d7c004f74a44ca8df8d65a2e14ee4c0f2c8
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-05-21 16:28:30 -0600

    Feat: App pet to application

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,5 +1,12 @@
-class PetApplications < ApplicationController
-  # def create
-  #   require 'pry'; binding.pry
-  # end
+class PetApplicationsController < ApplicationController
+  def new
+
+  end
+  
+  def create
+    application = Application.find(params[:application_id])
+    pet = Pet.find(params[:pet_id])
+    PetApplication.create!(application_id: application.id, pet_id: pet.id)
+    redirect_to "/applications/#{application.id}"
+  end
 end
