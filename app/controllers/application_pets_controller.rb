af5e1369d2fac2916485aa480ad16b51be1ffca3
Steven Stackhouse <stackmm@gmail.com>
2023-05-21 22:28:02 -0600

    add required method to application_pets_params

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationPetsController < ApplicationController
   def create
-    ApplicationPet.create(application_pets_params)
+    ApplicationPet.create!(application_pets_params)
     redirect_to "/applications/#{application_pets_params[:application_id]}"
   end
 
   private
   def application_pets_params
-    params.permit(:application_id, :pet_id)
+    params.required(:application_pet).permit(:application_id, :pet_id)
   end
 end
