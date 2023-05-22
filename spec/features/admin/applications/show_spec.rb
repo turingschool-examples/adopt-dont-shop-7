4c702c5d4778a73c1b01d5aa1590240d58cfddc0
Nick Sacco <nicksacco10@gmail.com>
2023-05-22 10:46:10 -0700

    test: us 12 beginning tests. No actions yet in tests

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -1,1 +1,21 @@
 require "rails_helper"
+
+RSpec.describe "Admin Application Show Page", type: :feature do
+  before(:each) do
+    admin_test_data
+  end
+
+  describe "Approving a Pet for an Application" do
+    describe "I see every pet that the app is for and buttons to approve each pet" do
+      it "can approve a pet for a specific application" do
+        visit("admin/applications/#{@application_1.id}")
+        expect(page).to have_content("Pending Pets:")
+        expect(page).to have_content("#{@pet_2.name}")
+        expect(page).to have_content("#{@pet_4.name}")
+
+        expect(page).to have_button("Approve #{@pet_2.name}")
+        expect(page).to have_button("Approve #{@pet_4.name}")
+      end
+    end
+  end
+end
--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -12,37 +12,7 @@ RSpec.describe "admin index page" do
     end
 
     it "shows applications with pending applications in each shelter" do
-      @shelter_1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: true, rank: 9)
-      @shelter_2 = Shelter.create!(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: true, rank: 5)
-      @shelter_3 = Shelter.create!(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: false, rank: 10)
-
-      @pet_1 = @shelter_1.pets.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald")
-      @pet_2 = @shelter_1.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster")
-
-      @pet_3 = @shelter_2.pets.create!(adoptable: true, age: 2, breed: "saint bernard", name: "Beethoven")
-
-      @pet_5 = @shelter_3.pets.create!(adoptable: true, age: 4, breed: "tabby", name: "Pepper")
-
-      @application_1 = Application.create!(name: "Bob", street_address: "1234 Southeast St",
-      city: "San Francisco", state: "CA", zip_code: 12345,
-      description: "Wants a dog", status: "Pending")
-      @application_2 = Application.create!(name: "Sally", street_address: "4321 Bridge Way",
-      city: "San Francisco", state: "CA", zip_code: 54321,
-      description: "Would like a siamese cat", status: "Pending")
-
-      @application_3 = Application.create!(name: "Fred", street_address: "376 Monroe St",
-      city: "Los Angeles", state: "CA", zip_code: 67890,
-      description: "My wife really wants this cat", status: "In Progress")
-      @application_5 = Application.create!(name: "Jimbo", street_address: "45865 Turnaround St",
-      city: "NYC", state: "NY", zip_code: 84930,
-      description: "I want a big ol dog", status: "Pending")
-
-      PetApplication.create!(pet: @pet_1, application: @application_2)
-      PetApplication.create!(pet: @pet_2, application: @application_1)
-
-      PetApplication.create!(pet: @pet_3, application: @application_5)
-
-      PetApplication.create!(pet: @pet_5, application: @application_3)
+      admin_test_data
       visit "/admin/shelters"
 
       within "#pending_apps" do
