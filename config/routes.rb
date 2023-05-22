00de1df333e4b08339ac51699f2c659beafedb25
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-22 12:46:56 -0600

    patch /applications/:id to update application

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   post "/applications", to: "applications#create"
   get "/applications/:id", to: "applications#show"
-
+  patch "/applications/:id", to: "applications#update"
+  
   post "/application_pets", to: "application_pets#create"
 
   get "/shelters", to: "shelters#index"
