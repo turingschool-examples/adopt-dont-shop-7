cd9ea298424ec1e67a45d4a0c0bbf7df1549ec08
Nick Sacco <nicksacco10@gmail.com>
2023-05-22 13:57:46 -0700

    feat: updating pet application wip

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,5 +1,12 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pa_status = PetApplication.find(params[:id])
+  end
+
+  def update
+    # @application = Application.find(params[:id])
+    # @pa_status = PetApplication.find(params[:id])
+    ## pet application params..
   end
 end
