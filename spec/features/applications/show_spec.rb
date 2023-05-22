b584c967d86c469ce943e4e6ed243ef693ec925f
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-21 23:32:00 -0600

    Refactor: Make prompts match user story better

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe "the application show" do
     application = Application.create!(name: "Fredrich Longbottom", address: "1234 1st St", city: "Denver", state: "CO", zip: "80202", description_why: "I love creatures.")
 
     visit "/applications/#{application.id}"
-    
+
     expect(page).to have_content(application.name)
     expect(page).to have_content("Application status: #{application.status}")
     expect(page).to have_content("Address: #{application.address}, #{application.city}, #{application.state} #{application.zip}")
-    expect(page).to have_content("Reasons for adoption: #{application.description_why}")
+    expect(page).to have_content("Description of why I would make a good home:")
+    expect(page).to have_content(application.description_why)
     expect(page).to have_content("Pets on this application:")
   end
 end
