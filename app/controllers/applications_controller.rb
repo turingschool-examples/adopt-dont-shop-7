4ceb846f8be7b1350a6f89770dabf9af8fb37e0b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-22 12:46:31 -0600

    add update method to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -19,8 +19,14 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def update 
+    application = Application.find(params[:id])
+    application.update(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
 private
   def application_params
-    params.permit(:name, :street_address, :city, :state, :zip, :description, :status)
+    params.permit(:name, :street_address, :city, :state, :zip, :description, :status, :reason)
   end
 end
