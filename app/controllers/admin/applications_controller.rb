a941a8c1af845728e10825fd5b2b90e812649276
Nick Sacco <nicksacco10@gmail.com>
2023-05-23 12:50:44 -0700

    feat: find pet app in controller and add query params to app approval path

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -4,8 +4,9 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def update
-    # @application = Application.find(params[:id])
-    # @pa_status = PetApplication.find(params[:id])
-    ## pet application params..
+    @application = Application.find(params[:id])
+    pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
+    pet_application.update(pet_app_status: "Approved")
+    redirect_to("/admin/applications/#{@application.id}")
   end
 end
