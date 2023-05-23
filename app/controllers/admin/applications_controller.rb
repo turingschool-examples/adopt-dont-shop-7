8cf54081074cd11a2e3b2cf5bfe5cd5fad308287
Nick Sacco <nicksacco10@gmail.com>
2023-05-23 13:27:05 -0700

    test/feat: Deny button and created conditional in controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -5,8 +5,13 @@ class Admin::ApplicationsController < ApplicationController
 
   def update
     @application = Application.find(params[:id])
-    pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
-    pet_application.update(pet_app_status: "Approved")
+    if params[:pet_app_status] == "Approved"
+      pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
+      pet_application.update(pet_app_status: "Approved")
+    else params[:pet_app_status] == "Denied"
+      pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
+      pet_application.update(pet_app_status: "Denied")
+    end
     redirect_to("/admin/applications/#{@application.id}")
   end
 end
