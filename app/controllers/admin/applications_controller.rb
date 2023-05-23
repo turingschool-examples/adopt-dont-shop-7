b8940412435f013fdb16cda114ee78dcd50e2dc2
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-23 13:54:59 -0600

    add update method to admin/applications controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -3,4 +3,10 @@ class Admin::ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
+
+  def update 
+    application_pet = ApplicationPet.find_application_pet(params[:pet_id], params[:application_id])
+    application_pet.update(status: params[:status])
+    redirect_to "/admin/applications/#{params[:application_id]}"
+  end
 end
