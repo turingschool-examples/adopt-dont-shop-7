8a729bf2a7001a7bd0df64ad4c6274ad0529e309
Javen Wilson <javenb022@gmail.com>
2023-05-23 17:34:54 -0400

    test: tests error if description isnt filled out

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Application Show Page", type: :feature do
     describe "I can see the following:" do
       it "can show applicant name and other attributes" do
         visit "/applications/#{@application_1.id}"
-        
+
         expect(page).to have_content("Applicant Name: #{@application_1.name}")
         expect(page).to have_content("Street Address: #{@application_1.street_address}")
         expect(page).to have_content("City: #{@application_1.city}")
@@ -17,9 +17,9 @@ RSpec.describe "Application Show Page", type: :feature do
         expect(page).to have_content("Zip Code: #{@application_1.zip_code}")
         expect(page).to have_content("Description: #{@application_1.description}")
         expect(page).to have_content("Status: #{@application_1.status}")
-        
+
         visit "/applications/#{@application_2.id}"
-        
+
         expect(page).to have_content("Applicant Name: #{@application_2.name}")
         expect(page).to have_content("Street Address: #{@application_2.street_address}")
         expect(page).to have_content("City: #{@application_2.city}")
@@ -28,80 +28,80 @@ RSpec.describe "Application Show Page", type: :feature do
         expect(page).to have_content("Description: #{@application_2.description}")
         expect(page).to have_content("Status: #{@application_2.status}")
       end
-      
+
       it "each pet name is a link to their show page" do
         visit "/applications/#{@application_1.id}"
         click_link("#{@pet_2.name}")
         expect(current_path).to eq("/pets/#{@pet_2.id}")
-        
+
         visit "/applications/#{@application_2.id}"
         click_link("#{@pet_1.name}")
         expect(current_path).to eq("/pets/#{@pet_1.id}")
-        
+
         visit "/applications/#{@application_3.id}"
         click_link("#{@pet_5.name}")
         expect(current_path).to eq("/pets/#{@pet_5.id}")
       end
     end
-    
+
     describe "Searching for Pets for an Application" do
       it "has a search function on page to 'Add a Pet to this Application'" do
         visit "/applications/#{@application_1.id}"
-        
+
         within "#pet_search" do
           expect(page).to have_content("Add a Pet to this Application")
           expect(page).to have_field("Search Pet")
-          
+
           fill_in("Search Pet", with: "Pepper")
           click_button("Search")
-          
+
           expect(current_path).to eq("/applications/#{@application_1.id}")
         end
         expect(page).to have_content("#{@pet_5.name}")
-        
+
         visit "/applications/#{@application_2.id}"
         within "#pet_search" do
           expect(page).to have_content("Add a Pet to this Application")
           expect(page).to have_field("Search Pet")
-          
+
           fill_in("Search Pet", with: "Marshall")
           click_button("Search")
-          
+
           expect(current_path).to eq("/applications/#{@application_2.id}")
         end
         expect(page).to have_content("#{@pet_4.name}")
       end
     end
-    
+
     describe "Adding pet to application after search" do
       it "can add a pet to an application" do
         visit "/applications/#{@application_1.id}"
         fill_in("Search Pet", with: "Pepper")
         click_button("Search")
-        
+
         expect(page).to have_content(@pet_5.name)
         expect(page).to have_button("Adopt This Pet")
-        
+
         click_button("Adopt This Pet")
-        
+
         expect(current_path).to eq("/applications/#{@application_1.id}")
         expect(page).to have_content(@pet_5.name)
         expect(page).to_not have_button("Adopt This Pet")
       end
     end
-    
+
     describe "Submitting an Application" do
       it "can enter description on why they would be a good owner and submit" do
         visit "/applications/#{@application_1.id}"
-        
+
         fill_in("Search Pet", with: "Pepper")
         click_button("Search")
         click_button("Adopt This Pet")
-        
+
         expect(page).to have_content(@pet_5.name)
         expect(page).to have_button("Submit Application")
         expect(page).to have_field("Description")
-        
+
         fill_in("Description", with: "Taxidermy Project")
         click_button("Submit Application")
         expect(current_path).to eq("/applications/#{@application_1.id}")
@@ -110,7 +110,7 @@ RSpec.describe "Application Show Page", type: :feature do
         expect(page).to_not have_button("Submit Application")
       end
     end
-    
+
     describe "Search pet render form" do
       it "renders search_pet pertial until application is submitted" do
         visit "/applications/#{@application_1.id}"
@@ -119,7 +119,7 @@ RSpec.describe "Application Show Page", type: :feature do
         click_button("Adopt This Pet")
         fill_in("Description", with: "Taxidermy Project")
         click_button("Submit Application")
-        
+
         expect(page).to_not have_field("Search Pet")
         expect(page).to_not have_button("Search")
         expect(page).to_not have_button("Submit Application")
@@ -131,9 +131,9 @@ RSpec.describe "Application Show Page", type: :feature do
         visit "applications/#{@application_6.id}"
         fill_in("Search Pet", with: "Marsh")
         click_button("Search")
-        
+
         expect(page).to have_content("Marshall")
-        
+
         fill_in("Search Pet", with: "Be")
         click_button("Search")
 
@@ -146,13 +146,24 @@ RSpec.describe "Application Show Page", type: :feature do
         visit "applications/#{@application_6.id}"
         fill_in("Search Pet", with: "mArS")
         click_button("Search")
-        
+
         expect(page).to have_content("Marshall")
-        
+
         fill_in("Search Pet", with: "BeeTH")
         click_button("Search")
 
         expect(page).to have_content("Beethoven")
+      end
+    end
+
+    describe "Has to have a description" do
+      it "throws an error to user that field must be filled out" do
+        visit "applications/#{@application_1.id}"
+
+        click_button("Submit Application")
+
+        expect(current_path).to eq("/applications/#{@application_1.id}")
+        expect(page).to have_content("Yo, fill the field please")
       end
     end
   end
