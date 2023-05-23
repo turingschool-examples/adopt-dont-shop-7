69ef059f8905e13cfaf49b08159600a952970687
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-23 16:54:32 -0600

    Feat: Add some seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,21 @@ Application.destroy_all
 Shelter.destroy_all
 Pet.destroy_all
 
-application = Application.create!(
+application_1 = Application.create!(
   name: "Fredrich Longbottom",
   address: "1234 1st St",
   city: "Denver",
   state: "CO",
   zip: "80202",
   description_why: "I love creatures."
+)
+application_2 = Application.create!(
+  name: "Mike Tyson",
+  address: "5678 2nd St",
+  city: "Los Angeles",
+  state: "CA",
+  zip: "90210",
+  description_why: "I love animalths."
 )
 
 shelter_1 = Shelter.create!(
@@ -25,21 +33,18 @@ shelter_1 = Shelter.create!(
   city: "Denver",
   rank: 1
 )
-
 shelter_2 = Shelter.create!(
   foster_program: true,
   name: "Seattle Animal Shelter",
   city: "Seattle",
   rank: 2
 )
-
 shelter_3 = Shelter.create!(
   foster_program: false,
   name: "Portland Animal Shelter",
   city: "Portland",
   rank: 3
 )
-
 shelter_4 = Shelter.create!(
   foster_program: true,
   name: "Dallas Animal Shelter",
@@ -54,7 +59,6 @@ pet_1 = shelter_1.pets.create!(
   name: "Alphonso",
   shelter_id: 1
 )
-
 pet_2 = shelter_1.pets.create!(
   adoptable: true,
   age: 4,
@@ -62,7 +66,6 @@ pet_2 = shelter_1.pets.create!(
   name: "Bailey",
   shelter_id: 1
 )
-
 pet_3 = shelter_1.pets.create!(
   adoptable: true,
   age: 2,
@@ -70,7 +73,6 @@ pet_3 = shelter_1.pets.create!(
   name: "Charlie",
   shelter_id: 1
 )
-
 pet_4 = shelter_1.pets.create!(
   adoptable: true,
   age: 5,
