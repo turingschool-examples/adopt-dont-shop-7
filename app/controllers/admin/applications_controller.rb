ce81d74bdb3417cb7e242555a22e53f51044287e
Nick Sacco <nicksacco10@gmail.com>
2023-05-23 10:49:27 -0700

    us12 controller and view wip

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,7 +1,6 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    @pa_status = PetApplication.find(params[:id])
   end
 
   def update
