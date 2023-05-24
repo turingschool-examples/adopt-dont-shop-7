8dea4a40d1c0843468f5fa1080b0f6729fea2461
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-24 12:28:17 -0600

    Feature: admin reject applications

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,21 +1,42 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    @application_pets = @application.pets
-    @app_petapp = @application.pet_applications
+    @pet_applications = PetApplication.find_applications(@application.id)
   end
 
   def update
-    if params[:pet_applications_status] == "Approved"
-      application = PetApplication.find(params[:application_id])
-      application.update(pet_applications_status: params[:pet_applications_status])
-      redirect_to "/admin/applications/#{application.application_id}"
-      # render :show
-    elsif params[:pet_applications_status] == "Rejected"
-      application = PetApplication.find(params[:application_id])
-      application.update(pet_applications_status: params[:pet_applications_status])
-      redirect_to "/admin/applications/#{application.application_id}"
-      # render :show
+    if params[:updated_status] == "Approved"
+      pet_application = PetApplication.find(params[:pet_application_id])
+      # how to better utilize private params here
+      pet_application.update(pet_applications_status: params[:updated_status])
+      redirect_to "/admin/applications/#{pet_application.application_id}"
+    elsif params[:updated_status] == "Rejected"
+      pet_application = PetApplication.find(params[:pet_application_id])
+      pet_application.update(pet_applications_status: params[:updated_status])
+      redirect_to "/admin/applications/#{pet_application.application_id}"
     end
   end
+
+  ## How and why would you utilize private params here and/or our find_applications class method from PetApplication
+  ## When we tried to utilize that it broke EVERYTHING
+  # private
+  # def application_params
+  #   params.permit(
+  #     :name,
+  #     :street_address,
+  #     :city,
+  #     :state,
+  #     :zip_code,
+  #     :description,
+  #     :status
+  #   )
+  # end
+
+  # def pet_application_params
+  #   params.permit(
+  #     :pet_id,
+  #     :application_id,
+  #     :pet_applications_status
+  #   )
+  # end
 end
