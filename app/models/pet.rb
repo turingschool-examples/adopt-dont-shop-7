f86ca17e0bd7ddd13843d3d6adb1ea7f143eba0f
Blaine Glasgow <glasgowb@outlook.com>
2023-05-24 07:10:59 -0700

    Feat: Make Actions find app and find app id

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,4 +12,13 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
+
+  def find_pet_app(app_id)
+    self.pet_applications.where(application_id: app_id).first
+  end
+
+  def find_pet_app_id(app_id)
+    self.pet_applications.where(application_id: app_id).pluck(:id).first
+  end
+
 end
