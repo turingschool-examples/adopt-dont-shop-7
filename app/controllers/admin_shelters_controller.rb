7e18b19c546910a7417681142ca9605ff787adef
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-23 20:17:16 -0600

    Refactor: Remove unneccessary code.

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -3,10 +3,4 @@ class AdminSheltersController < ApplicationController
     @shelters = Shelter.order_by_name_reversed
     @shelters_with_pending_apps = Shelter.with_pending_apps
   end
-
-  private
-
-  def shelter_params
-    params.permit(:id, :name, :address, :city, :state, :zip)
-  end
 end
