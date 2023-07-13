6721d210f0c90b0841bf3d911ce1b0794bc2a1fa
Allen Russell <garyrusselljr@gmail.com>
2023-07-13 17:37:05 -0600

    Add spec and view for pets show page links

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,6 +3,14 @@ require "rails_helper"
 RSpec.describe "application show page", type: :feature do
   before(:each) do
     @application = Application.create!(name: "Mr. Ape", street: "123 Turing Lane", city: "Boulder", state: "Colorado", zip: "80301", description: "I really want a dog because I love dogs", status: "In Progress")
+    
+    @shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+
+    @pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: @shelter.id)
+    @pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: @shelter.id)
+
+    @app_pet_1 = ApplicationPet.create!(application_id: @application.id, pet_id: @pet_1.id)
+    @app_pet_2 = ApplicationPet.create!(application_id: @application.id, pet_id: @pet_2.id)
   end
 
   describe "as a visitor" do
@@ -20,9 +28,23 @@ RSpec.describe "application show page", type: :feature do
         expect(page).to have_content(@application.status)
       end
 
-      it "I can see the names of all pets this application is for with links to their show page"
+      it "I can see the names of all pets this application is for with links to their show page" do
 
-      it "show the applications status to be either 'In Progress', 'Pending', 'Accepted', or 'Rejected'"
+        visit "/applications/#{@application.id}"
+
+        expect(page).to have_content(@pet_1.name)
+        expect(page).to have_content(@pet_2.name)
+        
+        expect(page).to have_link(@pet_1.name)
+        click_link @pet_1.name
+        expect(current_path).to eq("/pets/#{@pet_1.id}")
+
+        visit "/applications/#{@application.id}"
+
+        expect(page).to have_link(@pet_2.name)
+        click_link @pet_2.name
+        expect(current_path).to eq("/pets/#{@pet_2.id}")
+      end
     end
   end
 end
