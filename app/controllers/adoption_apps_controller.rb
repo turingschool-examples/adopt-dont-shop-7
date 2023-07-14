dcd8e3886686ee2c70f1dec439b3f13562fe2264
Dani Wilson <danirae.powell@gmail.com>
2023-07-14 15:47:26 -0600

    Add new and create methods to adoption app controller

--- app/controllers/adoption_apps_controller.rb
+++ app/controllers/adoption_apps_controller.rb
@@ -2,4 +2,20 @@ class AdoptionAppsController < ApplicationController
   def show
     @adoption_app = AdoptionApp.find(params[:id])
   end
+
+  def new
+    # @adoption_app = AdoptionApp.find(params[:id])
+  end
+
+  def create
+    new_app = AdoptionApp.new({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      description: params[:description],
+      status: params[:status]
+    })
+  end
 end
