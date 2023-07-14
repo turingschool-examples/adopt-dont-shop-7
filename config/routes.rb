18157c5d5a8e5dda5ce6cfaeb4d0b301be9e20cc
pcbennett108 <pcbennett42@gmail.com>
2023-07-14 16:55:55 -0600

    feat: Add application form submission to applications route, view, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,6 @@ Rails.application.routes.draw do
 
   get "/applications", to: "applications#index"
   get "/applications/new", to: "applications#new"
+  post "/applications", to: "applications#create"
   get "/applications/:id", to: "applications#show"
 end
