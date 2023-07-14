657f1618b1445b3b37827e48efc2a84bc43569d0
Davis Weimer <davisweimer@gmail.com>
2023-07-14 14:17:29 -0600

    Create test db seeds in spec_helper
    Co-authored-by: Connor <richmond.connor@gmail.com>
    Co-authored-by: Dani <danirae.powell@gmail.com>
    Co-authored-by: Davis <davisweimer@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,30 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+def test_data
+  
+  @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+  @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+  @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+
+  @pet_1 = @shelter_1.pets.create(name: "Wilmur", breed: "tuxedo shorthair", age: 5, adoptable: false)
+  @pet_2 = @shelter_1.pets.create(name: "Pillbus", breed: "shorthair", age: 3, adoptable: true)
+  @pet_3 = @shelter_3.pets.create(name: "Limb", breed: "sphynx", age: 8, adoptable: true)
+  @pet_4 = @shelter_1.pets.create(name: "Toulouse", breed: "ragdoll", age: 5, adoptable: true)
+
+  @vet_office_1 = VeterinaryOffice.create(name: 'Best Vets', boarding_services: true, max_patient_capacity: 20)
+  @vet_office_2 = VeterinaryOffice.create(name: 'Vets R Us', boarding_services: true, max_patient_capacity: 20)
+
+  @not_on_call_vet = Veterinarian.create(name: 'Sam', review_rating: 10, on_call: false, veterinary_office_id: @vet_office_1.id)
+  @vet_1 = Veterinarian.create(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office_id: @vet_office_1.id)
+  @vet_2 = Veterinarian.create(name: 'Jim', review_rating: 8, on_call: true, veterinary_office_id: @vet_office_1.id)
+  @vet_3 = Veterinarian.create(name: 'Sarah', review_rating: 9, on_call: true, veterinary_office_id: @vet_office_2.id)
+
+  @adoption_app_1 = AdoptionApp.create!(name: "Suzie", street_address: "1234 Elmo Road", city: "Hoboken", state: "New Jersey", zip_code: "85790", description: "I will spoil all the pets", pet_names: "Wilmur", status: "Pending")
+  @adoption_app_2 = AdoptionApp.create!(name: "Fred", street_address: "4567 Grover Ave", city: "Tillamook", state: "Montana", zip_code: "45627", description: "I eat cats and honestly I am a terrible person", pet_names: "Pillbus", status: "Rejected")
+  @adoption_app_3 = AdoptionApp.create!(name: "Liam", street_address: "8910 Oscar Blvd", city: "Denver", state: "Colorado", zip_code: "80211", description: "Animals are awesome, adopt DONT shop!", pet_names: "Limb, Toulouse", status: "Approved")
+
+end
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
