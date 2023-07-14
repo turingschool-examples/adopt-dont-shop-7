b70bd53ccd327fc3eef2fedb2fc32042d54a6219
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-07-13 19:06:37 -0600

    refactor db schema table

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -11,9 +11,9 @@ before :each do
   @pet_3 = @shelter_3.pets.create!(adoptable: true, age: 5, breed: 'Poodle', name: 'Max')
   @pet_4 = @shelter_1.pets.create!(adoptable: true, age: 2, breed: 'Cunucu', name: 'Spud')
 
-  @application_1 = Application.create!(name: 'John Smith', address: '1234 Fake Street', city: 'Denver', state: 'CO', zip_code: 80202, description: 'Big Yak guy, but a dog will do', status: 'In Progress')
-  @application_2 = Application.create!(name: 'Jane Doe', address: '5678 Wannabe Road', city: 'Boulder', state: 'CO', zip_code: 80301, description: 'I love cats!', status: 'In Progress')
-  @application_3 = Application.create!(name: 'Joe Schmoe', address: '90210 Round Drive', city: 'Dallas', state: 'TX', zip_code: 75214, description: 'I like Turtles!', status: 'In Progress')
+  @application_1 = Application.create!(name: 'John Smith', street_address: '1234 Fake Street', city: 'Denver', state: 'CO', zip_code: 80202, description: 'Big Yak guy, but a dog will do', status: 'In Progress')
+  @application_2 = Application.create!(name: 'Jane Doe', street_address: '5678 Wannabe Road', city: 'Boulder', state: 'CO', zip_code: 80301, description: 'I love cats!', status: 'In Progress')
+  @application_3 = Application.create!(name: 'Joe Schmoe', street_address: '90210 Round Drive', city: 'Dallas', state: 'TX', zip_code: 75214, description: 'I like Turtles!', status: 'In Progress')
 
   @application_pet_1 = ApplicationPet.create!(application: @application_1, pet: @pet_1)
   @application_pet_2 = ApplicationPet.create!(application: @application_2, pet: @pet_2)
