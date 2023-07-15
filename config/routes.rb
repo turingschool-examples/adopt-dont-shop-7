0a4a129b546db9d2beebab4ae4ce52f2b686665b
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-15 15:13:13 -0500

    Adds adopt this pet button
    Co-authored-by: Gabe <thomastgtorres21@gmail.com>
    Co-authored-by: Daniel <daniel.gallagher92@gmail.com>"

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post "/applications", to: "applications#create"
   get "/applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
+  post "/application_pets", to: "application_pets#create"
 
   get "/shelters", to: "shelters#index"
   get "/shelters/new", to: "shelters#new"
