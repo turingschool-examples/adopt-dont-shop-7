6f62a1efc1ef82a7fd06c812a07b588cb269e65f
Dani Wilson <danirae.powell@gmail.com>
2023-07-15 08:23:29 -0600

    Add conditional and private to controller

--- app/controllers/adoption_apps_controller.rb
+++ app/controllers/adoption_apps_controller.rb
@@ -9,19 +9,18 @@ class AdoptionAppsController < ApplicationController
 
   def create
     created = "In Progress"
-    new_app = AdoptionApp.new({
-      name: params[:name],
-      street_address: params[:street_address],
-      city: params[:city],
-      state: params[:state],
-      zip_code: params[:zip_code],
-      description: params[:description],
-      pet_names: params[:pet_names],
-      status: created
-    })
+    new_app = AdoptionApp.new(adoption_app_params)
 
-    new_app.save    
-
+    if new_app.save    
     redirect_to "/adoption_apps/#{new_app.id}"
+    else
+      flash[:notice] = "Application not created: Required information missing."
+      render :new
+    end
+  end
+
+  private
+  def adoption_app_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
   end
 end
