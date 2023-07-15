acc1e299354079bbe4af081a6fe7ee54cfc9bb14
Artemy Gibson <artemygibson@gmail.com>
2023-07-14 21:46:19 -0700

    Test for error messages and valid form submission

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -61,4 +61,111 @@ RSpec.describe "Application Creation" do
     expect(page).to have_content("I work from home and will be around all day to offer companionship")
     expect(page).to have_content("In Progress")
   end
+
+  # As a visitor
+  # When I visit the new application page
+  # And I fail to fill in any of the form fields
+  # And I click submit
+  # Then I am taken back to the new applications page
+  # And I see a message that I must fill in those fields.
+
+  it "should raise error if user doesn't fill out name" do
+    visit "/applications/new"
+
+    fill_in(:street_address, with: "253 Doggie Lane")
+    fill_in(:city, with: "Kitty City")
+    fill_in(:state, with: "WA")
+    fill_in(:zipcode, with: "35467")
+    fill_in(:reason_for_adoption, with: "I work from home and will be around all day to offer companionship")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: Name can't be blank")
+  end
+
+  it "should raise error if user doesn't fill out address" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:city, with: "Kitty City")
+    fill_in(:state, with: "WA")
+    fill_in(:zipcode, with: "35467")
+    fill_in(:reason_for_adoption, with: "I work from home and will be around all day to offer companionship")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: Street address can't be blank")
+  end
+
+  it "should raise error if user doesn't fill out city" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:street_address, with: "253 Doggie Lane")
+    fill_in(:state, with: "WA")
+    fill_in(:zipcode, with: "35467")
+    fill_in(:reason_for_adoption, with: "I work from home and will be around all day to offer companionship")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: City can't be blank")
+  end
+
+  it "should raise error if user doesn't fill out state" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:street_address, with: "253 Doggie Lane")
+    fill_in(:city, with: "Kitty City")
+    fill_in(:zipcode, with: "35467")
+    fill_in(:reason_for_adoption, with: "I work from home and will be around all day to offer companionship")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: State can't be blank")
+    #remake the form with a dropdown?
+  end
+
+  it "should raise error if user doesn't fill out zipcode" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:street_address, with: "253 Doggie Lane")
+    fill_in(:city, with: "Kitty City")
+    fill_in(:state, with: "WA")
+    fill_in(:reason_for_adoption, with: "I work from home and will be around all day to offer companionship")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: Zipcode can't be blank")
+  end
+
+  it "should raise error if user doesn't fill out reason for adoption" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:street_address, with: "253 Doggie Lane")
+    fill_in(:city, with: "Kitty City")
+    fill_in(:state, with: "WA")
+    fill_in(:zipcode, with: "35467")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: Reason for adoption can't be blank")
+  end
+
+  it "may have multiple errors" do
+    visit "/applications/new"
+
+    fill_in(:name, with: "Jennifer")
+    fill_in(:state, with: "WA")
+    fill_in(:zipcode, with: "35467")
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("Error: Street address can't be blank, City can't be blank, Reason for adoption can't be blank")
+  end
+
+  #edge case- check that zip code is a 5-digit number
+  # validate that street address contains a number?
 end
