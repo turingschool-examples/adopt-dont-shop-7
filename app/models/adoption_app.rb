e4eda9a9d1fc1eaff7f0a015076399ddfe521c08
Connor Richmond <richmond.connor@gmail.com>
2023-07-15 14:42:11 -0400

    Another attempt at casing
    Co-authored-by: Connor <richmond.connor@gmail.com>
    Co-authored-by: Dani <danirae.powell@gmail.com>
    Co-authored-by: Davis <davisweimer@gmail.com>

--- app/models/adoption_app.rb
+++ app/models/adoption_app.rb
@@ -1,8 +1,12 @@
 class AdoptionApp < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+
   def adoption_status
     params[:status] = "In Progress"
     #if the application is submitted, it changes status to "In Progress"/defaults to "In Progress"
     #if the application is submitted AND approved, it changes status to "Approved"
     #If the application is submitted AND rejected, it changes status to "Rejected"
+    
   end
 end
