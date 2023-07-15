ade39351356b4d47c746d617f1476971a4be65a3
Matthew Lim <matlim200@gmail.com>
2023-07-15 13:19:23 -0700

    Feat: Added all functionality for user story 2

--- spec/features/applications/application_spec.rb
+++ spec/features/applications/application_spec.rb
@@ -48,26 +48,17 @@ RSpec.describe "application" do
   it "displays a link to specfic pets" do
     @shelter_1 = Shelter.create!(foster_program: true, name: "Denver Animal Shelter", city: "Denver", rank: 1)
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: @shelter_1.id)
-    @application_1 = Application.create!(name_of_applicant: "Matt Lim", street_address: "1234 Example St", city: "Denver", state: "CO", zip_code: 80202, description: "I love animals", pet_names: "Lucille Bald", application_status: "Pending", shelter_id: @shelter_1.id)
-    @application_2 = Application.create!(name_of_applicant: "Jorja Fleming", street_address: "5678 Sample St", city: "Denver", state: "CO", zip_code: 80202, description: "I am lonely and need cats", pet_names:"Fido", application_status: "Pending", shelter_id: @shelter_1.id)
+    @application_1 = Application.create!(name_of_applicant: "Matt Lim", street_address: "1234 Example St", city: "Denver", state: "CO", zip_code: 80202, description: "I love animals", application_status: "Pending", shelter_id: @shelter_1.id)
+    @application_2 = Application.create!(name_of_applicant: "Jorja Fleming", street_address: "5678 Sample St", city: "Denver", state: "CO", zip_code: 80202, description: "I am lonely and need cats", application_status: "Pending", shelter_id: @shelter_1.id)
     @application_1.pets << @pet_1
 
     visit "/applications/#{@application_1.id}"
-    
-    expect(page).to have_content("Matt Lim")
-    save_and_open_page
 
+    expect(page).to have_content("Matt Lim")
+   
     click_link 'Lucille Bald'
     expect(page).to have_current_path("/applications/pets/#{@pet_1.id}")
-
-
-    
-    # visit "/pets"
-    # expect(page).to have_link("Lucille Bald")
-    # save_and_open_page
-
-    # click_link 'Lucille Bald'
-    # expect(have_current_path).to eq("/pets/1")
-
   end
+
+
 end
