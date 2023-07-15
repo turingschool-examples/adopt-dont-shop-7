4f561468f64bcbe033cb4bc2717ec8795184008c
Dani Wilson <danirae.powell@gmail.com>
2023-07-15 08:41:09 -0600

    Edit create method to be how it was before

--- app/controllers/adoption_apps_controller.rb
+++ app/controllers/adoption_apps_controller.rb
@@ -9,7 +9,16 @@ class AdoptionAppsController < ApplicationController
 
   def create
     created = "In Progress"
-    new_app = AdoptionApp.new(adoption_app_params)
+    new_app = AdoptionApp.new({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      description: params[:description],
+      pet_names: params[:pet_names],
+      status: created
+    })
 
     if new_app.save    
     redirect_to "/adoption_apps/#{new_app.id}"
