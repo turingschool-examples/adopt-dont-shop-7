b3d4963846d9c0d64c42f9707ce867f5fa3ffa71
pcbennett108 <pcbennett42@gmail.com>
2023-07-15 21:17:22 -0600

    feat: Add vaildation to New Application form along with Flash :alert error message

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,8 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates :name, :street, :city, :state, :zip, :description, presence: true
+  validates :zip, length: { is: 5 }
+  validates :state, format: { with: /[A-Z]{2}/, message: "Please use 2 capital letters for the state abbreviation" }
 end
