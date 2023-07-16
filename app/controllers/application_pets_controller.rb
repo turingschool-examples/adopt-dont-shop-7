8279b220fdfa92433afe3a42efb35c236cb75bc4
Allen Russell <garyrusselljr@gmail.com>
2023-07-16 16:53:24 -0600

    feat: Add validation to application_pet spec and AplicationPet model to display error for duplicate pet entry

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -5,8 +5,13 @@ class ApplicationPetsController < ApplicationController
   def create
     application = Application.find(params[:application_id])
     pet = Pet.find(params[:pet_id])
-    application.pets << pet
-
-    redirect_to "/applications/#{application.id}"
+    application_pet = ApplicationPet.new(application_id: params[:application_id], pet_id: params[:pet_id])
+   
+    if application_pet.save
+      redirect_to "/applications/#{application.id}"
+    else 
+      redirect_to "/applications/#{application.id}"
+      flash[:alert] = "Error: #{error_message(application_pet.errors)}"
+    end
   end
 end
--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,6 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  validates :application_id, uniqueness: { scope: :pet_id, message: "already has that pet." }
 end
