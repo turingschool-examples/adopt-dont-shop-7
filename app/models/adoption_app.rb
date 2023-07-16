a8f878ba9b7587c5b592f029fc00f71b495b9204
Dani Wilson <danirae.powell@gmail.com>
2023-07-16 15:52:45 -0600

    Edit spacing

--- app/models/adoption_app.rb
+++ app/models/adoption_app.rb
@@ -8,6 +8,5 @@ class AdoptionApp < ApplicationRecord
     #if the application is submitted, it changes status to "In Progress"/defaults to "In Progress"
     #if the application is submitted AND approved, it changes status to "Approved"
     #If the application is submitted AND rejected, it changes status to "Rejected"
-    
   end
 end
