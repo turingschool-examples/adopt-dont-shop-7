ca5e8eb8935fa184d6202e3df463bdc3aebb3175
Kaina Cockett <kainacockett@gmail.com>
2023-07-16 10:18:11 -0600

    test:feat: 3 Application form sad path

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,13 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  attribute :description, :string, default: ""
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :status, presence: true
+  attribute :status, :string, default: "In Progress"
 end
