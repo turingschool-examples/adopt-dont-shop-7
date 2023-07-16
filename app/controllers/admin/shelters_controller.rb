caf26870139c41192b515bc46d49bbf75e1682f7
Artemy Gibson <artemygibson@gmail.com>
2023-07-15 20:23:23 -0700

    Create admin shelters controller

---
+++ app/controllers/admin/shelters_controller.rb
@@ -0,0 +1,11 @@
+class Admin::SheltersController < ApplicationController
+  def index
+    if params[:sort].present? && params[:sort] == "pet_count"
+      @shelters = Shelter.order_by_number_of_pets
+    elsif params[:search].present?
+      @shelters = Shelter.search(params[:search])
+    else
+      @shelters = Shelter.reverse_alphabetical
+    end
+  end
+end
