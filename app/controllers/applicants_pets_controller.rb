30d97f0386dac43f9baf49554eecf5ae6f279031
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-17 16:10:20 -0400

    Refactor: adds prevention of adding pet to an application if it was already added

--- app/controllers/applicants_pets_controller.rb
+++ app/controllers/applicants_pets_controller.rb
@@ -2,7 +2,14 @@ class ApplicantsPetsController < ApplicationController
   def create
     @applicant = Applicant.find(params[:applicant_id])
     @pet = Pet.find(params[:pet_id])
-    @applicant.pets << @pet
+
+    # Check if the pet is already associated with the applicant
+    if @applicant.pets.exists?(@pet.id)
+      flash[:error] = 'Pet is already associated with the applicant.'
+    else
+      @applicant.pets << @pet
+    end
+
     redirect_to applicant_path(@applicant)
   end
 end
