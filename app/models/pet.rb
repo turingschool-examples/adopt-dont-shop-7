baf511429c8192702bc801bc94630e200165ce2f
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-17 12:22:48 -0400

    Creates applications_pets table as a join table

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,10 @@
 class Pet < ApplicationRecord
+  belongs_to :shelter
+  has_many :applicant_pets
+  has_many :applicants, through: :applicant_pets
+
   validates :name, presence: true
   validates :age, presence: true, numericality: true
-  belongs_to :shelter
 
   def shelter_name
     shelter.name
