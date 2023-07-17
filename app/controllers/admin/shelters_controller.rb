96a2f34b19a241197bb228c6415a8dd120398491
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-17 17:14:26 -0500

    added controller action for pending applications
    Co-authored-by: Gabe thomastgtorres21@gmail.com
    Co-authored-by: Daniel daniel.gallagher92@gmail.co

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,7 +1,8 @@
 class Admin::SheltersController < ApplicationController
   
-def index
-  @shelters_all = Shelter.all
-  @shelters = Shelter.reverse_alphabetical_order
-end
+  def index
+    @shelters_all = Shelter.all
+    @shelters = Shelter.reverse_alphabetical_order
+    @shelters_with_pending_apps = Shelter.pending_applications
+  end
 end
