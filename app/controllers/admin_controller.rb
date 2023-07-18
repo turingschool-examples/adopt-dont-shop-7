4c20e51092c84726306d1de61671204b1b6a2bd2
Anna Wiley <awiley33@gmail.com>
2023-07-17 22:01:13 -0500

    feat wip: Add partial functionality for user story 12

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,13 @@ class AdminController < ApplicationController
   def shelters_index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
   end
+
+  def applications_show
+    @application = Application.find(params[:id])
+  end
+
+  def applications_update
+    @application = Application.find(params[:id])
+    @pet_application = PetApplication.find(params[:application_id, pet_id])
+  end
 end
