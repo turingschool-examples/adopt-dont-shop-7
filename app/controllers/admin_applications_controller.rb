8b8e9d1e9d24536954e536f77ae0250698c0afd8
Allen Russell <garyrusselljr@gmail.com>
2023-07-18 16:42:56 -0600

    feat: Add update method to controller

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,5 +1,12 @@
 class AdminApplicationsController < ApplicationsController
   def show
-    @applications = Application.find(params[:application_id])
+    @application = Application.find(params[:application_id])
+  end
+
+  def update
+    application = Application.find(params[:application_id])
+    application.update(status: "Approved")
+
+    redirect_to "/admin/applications/#{application.id}"
   end
 end
