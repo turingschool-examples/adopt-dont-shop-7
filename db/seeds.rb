25ca2207750afac91706891e00c8f049822f7a20
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-18 11:48:27 -0400

    refactor: updates seeds file for easier reading on seperate lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,46 +15,121 @@ Applicant.destroy_all
 
 # Create shelters
 shelters = Shelter.create([
-                            { name: 'Shelter 1', city: 'City 1', foster_program: true, rank: 1 },
-                            { name: 'Shelter 2', city: 'City 2', foster_program: false, rank: 2 },
-                            { name: 'Shelter 3', city: 'City 3', foster_program: false, rank: 3 }
+                            {
+                              name: 'Happy Tails Shelter',
+                              city: 'City 1',
+                              foster_program: true,
+                              rank: 1
+                            },
+                            {
+                              name: 'Caturdays are for the Boys',
+                              city: 'City 2',
+                              foster_program: false,
+                              rank: 2
+                            },
+                            {
+                              name: 'Barking Lot',
+                              city: 'City 3',
+                              foster_program: false,
+                              rank: 3
+                            }
                           ])
 
 # Create veterinary offices
 veterinary_offices = VeterinaryOffice.create([
-                                               { name: 'Vet Office 1', boarding_services: true,
-                                                 max_patient_capacity: 50 },
-                                               { name: 'Vet Office 2', boarding_services: false,
-                                                 max_patient_capacity: 30 },
-                                               { name: 'Vet Office 3', boarding_services: true,
-                                                 max_patient_capacity: 40 }
+                                               {
+                                                 name: 'Vet Office 1',
+                                                 boarding_services: true,
+                                                 max_patient_capacity: 50
+                                               },
+                                               {
+                                                 name: 'Vet Office 2',
+                                                 boarding_services: false,
+                                                 max_patient_capacity: 30
+                                               },
+                                               {
+                                                 name: 'Vet Office 3',
+                                                 boarding_services: true,
+                                                 max_patient_capacity: 40
+                                               }
                                              ])
 
 # Create veterinarians
 veterinarians = Veterinarian.create([
-                                      { name: 'Vet 1', veterinary_office: veterinary_offices.first, on_call: true,
-                                        review_rating: 4 },
-                                      { name: 'Vet 2', veterinary_office: veterinary_offices.first, on_call: false,
-                                        review_rating: 5 },
-                                      { name: 'Vet 3', veterinary_office: veterinary_offices.second, on_call: true,
-                                        review_rating: 3 }
+                                      {
+                                        name: 'Vet 1',
+                                        veterinary_office: veterinary_offices.first,
+                                        on_call: true,
+                                        review_rating: 4
+                                      },
+                                      {
+                                        name: 'Vet 2',
+                                        veterinary_office: veterinary_offices.first,
+                                        on_call: false,
+                                        review_rating: 5
+                                      },
+                                      {
+                                        name: 'Vet 3',
+                                        veterinary_office: veterinary_offices.second,
+                                        on_call: true,
+                                        review_rating: 3
+                                      }
                                     ])
 
 # Create pets
 pets = Pet.create([
-                    { name: 'Fluffy', age: 2, breed: 'Breed 1', shelter: shelters.first, adoptable: true },
-                    { name: 'Spike', age: 3, breed: 'Breed 2', shelter: shelters.second, adoptable: false },
-                    { name: 'Rex', age: 4, breed: 'Breed 3', shelter: shelters.third, adoptable: true }
+                    {
+                      name: 'Fluffy',
+                      age: 2,
+                      breed: 'Breed 1',
+                      shelter: shelters.first,
+                      adoptable: true
+                    },
+                    {
+                      name: 'Spike',
+                      age: 3,
+                      breed: 'Breed 2',
+                      shelter: shelters.second,
+                      adoptable: false
+                    },
+                    {
+                      name: 'Rex',
+                      age: 4,
+                      breed: 'Breed 3',
+                      shelter: shelters.third,
+                      adoptable: true
+                    }
                   ])
 
 # Create applicants
 applicants = Applicant.create([
-                                { name: 'Mike Wood', street_address: '123 Main St', city: 'City 1', state: 'State 1', zip_code: 12_345,
-                                  description: 'Description 1', status: 'In Progress' },
-                                { name: 'John Clay O\'Leary', street_address: '456 Elm St', city: 'City 2', state: 'State 2', zip_code: 67_890,
-                                  description: 'Description 2', status: 'In Progress' },
-                                { name: 'Jane Fonda', street_address: '789 Oak St', city: 'City 3', state: 'State 3', zip_code: 90_123,
-                                  description: 'Description 3', status: 'In Progress' }
+                                {
+                                  name: 'Mike Wood',
+                                  street_address: '123 Main St',
+                                  city: 'City 1',
+                                  state: 'State 1',
+                                  zip_code: 12_345,
+                                  description: 'Description 1',
+                                  status: 'In Progress'
+                                },
+                                {
+                                  name: "John Clay O'Leary",
+                                  street_address: '456 Elm St',
+                                  city: 'City 2',
+                                  state: 'State 2',
+                                  zip_code: 67_890,
+                                  description: 'Description 2',
+                                  status: 'In Progress'
+                                },
+                                {
+                                  name: 'Jane Fonda',
+                                  street_address: '789 Oak St',
+                                  city: 'City 3',
+                                  state: 'State 3',
+                                  zip_code: 90_123,
+                                  description: 'Description 3',
+                                  status: 'In Progress'
+                                }
                               ])
 
 # Create associations between applicants and pets
