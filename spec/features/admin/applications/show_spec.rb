57ae248509bb2828fc66dd6aa995e6a186fc2ccf
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-18 12:45:15 -0500

    Test: add tests and completed user story 14

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -74,5 +74,30 @@ RSpec.describe "admin application show page" do
         expect(page).to have_content("Rejected")
       end
     end
+    describe "When there are two applications in the system with the same pet" do
+      it "When I approve on one, its rejects the same pet on the other" do
+        shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+        scrappy = Pet.create(name: "Scrappy", age: 1, breed: "Great Dane", adoptable: true, shelter_id: shelter_1.id)
+        scooby = Pet.create(name: "Scooby", age: 1, breed: "Great Dane", adoptable: true, shelter_id: shelter_1.id)
+        application_1 = Application.create!(name: "Corey Chavez", street_address: "123 Happy Ln", city: "Eugene", state: "OR", zipcode: "12735", description: "Friendly", status: "Pending")
+        application_2 = Application.create!(name: "Korey Chavez", street_address: "123 Happy Ln", city: "Eugene", state: "OR", zipcode: "12735", description: "Friendlier", status: "Pending")
+        
+        application_1.pets << scrappy
+        application_2.pets << scrappy
+
+        visit "admin/applications/#{application_1.id}"
+
+        click_button "Approve"
+        
+        expect(page).to have_content("Approved")
+        
+        visit "admin/applications/#{application_2.id}"
+        
+        expect(page).to_not have_content("Rejected")
+        expect(page).to have_button("Reject")
+        expect(page).to have_button("Approve")
+
+      end
+    end
   end
 end
