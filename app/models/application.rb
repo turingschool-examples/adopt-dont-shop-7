dcb21125197921d3ab3e9417f9bbdf8262068a64
Parker Boeing <parker.boeing@gmail.com>
2023-07-18 23:03:57 -0600

    feat: add all_pets_approved? instance method to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,8 @@ class Application < ApplicationRecord
   validates :name, :street_address, :city, :state, :zip, :description, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def all_pets_approved?
+    pet_applications.all? { |pet_application| pet_application.status == "Approved" }
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,61 @@ RSpec.describe Application, type: :model do
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications)}
   end
+
+  describe "instance methods" do
+    before(:each) do
+    @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+    @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+    @pet_1 = @shelter_1.pets.create!(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)
+    @pet_2 = @shelter_1.pets.create!(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+    @pet_3 = @shelter_3.pets.create!(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    @pet_4 = @shelter_1.pets.create!(name: "Mugsy", breed: "mixed breed terrier", age: 5, adoptable: true)
+    @pet_5 = @shelter_2.pets.create!(name: "Luca", breed: "pitbull", age: 7, adoptable: true)
+    @pet_6 = @shelter_3.pets.create!(name: "Enzo", breed: "goldendoodle", age: 1, adoptable: true)
+    @application_1 = Application.create!(
+      name: "Bob",
+      street_address: "123 Fake St",
+      city: "Lander",
+      state: "WY",
+      zip: 82520,
+      description: "I am loving and attentive.",
+      status: "Accepted")
+    @application_2 = Application.create!(
+      name: "Sarah",
+      street_address: "321 Faux Ln",
+      city: "Salt Lake City",
+      state: "UT",
+      zip: 84104,
+      description: "I live in a small apartment but am lonely so want a pet.",
+      status: "In Progress")
+    @application_3 = Application.create!(
+      name: "Jen",
+      street_address: "475 Yikes Ave",
+      city: "Boulder",
+      state: "CO",
+      zip: 80301,
+      description: "My dog Rex is great with cats",
+      status: "Pending")
+    @application_4 = Application.create!(
+      name: "Dominique",
+      street_address: "321 Faux Ln",
+      city: "Bloomington",
+      state: "IL",
+      zip: 61701,
+      description: "I love animals.",
+      status: "Pending")
+    @pet_application_1 = PetApplication.create!(pet: @pet_1, application: @application_1)
+    @pet_application_2 = PetApplication.create!(pet: @pet_2, application: @application_1)
+    @pet_application_3 = PetApplication.create!(pet: @pet_5, application: @application_3)
+    @pet_application_4 = PetApplication.create!(pet: @pet_6, application: @application_4, status: "Approved")
+    @pet_application_5 = PetApplication.create!(pet: @pet_5, application: @application_4, status: "Approved")
+    @pet_application_6 = PetApplication.create!(pet: @pet_6, application: @application_3)
+  end
+    it "can check an application to see if all pets have been approved" do
+      require 'pry'; binding.pry
+      expect(@application_4.all_pets_approved?).to eq(true)
+      expect(@application_2.all_pets_approved?).to eq(false)
+    end
+  end
 end
