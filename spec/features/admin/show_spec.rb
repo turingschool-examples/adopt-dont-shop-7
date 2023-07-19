5208c6b13a9288519769ccaa1edf47c9fa6dfaef
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-19 12:35:15 -0400

    Adds test for user_story_12

---
+++ spec/features/admin/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin/applicants/applicant_id' do
+  # User Story 12 Test
+  it 'shows an approve applicant button' do
+    shelter1 = Shelter.create(
+      name: 'Aurora Shelter',
+      city: 'Aurora, CO',
+      foster_program: false,
+      rank: 9
+    )
+    pet1 = Pet.create(
+      name: 'Dr. Dog',
+      age: 3,
+      breed: 'Husky',
+      shelter: shelter1,
+      adoptable: true
+    )
+    applicant1 = Applicant.create(
+      name: 'John Smith',
+      street_address: '1234 Example St',
+      city: 'Longmont',
+      state: 'CO',
+      zip_code: '80503',
+      description: 'I love pets!'
+    )
+    ApplicantsPet.create(applicant: applicant1, pet: pet1)
+    applicant1.reload
+
+    visit "/pets/#{pet1.id}"
+    expect(page).to have_content("Shelter Name: #{shelter1.name}")
+
+    visit "/applicants/#{applicant1.id}"
+    expect(page).to have_content('Status: Pending')
+
+    visit '/admin/shelters'
+    within('#pending-applications-title + p', text: shelter1.name) do
+      expect(page).to have_content(shelter1.name)
+    end
+
+    visit "/admin/applicants/#{applicant1.id}"
+    expect(page).to have_button('Approve Adoption', class: 'approve-button')
+    click_button 'Approve Adoption'
+    expect(page).not_to have_button('Approve Adoption', class: 'approve-button')
+    expect(page).to have_content('Status: Approved')
+  end
+end
