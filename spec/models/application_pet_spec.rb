87d0817949566616971b8d7589c5f86586c1652c
Wil Fady <wrfady@gmail.com>
2023-07-18 21:48:49 -0400

    test: application pets model and validations

---
+++ spec/models/application_pet_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper" 
+
+RSpec.describe ApplicationPet, type: :model do
+  describe "relationships" do
+    it { should belong_to(:pet) }
+    it { should belong_to(:application) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:pet_id) }
+    it { should validate_presence_of(:application_id) }
+    it { should validate_presence_of(:approved) }
+    it { should validate_numericality_of(:pet_id) }
+    it { should validate_numericality_of(:application_id) }
+  end
+end
