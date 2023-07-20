6fd2d86d32f45d38f2c98d7c4d0073df5acff959
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-07-19 18:16:02 -0600

    refactor

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord 
-  validates :street_address, :city, :state, :zip_code, :description, :pet_description, presence: true
+  validates :name, :street_address, :city, :state, :zip_code, :description, :pet_description, presence: true
 
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
