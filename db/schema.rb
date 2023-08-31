9e438efab11383c0672a28c89e6057bb8b6f36cb
Nick Spencer <strangebrew202@gmail.com>
2023-08-31 15:43:25 -0600

    Add: additional columns to applicants table and rename address to street_address

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_210527) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_213747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "applicants", force: :cascade do |t|
     t.string "name"
-    t.string "address"
+    t.string "street_address"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
   end
 
   create_table "pets", force: :cascade do |t|
