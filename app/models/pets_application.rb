19f9cc71dc8397ee137739c567e50c74591a081c
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-31 16:09:42 -0500

    add: applicant model, pets_applications model, many to many associations and relations spec tests

---
+++ app/models/pets_application.rb
@@ -0,0 +1,6 @@
+class PetsApplication < ApplicationRecord
+  belongs_to :pet
+  belongs_to :applicant
+
+
+end
---
+++ db/migrate/20230831205054_create_pets_applications.rb
@@ -0,0 +1,10 @@
+class CreatePetsApplications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pets_applications do |t|
+      t.references :pets, null: false, foreign_key: true
+      t.references :applicants, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
