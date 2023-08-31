dc1cf6a45f76c6b508a107578d97bdd3a362737d
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-31 15:35:23 -0600

    test: add user story 1 test

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "Application Show",type: :feature do
+  describe "as a visitor" do
+    describe "when I visit an applications show page" do
+      it "then I can see the name, the full address of the applicant including street address, city, state, and zip code, description of why the applicant says they'd be a good home for this pet(s), names of all pets that this application is for (all names of pets should be links to their show page), and The Application's status, either 'In Progress', 'Pending', 'Accepted', or 'Rejected'" do
+        shelter = Shelter.create!(
+          foster_program: true,
+          name: "The Shelter",
+          city: "Happy City",
+          rank: 1
+        )
+
+        pet_1 = shelter.pets.create!(
+          adoptable: true,
+          age: 3,
+          breed: "orange cat",
+          name: "Cheesecake"
+        )
+
+        application = Application.create!(
+          name: "John Smith",
+          address: "1234 Lane Street, Happy City, CO, 80111",
+          description: "I want an animal",
+          pets: "#{pet_1.name}",
+          status: "Accepted"
+        )
+
+        visit "/applications/#{application.id}"
+
+        expect(page).to have_content("#{application.name}")
+        expect(page).to have_content("#{application.address}")
+        expect(page).to have_content("#{application.description}")
+        expect(page).to have_content("#{application.pets}")
+        expect(page).to have_content("#{application.status}")
+      end
+    end
+  end
+end
