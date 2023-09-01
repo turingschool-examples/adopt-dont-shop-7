319a6421486d3f1fb1729a1e3b5a571bab820024
Joop Stark <stark.joop@gmail.com>
2023-09-01 14:23:04 -0600

    feat: validation for applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,8 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  validates_presence_of :applicant_name
+  validates_presence_of :full_address
+  validates_presence_of :application_status
+  validates_presence_of :description
 end
