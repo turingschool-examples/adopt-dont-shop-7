96a392811615f61ba0440a2592c955f6b84dd72e
Joop Stark <stark.joop@gmail.com>
2023-09-01 14:21:05 -0600

    test: validation for applications

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe Application, type: :model do
   describe "relationships" do
     it { should have_many(:application_pets) }
     it { should have_many(:pets).through(:application_pets) }
+    it { should validate_presence_of :applicant_name }
+    it { should validate_presence_of :full_address}
+    it { should validate_presence_of :application_status }
+    it { should validate_presence_of :description }
   end
 end
