802944f4ce085197673450b1ddee208431363200
Antoine Aube <aaube3@gmail.com>
2023-09-01 17:22:57 -0600

    test: validations for pet_application model

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,7 @@
 class PetApplication < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+  validates :application_id, presence: true
+  validates :pet_id, presence: true
+  validates :status, presence: true
 end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe PetApplication, type: :model do
     it { should belong_to :application }
     it { should belong_to :pet }
   end
+
+  describe "validations" do 
+    it{ should validate_presence_of :application_id}
+    it{ should validate_presence_of :pet_id}
+    it{ should validate_presence_of :status}
+  end
 end
