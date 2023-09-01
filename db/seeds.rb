77695e177ef74925cca7c129fa307bf561f4ccf1
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-01 16:15:32 -0600

    feat: add data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+shelter = Shelter.create!(
+  foster_program: true,
+  name: "The Shelter",
+  city: "Happy City",
+  rank: 1
+)
+
+pet_1 = shelter.pets.create!(
+  adoptable: true,
+  age: 3,
+  breed: "orange cat",
+  name: "Cheesecake"
+)
+
+application = Application.create!(
+  name: "John Smith",
+  address: "1234 Lane Street, Happy City, CO, 80111",
+  description: "I want an animal",
+  pets: [pet_1],
+  status: "Accepted"
+)
