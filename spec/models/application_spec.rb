32cb955bfbe96efe0cf3c868ece42cfd179e65c7
Antoine Aube <aaube3@gmail.com>
2023-09-01 17:30:04 -0600

    test: validations for pet applications

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,7 +2,17 @@ require "rails_helper"
 
 RSpec.describe Application, type: :model do
   describe "realtionships" do
-    it { should have_many :pet_applications }
+    it { should have_many(:pet_applications) }
     it { should have_many(:pets).through(:pet_applications) }
+  end
+
+  describe "validations" do 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zipcode) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
   end
 end
