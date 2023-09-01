743d1362f8a3279ec46aa7cfa1a7126f0f254700
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-01 16:16:50 -0600

    Pass show test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "the application show" do
   it "shows the application and all of its attributes" do
     shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
     pet = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-    application_1 = Application.create(name: "John Dwayne", address: "1010 W 50th Ave, Denver, CO, 80020", description: "Background as a dog sitter", status: "Pending", pet_id: pet.id)
+    application = Application.create(name: "John Dwayne", address: "1010 W 50th Ave, Denver, CO, 80020", description: "Background as a dog sitter", status: "Pending")
 
-    visit "/applications/#{application_1.id}"
+    visit "/applications/#{application.id}"
 
-    expect(page).to have_content(application_1.name)
-    expect(page).to have_content(application_1.address)
-    expect(page).to have_content(application_1.description)
-    expect(page).to have_content(application_1.status)
-    expect(page).to have_content(application_1.pet_id)
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(application.status)
+    #expect(page).to have_content(application_1.pet_id)
   end
 end
