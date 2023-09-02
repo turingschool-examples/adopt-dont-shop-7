7a6714353a5fc1a358e07893d7bffceb9f516a89
Cory Powell <coryrpowell@gmail.com>
2023-09-02 17:53:47 -0400

    feat: added submission button with reasons for adopting

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get "/applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
-  post "/applications", to: "applications#create"
   patch "/applications/:id", to: "applications#update"
+  post "/applications", to: "applications#create"
 
   post "/applications/:id/pet_applications", to: "pet_applications#create"
 
