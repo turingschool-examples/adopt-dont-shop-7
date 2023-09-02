c7d227060688f018d5307b84a949694b859bbfe5
Nick Spencer <strangebrew202@gmail.com>
2023-09-02 17:06:41 -0600

    Add new @pets method in admin_controler#show, need to relocate this to model level just saving progress

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,18 @@ class AdminController < ActionController::Base
 
   end
 
-  def applications_show
-    
+  def show
+    @application = PetsApplication.find(params[:id])
+    @pets = Pet.joins(applicants: :pets_applications)
+            .where('pets_applications.applicant_id = ?', @application.applicant_id)
+            .select("pets_applications.status", "pets.name", "pets.id")
+            .distinct
+    # @pets = Applicant.find(@application.applicant_id).pets
+  end
+
+  def approve_reject
+    @application = PetsApplication.where('pet_id = ?', params[:pet_id])
+    @application.update(status: params[:status])
+    redirect_to "/admin/applications/#{params[:id]}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,6 @@ Rails.application.routes.draw do
   patch "/applications/:id", to: "applications#update"
 
   get "/admin/shelters", to: "admin#shelters_index"
-  get "/admin/shelters", to: "admin#applications_show"
+  get "/admin/applications/:id", to: "admin#show"
+  patch "/admin/applications/:id", to: "admin#approve_reject"
 end
