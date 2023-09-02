32fc3b91d075a2229ca6d879ebefa6f52004c633
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 13:11:03 -0700

    refactor: comment out validation test for description attribute for now

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Applicant, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip_code) }
-    it { should validate_presence_of(:description) }
+    # it { should validate_presence_of(:description) }
     it { should validate_presence_of(:application_status) }
   end
 end
