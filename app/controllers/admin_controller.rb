35c97126192bef1aab7980b3ffb9ba8fb586d188
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-02 14:15:53 -0500

    added sql functionality to order entries by reverse alphabetical order

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ActionController::Base
   def shelters_index
-
+    @shelters = Shelter.order_by_name_alphabetical
   end
 
   def applications_show
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -41,6 +41,12 @@ RSpec.describe Shelter, type: :model do
         expect(Shelter.order_by_number_of_pets).to eq([@shelter_1, @shelter_3, @shelter_2])
       end
     end
+
+    describe "order_by_name_alphabetical" do
+      it "returns a list of shelters in reverse desc order by name" do
+        expect(Shelter.order_by_name_alphabetical).to eq([@shelter_2, @shelter_3, @shelter_1])
+      end
+    end
   end
 
   describe "instance methods" do
