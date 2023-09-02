58c91a42bc87767ff2aa7b2ad527da0043dd31fd
Nick Spencer <strangebrew202@gmail.com>
2023-09-02 09:43:28 -0600

    Fix: fixed search form routing issue causing search function to not route back to application page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,6 +8,7 @@ class ApplicationsController < ApplicationController
     else
       @searched_pets = []
     end
+    @application = PetsApplication.find(params[:id])
     @applicant = Applicant.retrieve_applicant(params[:id])
     # @pets = list_pets(@applicant)
     @pets = @applicant.pets
