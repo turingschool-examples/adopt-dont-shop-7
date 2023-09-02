588864130a472dc6ff878ad49582df35034eb137
Will Chen <wfchen1990@gmail.com>
2023-09-02 13:28:09 -0600

    Add individual address params to application

--- db/migrate/20230902182202_create_applications.rb
+++ db/migrate/20230902182202_create_applications.rb
@@ -2,7 +2,10 @@ class CreateApplications < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
       t.string :applicant_name
-      t.string :address
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
       t.string :description
       t.string :pet_names
       t.string :status
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,13 +5,16 @@ RSpec.feature "the application show" do
     scenario 'displays all application info' do
 
 
-      application = Application.create!(applicant_name: "Thomas Jefferson", address: "123 Main St., Boston, MA, 12345", description: "I'm on a fiver", pet_names: "Zeus, Demeter", status: "In Progress")
+      application = Application.create!(applicant_name: "Thomas Jefferson", street_address: "123 Main St.", city: "Boston", state: "MA", zip_code: "12345", description: "I'm on a fiver", pet_names: "Zeus, Demeter", status: "In Progress")
 
       visit "/applications/#{application.id}"
       save_and_open_page
 
       expect(page).to have_content(application.applicant_name)
-      expect(page).to have_content(application.address)
+      expect(page).to have_content(application.street_address)
+      expect(page).to have_content(application.city)
+      expect(page).to have_content(application.state)
+      expect(page).to have_content(application.zip_code)
       expect(page).to have_content(application.description)
       expect(page).to have_content(application.pet_names)
       expect(page).to have_content(application.status)
