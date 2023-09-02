6c4effd4c3f145aab234ca81b416ec0217d32581
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 13:14:25 -0700

    feat: add new and create actions to applicants controller, as well as strong params for an applicant in a private method

---
+++ app/controllers/applicants_controller.rb
@@ -0,0 +1,20 @@
+class ApplicantsController < ApplicationController
+  def show
+    @applicant = Applicant.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Applicant.create!(applicant_params)
+    applicant = Applicant.find(params[:id])
+    redirect_to "/applicants/#{applicant.id}"
+  end
+
+  private
+
+  def applicant_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description, :application_status)
+  end
+end
