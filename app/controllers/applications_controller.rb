187033efb9bd84615dc6b211d89fe3b7741db414
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-01 18:46:49 -0600

    feat: add new and create actions to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,22 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    application = Application.create!({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      description: params[:description],
+      pets: [],
+      status: "In Progress"
+    })
+
+    redirect_to "/applications/#{application.id}"
+  end
 end
