332cdb81ff05741d1e933904d1bd3b888a3c4e6e
Nicholas McEnroe <nsmcenro@gmail.com>
2023-09-03 15:01:25 -0600

    Feat: Add case insensitive query searches and accompanying tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -109,12 +109,11 @@ RSpec.describe "the application show" do
   it "shows application and search and returns pet, even if the name doesnt't exactly mathch" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     application = Application.create(name:"Bob", address:"SF", city: "Town", state: "Colorado", zip: "12345", description: "Fuzzy", status: "In Progress")
-    # pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Bare-y Manilow", shelter_id: @shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter.id)
-    # pet_3 = Pet.create(adoptable: true, age: 1, breed: "domestic shorthair", name: "Sylvester", shelter_id: @shelter_2.id)
-    # pet_4 = Pet.create(adoptable: true, age: 1, breed: "orange tabby shorthair", name: "Lasagna", shelter_id: @shelter.id)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "fluffy", shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "fluff", shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 1, breed: "domestic shorthair", name: "mr. fluff", shelter_id: shelter.id)
     visit "/applications/#{application.id}"
-    fill_in( :search, with: 'lobster')
+    fill_in( :search, with: 'fluff')
     click_button('Search')
 
     expect(page).to have_content(application.name)
@@ -123,7 +122,31 @@ RSpec.describe "the application show" do
     expect(page).to have_content(application.state)
     expect(page).to have_content(application.zip)
     expect(page).to have_content(application.description)
+    expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_3.name)
+    expect(page).to have_content(application.status)
+  end
+
+  it "shows application and search and returns pet, even if the name doesn't match with capitalization" do
+    shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    application = Application.create(name:"Bob", address:"SF", city: "Town", state: "Colorado", zip: "12345", description: "Fuzzy", status: "In Progress")
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Fluffy", shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "FLUFF", shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 1, breed: "domestic shorthair", name: "Mr.FlUfF", shelter_id: shelter.id)
+    visit "/applications/#{application.id}"
+    fill_in( :search, with: 'fluff')
+    click_button('Search')
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_3.name)
     expect(page).to have_content(application.status)
   end
 end
