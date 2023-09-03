f1b0788c357b631a494c71d4b292ec9b33b68dc1
Joop Stark <stark.joop@gmail.com>
2023-09-03 00:49:03 -0600

    feat: approve button

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,6 +1,9 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    # @tables = ApplicationPet.find(params[:id])
+  
+    @pets = Pet.joins(:applications).where(applications: {id: @application.id})
+    
+    @status = ApplicationPet.where(application_id: "#{@application.id}")
   end
 end
