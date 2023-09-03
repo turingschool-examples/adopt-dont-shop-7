a63c4e4dcae4308e57b482c81b40a46e534c3200
Antoine Aube <aaube3@gmail.com>
2023-09-03 15:28:15 -0600

    feat: reverse alphabetical admin/shelters index page

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,7 +1,7 @@
 
   class Admin::SheltersController < ApplicationController
     def index
-      @shelters = Shelter.all
+      @shelters = Shelter.reverse_alphabetical
       # require 'pry';binding.pry
     end 
   end
