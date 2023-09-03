c34bf3197cd105b163fd7eeda4f525ff52e384cd
Cory Powell <coryrpowell@gmail.com>
2023-09-03 19:34:21 -0400

    feat: added the ability to show pending applications to the index

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,7 +2,9 @@
   class Admin::SheltersController < ApplicationController
     def index
       @shelters = Shelter.reverse_alphabetical
+      @pending_applications = Shelter.pending_applications
       # require 'pry';binding.pry
     end 
+
   end
 
