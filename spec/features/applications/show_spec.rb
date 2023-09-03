dfc20e4e2cfb9d6b20d76d36922ad5500b45142f
Nicholas McEnroe <nsmcenro@gmail.com>
2023-09-03 13:51:39 -0600

    Test: Add partial name search test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -105,4 +105,25 @@ RSpec.describe "the application show" do
     expect(page).to have_content(application.status)
     expect(page).to have_no_content("Submit Application")
   end
+
+  it "shows application and search and returns pet, even if the name doesnt't exactly mathch" do
+    shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    application = Application.create(name:"Bob", address:"SF", city: "Town", state: "Colorado", zip: "12345", description: "Fuzzy", status: "In Progress")
+    # pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Bare-y Manilow", shelter_id: @shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter.id)
+    # pet_3 = Pet.create(adoptable: true, age: 1, breed: "domestic shorthair", name: "Sylvester", shelter_id: @shelter_2.id)
+    # pet_4 = Pet.create(adoptable: true, age: 1, breed: "orange tabby shorthair", name: "Lasagna", shelter_id: @shelter.id)
+    visit "/applications/#{application.id}"
+    fill_in( :search, with: 'lobster')
+    click_button('Search')
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(application.status)
+  end
 end
