a253f841e37549a77367b359d20261cad5df9a21
Lane Bretschneider <lanebret85@gmail.com>
2023-09-03 12:43:38 -0700

    feat: add update action, @applicant_pets variable, and modify @pets to be an empty array if there aren't any pet names in the search params

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,10 +1,11 @@
 class ApplicantsController < ApplicationController
   def show
     @applicant = Applicant.find(params[:id])
+    @applicant_pets = @applicant.pets
     if params[:pet_name].present?
       @pets = Pet.search(params[:pet_name])
     else
-      @pets = @applicant.pets
+      @pets = []
     end
   end
 
@@ -19,6 +20,14 @@ class ApplicantsController < ApplicationController
       flash[:error] = 'Invalid data. Please fill out fields correctly.'
       redirect_to "/applicants/new"
     end
+  end
+
+  def update
+    applicant = Applicant.find(params[:applicant_id])
+    pet = Pet.find(params[:pet_id])
+    applicant.pets << pet
+
+    redirect_to "/applicants/#{params[:applicant_id]}"
   end
 
   private
