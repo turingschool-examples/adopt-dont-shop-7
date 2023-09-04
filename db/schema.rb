b4874c73bd947356d3327f3279a0b7d134b7cd73
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-04 10:52:39 -0600

    add migration for adoption_description column on applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_210212) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_165028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_210212) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "adoption_description"
   end
 
   create_table "pet_applications", force: :cascade do |t|
