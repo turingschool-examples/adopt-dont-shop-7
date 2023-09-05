9b82053661e87c9b14761394c037ebfdcb61fa5c
Antoine Aube <aaube3@gmail.com>
2023-09-04 21:23:11 -0600

    update method in pet applications controller to update the individual pet status

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -11,4 +11,9 @@ class PetApplicationsController < ApplicationController
     redirect_to "/applications/#{applicant.id}"
   end
 
+  def update
+    pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
+    pet_application.update(status: params[:status])
+    redirect_to "/admin/applications/#{params[:id]}"
+  end
 end
