e488de037fb63c0fa90f9193d8b3378949d2a3cc
Antoine Aube <aaube3@gmail.com>
2023-09-04 22:04:23 -0600

    feature: reject adoption on admin side

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -13,7 +13,8 @@ class PetApplicationsController < ApplicationController
 
   def update
     pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
-    pet_application.update(status: params[:status])
+    # require 'pry';binding.pry
+    pet_application.update!(status: params[:status])
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end
