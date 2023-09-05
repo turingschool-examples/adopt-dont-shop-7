fca687cfa0a3b91284e65e0bd97e2288eb6d2508
Allan Evans <oneblankpage27@gmail.com>
2023-09-05 17:01:03 -0600

    Attempt at US 15

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -31,7 +31,7 @@ class ApplicationsController < ApplicationController
     application = Application.find(params[:app_id])
     application.update({
       description: params[:reason],
-      status: [params[:app_status]]
+      status: params[:app_status]
     })
     application.save
     redirect_to "/applications/#{application.id}"
