a46dd3b09f0e8d003ffe11cec5271b080875f1ac
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-06 10:59:22 -0600

    test: add alphabetical_sort test for shelter model

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -67,5 +67,11 @@ RSpec.describe Shelter, type: :model do
         expect(@shelter_1.pet_count).to eq(3)
       end
     end
+
+    describe ".alphabetical_sort" do
+      it "returns shelters in reverse alphabetical order by name" do
+        expect(Shelter.alphabetical_sort).to eq([@shelter_2, @shelter_3, @shelter_1])
+      end
+    end
   end
 end
