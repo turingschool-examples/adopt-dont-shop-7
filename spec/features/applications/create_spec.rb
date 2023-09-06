483697769089601e9ddae17d2a34e39974d3c36e
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-06 11:00:05 -0600

    Add additional testing to create an application

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -1,6 +1,50 @@
 require "rails_helper"
 
 RSpec.describe "Application Creation" do
+
+    it "has link from the pet index page" do
+        visit "/pets"
+
+        expect(page).to have_link("Start an Application", href: "/applications/new")
+    end
+
+    it "has a form to create an application" do
+        visit "/pets" 
+
+        click_link("Start an Application")
+
+        expect(page).to have_field(:name)
+        expect(page).to have_field(:street_address)
+        expect(page).to have_field(:city)
+        expect(page).to have_field(:state)
+        expect(page).to have_field(:zip_code)
+        expect(page).to have_field(:reason_for_adoption)
+        expect(page).to have_button("Submit")
+    end
+
+    it "can create a application and redirect to applicant show page" do
+        visit "/applications/new"
+
+        fill_in(:name, with: "Roger")
+        fill_in(:street_address, with: "6767 Running Ln")
+        fill_in(:city, with: "Boulder")
+        fill_in(:state, with: "CO")
+        fill_in(:zip_code, with: "80233")
+        fill_in(:reason_for_adoption, with: "because")
+        click_button("Submit")
+
+        app = Application.last
+
+        expect(current_path).to eq("/applications/#{app.id}")
+        expect(page).to have_content("Roger")
+        expect(page).to have_content("6767 Running Ln")
+        expect(page).to have_content("Boulder")
+        expect(page).to have_content("CO")
+        expect(page).to have_content("80233")
+        expect(page).to have_content("because")
+        expect(page).to have_content("In Progress")
+    end
+
     it "should show an error if user does not fill out name" do
         visit "/applications/new"
 
