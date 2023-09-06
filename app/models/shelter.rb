c11eac09826a05f67f8912b5e437099850ea1702
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-06 10:56:59 -0600

    feat: add alphabetical sort method for shelter model

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -31,4 +31,8 @@ class Shelter < ApplicationRecord
   def shelter_pets_filtered_by_age(age_filter)
     adoptable_pets.where("age >= ?", age_filter)
   end
+
+  def self.alphabetical_sort
+    Shelter.order(name: :desc)
+  end
 end
