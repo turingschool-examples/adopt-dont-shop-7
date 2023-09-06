1b3c7b462b25cdb833f51ef552b02ffa4ea14938
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-05 20:20:22 -0600

    refactor: change description to owner_description to fit db, add Pending to inclusion of status

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,6 +7,6 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
-  validates :description, presence: true
-  validates :status, inclusion: {in: ["In Progress", "Accepted", "Rejected"]}
+  validates :owner_description, presence: true
+  validates :status, inclusion: {in: ["In Progress", "Accepted", "Rejected", "Pending"]}
 end
