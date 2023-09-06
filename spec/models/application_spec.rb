c5d5650cb3a2e23ff02c6611a998c6819294e4a2
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-05 19:22:55 -0600

    Finished model testing

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe Application do
         it {should validate_presence_of :zip_code}
         it {should validate_presence_of :reason_for_adoption}
     end
+
+    describe "find_pet_application_status" do
+        it "returns the application status for a pet" do
+          shelter_1 = Shelter.create(name: "Steven Shelter", city: "Fort Collins", foster_program: false, rank: 9)
+          pet_1 = shelter_1.pets.create(name: "Pickles", breed: "Pig", age: 5, adoptable: true)
+          pet_2 = shelter_1.pets.create(name: "Claws", breed: "Gator", age: 3, adoptable: true)
+          pet_3 = shelter_1.pets.create(name: "Steven", breed: "Horse", age: 3, adoptable: false)
+
+          application = Application.create(
+            name: "James Dean",
+            street_address: "456 Rock rd",
+            city: "Rainbow Rd",
+            state: "CA",
+            zip_code: "15678",
+            reason_for_adoption: "I need Steven",
+            status: "pending"
+          )
+    
+          PetApplication.create(pet: pet_3, application: application, application_status: "approved")
+    
+          expect(application.find_pet_application_status(pet_3.id)).to eq("approved")
+        end
+    end
 end
