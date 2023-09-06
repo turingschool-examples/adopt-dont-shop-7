438fe24f25a0fe92a920f28885f9fc1aaca6ac88
Will Chen <wfchen1990@gmail.com>
2023-09-06 11:06:40 -0600

    Add US16

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -23,16 +23,32 @@ RSpec.describe Application, type: :model do
         application2.pets << pet1
         application2.pets << pet2
         application2.pet_applications.find_by(pet_id: pet1.id).approve
-
+        
         application2.update_status
         expect(application2.status).to eq("Pending")
-
+        
         application2.pet_applications.find_by(pet_id: pet2.id).approve
+        application2.reload
 
         expect(application2.status).to eq("Pending")
         application2.update_status
+
         expect(application2.status).to eq("Approved")
       end
+
+      it 'US16 updates the status of an application to rejected if any associated pet_applications are rejected' do
+        application2 = Application.create!(applicant_name: "Benjamin Franklin", street_address: "456 Main St.", city: "Boston", state: "MA", zip_code: "12345", description: "I like turkeys", status: "Pending")
+        shelter1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+        pet1 = shelter1.pets.create!(name: "Zeus", breed: "Great Dane", age: 3, adoptable: true)
+        pet2 = shelter1.pets.create!(name: "Demeter", breed: "Golden Retriever", age: 4, adoptable: true)
+        application2.pets << pet1
+        application2.pets << pet2
+
+        application2.pet_applications.find_by(pet_id: pet1.id).reject
+        expect(application2.status).to eq("Pending")
+        application2.update_status
+        expect(application2.status).to eq("Rejected")
+      end 
     end
   end
 end
