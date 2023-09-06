2698b1ed46c70cd3c50b3a23814923ec1be926ed
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-06 10:30:54 -0600

    Implement view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,12 @@
 class AdminController < ApplicationController
 
-  def index 
-
+  def index
+    if params[:sort].present? && params[:sort] == "pet_count"
+      @shelters = Shelter.order_by_number_of_pets
+    elsif params[:search].present?
+      @shelters = Shelter.search(params[:search])
+    else
+      @shelters = Shelter.order_by_recently_created
+    end
   end
 end
