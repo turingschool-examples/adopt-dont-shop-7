e58c3444b072aa06621112bafa7f384415c0dec7
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-06 23:37:34 -0500

    add testing for admin/applications show page to see if approve and reject buttons work properly

--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -51,10 +51,7 @@ RSpec.feature "Admin Application Show", type: :feature do
 
         application.pets.each do |pet|
           expect(page).to have_button("Approve #{pet.name}")
-
           click_button("Approve #{pet.name}")
-
-          save_and_open_page
 
           expect(page).to have_no_button("Approve #{pet.name}")
           expect(page).to have_content("#{pet.name} has been approved")
@@ -111,12 +108,63 @@ RSpec.feature "Admin Application Show", type: :feature do
 
         visit "/admin/applications/#{application.id}"
 
-        pets.each do |pet|
+        application.pets.each do |pet|
           expect(page).to have_button("Reject #{pet.name}")
           click_button("Reject #{pet.name}")
 
           expect(page).to have_no_button("Reject #{pet.name}")
           expect(page).to have_content("#{pet.name} has been rejected")
+        end
+      end
+    end
+  end
+
+  describe "as a visitor" do
+    describe "when there are two applications in the system for the same pet" do
+      describe "when I visit the admin application show page for one of the applications and I approve or reject the pet for that application" do
+        describe "when I visit the other application's admin page" do
+          it "Then I do not see that the pet has been accepted or rejected for that application and instead I see buttons to approve or reject hte pet for this specific application" do
+            shelter = Shelter.create!(
+              foster_program: true,
+              name: "The Shelter",
+              city: "Happy City",
+              rank: 1
+            )
+            pet_1 = shelter.pets.create!(
+              adoptable: true,
+              age: 3,
+              breed: "orange cat",
+              name: "Cheesecake"
+            )
+            application_1 = Application.create!(
+              name: "John Smith",
+              street_address: "1234 Lane Street",
+              city: "Happy City",
+              state: "CO",
+              zip_code: "80111",
+              owner_description: "I want an animal",
+              status: "Pending"
+            )
+            application_2 = Application.create!(
+              name: "Jill Lane",
+              street_address: "3462 Cat Avenue",
+              city: "Cat City",
+              state: "CO",
+              zip_code: "80111",
+              owner_description: "I have fostered before.",
+              status: "Pending"
+            )
+            application_1.pets << pet_1
+            application_2.pets << pet_1
+
+            visit "/admin/applications/#{application_1.id}"
+
+            click_button("Approve #{pet_1.name}")
+
+            visit "/admin/applications/#{application_2.id}"
+
+            expect(page).to have_button("Approve #{pet_1.name}")
+          end
         end
       end
     end
