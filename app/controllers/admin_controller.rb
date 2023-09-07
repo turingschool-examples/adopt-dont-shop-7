cd93cca3408935e263a5b6630455fdf1044d4196
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-06 23:32:13 -0500

    updat admin_controller update and show methods

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,5 +6,19 @@ class AdminController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+    if !params[:relationship_id].nil?
+      @relationship = ApplicationPet.find(params[:relationship_id])
+    end
+  end
+
+  def update
+    relationship = ApplicationPet.where("pet_id = ? AND application_id = ?", params[:pet_id], params[:application_id])
+    if params[:status] == "approved"
+      relationship.first.update({status: "Approved"})
+    else
+      relationship.first.update({status: "Rejected"})
+    end
+
+    redirect_to "/admin/applications/#{params[:application_id]}"
   end
 end
