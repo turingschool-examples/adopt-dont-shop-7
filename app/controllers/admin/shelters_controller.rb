00dd743ec383af20e555f9e31d07c424b4516726
Captainlearyo <clayoleary@gmail.com>
2023-09-06 22:09:46 -0500

    fix admin shelter

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC;") #order(name: :desc)
+  
     @shelters_with_pending_applicants = Shelter.joins(pets: { applicants_pets: :applicant })
                                                .where(applicants: { application_status: 'Pending' })
                                                .distinct
