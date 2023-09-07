4cca1020ee1d5d9238f1009a1d1114961556c25c
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-06 18:47:15 -0600

    add rejection button that updates the associated petapplication status

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -49,5 +49,22 @@ RSpec.describe "Admin Applications Show" do
         expect(page).to have_content("Pet status: Approved")
       end
     end
+
+    it "Each pet has a rejection button that redirects to the same page and the pet is shown as rejected" do
+      visit "/admin/applications/#{@application_1.id}"
+
+      within("#pet-#{@pet_1.id}") do
+        expect(page).to have_button("Reject Pet")
+        click_button("Reject Pet")
+      end
+
+      expect(page).to have_current_path("/admin/applications/#{@application_1.id}")
+
+      within("#pet-#{@pet_1.id}") do
+        expect(page).to have_content("Pet status: Rejected")
+        expect(page).to_not have_button("Reject Pet")
+        expect(page).to_not have_button("Approve Pet")
+      end
+    end
   end
 end
