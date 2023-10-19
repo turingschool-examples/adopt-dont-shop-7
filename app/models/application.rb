37df4d3a0f18a72933ef03404ca520251103be7a
Anthea Yur <acyur6@gmail.com>
2023-10-19 17:26:13 -0500

    Create Many to Many relationships for application, application_pets, and pets

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,7 @@
 class Application < ApplicationRecord
+  has_many :application_pets
+  has_many :pets, through: :application_pets
+
   def address_join
     #ActiveRecord coding to join the entire address together
   end
