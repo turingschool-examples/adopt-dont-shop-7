eaca59e56ac0ab9db176e9284df533f91ff311ae
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-19 17:00:41 -0600

    Set up applications to pets relationship

---
+++ spec/features/application/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe "the application show" do
+  it "shows the applications and all of their attributes" do
+    application = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: "Bruno", status: "In Progress")
+    visit "/applications/#{application.id}"
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.street_address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip_code)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(application.pet_names)
+    expect(page).to have_content(application.status)
+  end
+end
