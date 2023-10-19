c606d8777f1cad11c18b7f75cce20c8491caf459
Anthea Yur <acyur6@gmail.com>
2023-10-19 14:53:24 -0500

    setup/ Add destroy_all to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+{Pets}.destroy_all
+{Shelters}.destroy_all
+{Veterinarians}.destroy_all
+{Veterinary_offices}.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
