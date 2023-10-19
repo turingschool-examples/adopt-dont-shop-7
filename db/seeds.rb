417ee7d79d0b22822db37d6cf318c5db7b0d49cd
Anthea Yur <acyur6@gmail.com>
2023-10-19 15:00:46 -0500

    setup/ Oopsies on curly brackets for seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-{Pets}.destroy_all
-{Shelters}.destroy_all
-{Veterinarians}.destroy_all
-{Veterinary_offices}.destroy_all
+Pets.destroy_all
+Shelters.destroy_all
+Veterinarians.destroy_all
+Veterinary_offices.destroy_all
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
