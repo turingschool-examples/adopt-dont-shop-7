34ed96138071da567a410daa321404a555f0612e
Nathan Trautenberg <Natester123@mac.com>
2023-10-19 13:50:52 -0600

    added weird line about destroying all models in the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# WHAT IS THIS?? RENDER SAID WE NEED? ASK INSTRUCTOR!! {Model}.destroy_all
+
