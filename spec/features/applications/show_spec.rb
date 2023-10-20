7c1d596b9b1b3fa7607f5fd063459967e0e94431
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-20 15:31:03 -0500

    Removed status from test data in show_spec.

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'application show page', type: :feature do
     @pet_2 = @shelter.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: @shelter.id)
     @pet_3 = @shelter.pets.create!(adoptable: true, age: 5, breed: "nebelong", name: "Pom Pom", shelter_id: @shelter.id)
     
-    @applicant1 = Application.create!(name: "Hannah Banana", street_address: "1234 Sugarwood Cir", city: "Newport", state: "Kentucky", zip_code: "41071", description: "I already have a cat and my cat Dave needs a friend. Dave is very friendly and other cat would be a great addition for our household!", status: "In progress")
-    @applicant2 = Application.create!(name: "Britney Spears", street_address: "250 Zimmerman Rd", city: "Hamburg", state: "New York", zip_code: "14075", description: "I am looking for my first dog. I am always home, because I am working from home. Since I am always around, I will be a great responsible dog owner!", status: "In progress")
+    @applicant1 = Application.create!(name: "Hannah Banana", street_address: "1234 Sugarwood Cir", city: "Newport", state: "Kentucky", zip_code: "41071", description: "I already have a cat and my cat Dave needs a friend. Dave is very friendly and other cat would be a great addition for our household!")
+    @applicant2 = Application.create!(name: "Britney Spears", street_address: "250 Zimmerman Rd", city: "Hamburg", state: "New York", zip_code: "14075", description: "I am looking for my first dog. I am always home, because I am working from home. Since I am always around, I will be a great responsible dog owner!")
   
     @applicant1.pets << @pet_1
     @applicant1.pets << @pet_3
