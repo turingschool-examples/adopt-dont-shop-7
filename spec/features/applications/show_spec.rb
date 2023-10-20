bd2c858cce6a8ee42194dff687426986e70c6c92
Taylor Pubins <tpubz@icloud.com>
2023-10-20 16:50:15 -0700

    Add US5 to [/applications/show_spec.rb]

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -70,5 +70,17 @@ RSpec.describe "applications show page" do
 
       expect(page).to have_content "Search Results: Buster"
     end
+
+    # As a visitor
+    # When I visit an application's show page
+    # And I search for a Pet by name
+    # And I see the names Pets that match my search
+    # Then next to each Pet's name I see a button to "Adopt this Pet"
+    # When I click one of these buttons
+    # Then I am taken back to the application show page
+    # And I see the Pet I want to adopt listed on this application
+    it "shows a button to adopt pet after search match" do
+      
+    end
   end
 end
