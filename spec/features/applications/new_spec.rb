a35f03b7960a2fb7c546586690f0afc92e6358bc
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-19 20:06:17 -0600

    Write feature tests for User Story 3 4 and 5

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "new application" do
 
   describe 'application new page' do
+    # User Story 2, Starting an Application
     it 'it will show a new application for the user to fill out' do
       application = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: "Bruno", status: "In Progress")
       
@@ -28,6 +29,22 @@ RSpec.describe "new application" do
       expect(page).to have_content("108 Clay St Hialeah FL 33010")
       expect(page).to have_content("I would like a dog")
       expect(page).to have_content("In Progress")
+    end
+    
+    # User Story 3, Starting an Application, Form not Completed
+    it 'will not let me submit form if fields not filled' do
+      visit '/applications/new'
+
+      fill_in("Name", with: "Arthur")
+      fill_in("Street address", with: "108 Clay St")
+      fill_in("City", with: "Hialeah")
+      fill_in("State", with: "FL")
+      fill_in("Zip code", with: "33010")
+
+      click_button("Submit")
+
+      expect(current_path).to eq('/applications/new')
+      expect(page).to have_content("You must fill in all fields.")
     end
   end
 end
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 RSpec.describe "the application show" do
+
+  # User Story 1, Application Show Page
   it "shows the applications and all of their attributes" do
     application = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: "Bruno", status: "In Progress")
     
@@ -13,5 +15,40 @@ RSpec.describe "the application show" do
     expect(page).to have_content(application.description)
     expect(page).to have_content(application.pet_names)
     expect(page).to have_content(application.status)
+  end
+
+  # User Story 4, Searching for Pets for an Application
+  it 'Alows search for pet by name' do
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_field("Enter a Pet's Name")
+    expect(page).to have_button("Search By Name")
+    
+    fill_in "search", with: "Bru"
+    click_button("Search")
+
+    expect(page).to have_content(@bruno.name)
+    expect(page).to have_content(@bruiser.name)
+  end
+
+  # User Story 5, Add a Pet to an Application
+  it 'Allows for pet adoption' do
+    visit "/applications/#{application.id}"
+    fill_in "search", with: "Bru"
+    click_button("Search")
+
+    within(".adopt_#{@bruno.id}") do
+      expect(page).to have_content "Adopt This Pet"
+    end
+    within(".adopt_#{@bruiser.id}") do
+      expect(page).to have_content "Adopt This Pet"
+    end
+
+    within(".adopt_#{@bruno.id}") do
+      click_button("Adopt This Pet")
+    end
+
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content(@bruno.name)
   end
 end
