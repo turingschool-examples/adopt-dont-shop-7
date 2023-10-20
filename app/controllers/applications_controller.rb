80d1c33904b6c514e94d7ad47f2617eabba7a656
Terry Lamb <terrylamb@gmail.com>
2023-10-19 23:46:07 -0400

    started user story 2, passing it off to Scott

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,21 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    application = Application.create(app_params)
+    # application.create!(app_params)
+    # require 'pry'; binding.pry
+    redirect_to("/applications/#{application.id - 1}")
+  end
+
+  private
+
+  def app_params
+    params.permit(:name, :address, :city, :state, :zip_code, :description)
+  end
 end
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -83,16 +83,36 @@ RSpec.describe "the pets index" do
   end
 
   #User Story 2
-  xit "Shows a link to 'Start an Application' that takes me to the new application page" do
+  it "Shows a link to 'Start an Application' that takes me to the new application page" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter.id)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: shelter.id)
     pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: shelter.id)
-
+    
     visit "/pets"
 
     click_on "Start an Application"
 
     expect(current_path).to eq "/applications/new"
+  end
+
+  it "when I click the 'Start an Application' link, I see a form with the following attributes" do
+    application = Application.create
+
+    visit "/applications/new"
+
+    fill_in("Name", with: "Joey Jones")
+    fill_in("Address", with: "100 Apple Ave")
+    fill_in("City", with: "Orlando")
+    fill_in("State", with: "FL")
+    fill_in("Zipcode", with: "56987")
+    fill_in("Description", with: "I have three other dogs to be friends with.")
+
+    click_button("Submit")
+
+    expect(current_path).to eq("/applications/#{application.id}")
+
+    # save_and_open_page
+    # expect(page).to have_content("In Progress")
   end
 end
