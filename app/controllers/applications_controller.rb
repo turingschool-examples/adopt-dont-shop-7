26db547c8c3408f8bb0013c05047c259a69795e3
Billy Burch <billyburch40@gmail.com>
2023-10-21 11:35:54 -0600

    create new application form through writing test, route, controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,18 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    Application.create(application_params)
+    redirect_to "/applications/#{Application.last.id}"    
+  end
+
+  private
+    def application_params
+      params.permit(:name, :street, :city, :state, :zip, :description, :status)
+    end
+ 
 end
