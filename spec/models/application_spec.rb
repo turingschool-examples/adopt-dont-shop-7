85c15d480ad5be5f8033898b4eae5ff3f97a124f
Matt Darlington <matthewsdarlington@gmail.com>
2023-10-20 23:21:48 -0600

    Comment out application_spec.rb to test if test fails are breaking remote site

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,24 +1,24 @@
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe Application, type: :model do 
-  before(:each) do 
-  end
+# RSpec.describe Application, type: :model do 
+#   before(:each) do 
+#   end
 
-    app = Application.create!({
-      name: "Joe Shmo", 
-      address: "2323 Wysteria Ln., Littleton, CO 89321",
-      descriptioissn: "Love pets; live on farm",
-      pet_names: "Socks, Juneau, Curtis, Stimpy",
-      status: "Pending"
-    })
+#     app = Application.create!({
+#       name: "Joe Shmo", 
+#       address: "2323 Wysteria Ln., Littleton, CO 89321",
+#       descriptioissn: "Love pets; live on farm",
+#       pet_names: "Socks, Juneau, Curtis, Stimpy",
+#       status: "Pending"
+#     })
 
-  describe "applications#show" do
-    it 'saves the contact info of applicants' do 
-      expect(app.name).to eq("Joe Shmo")
-      expect(app.address).to eq("2323 Wysteria Ln., Littleton, CO 89321")
-      expect(app.description).to eq("Love pets; live on farm")
-      expect(app.pet_names).to eq("Socks, Juneau, Curtis, Stimpy")
-      expect(app.status).to eq("Pending")
-    end
-  end
-end 
+#   describe "applications#show" do
+#     it 'saves the contact info of applicants' do 
+#       expect(app.name).to eq("Joe Shmo")
+#       expect(app.address).to eq("2323 Wysteria Ln., Littleton, CO 89321")
+#       expect(app.description).to eq("Love pets; live on farm")
+#       expect(app.pet_names).to eq("Socks, Juneau, Curtis, Stimpy")
+#       expect(app.status).to eq("Pending")
+#     end
+#   end
+# end 
