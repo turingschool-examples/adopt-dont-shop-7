78fc239c4a06f94ed3f72dfc2640fe741e9cc4f7
Miranda Delaney <delaney.miranda1@gmail.com>
2023-10-21 10:41:19 -0600

    feat: user story 4 finished with test passing

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Application Show Page' do
   before(:each) do
     @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     @pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: @shelter_1.id)
-    @pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: @shelter_1.id)
+    @pet_2 = @shelter_1.pets.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: @shelter_1.id)
     @pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: @shelter_1.id)
     
     #After creatings pets, put the pet link in place of pets for Stacy
@@ -26,4 +26,20 @@ RSpec.describe 'Application Show Page' do
     expect(page).to have_content("1870 Canopy Rd, Los Angeles, CA, 90001")
   end
 
+  # user story 4
+  it "displays option to add a pet to application" do
+    application = Application.create!(name: "Stacy Chapman", street_address: "1870 Canopy Rd", city: "Los Angeles", state: "CA", zip_code: 90001, description: "I grew up with dachshunds and felt really connected", status: "In Progress")
+    visit "/applications/#{application.id}"
+    
+    expect(page).to have_content("Add a Pet to this Application")
+
+    fill_in "Search for Pets", with: "Babe"
+
+    click_button "Submit"
+    expect(current_path).to eq("/applications/#{application.id}")
+    save_and_open_page
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to have_content(@pet_2.breed)
+    expect(page).to have_content(@pet_2.age)
+  end
 end
