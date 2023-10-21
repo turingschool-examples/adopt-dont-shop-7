f6f9e1c5a3bb2c5535f3dcfd7e292a3fa27494a5
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-21 17:58:31 -0500

    test: sad path testing- US 7 without a pet on an application a user is not able to submit an application for adoption. Model test: added testing of address method. All testing passes.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :status }
   end
+
+  describe "model method" do 
+    it 'gives you a string address' do
+      application1 = Application.create!(name: "Hannah Banana", street_address: "1234 Sugarwood Cir", city: "Newport", state: "Kentucky", zip_code: "41071", description: "I already have a cat and my cat Dave needs a friend. Dave is very friendly and other cat would be a great addition for our household!")
+
+      expect(application1.address).to eq("1234 Sugarwood Cir, Newport, Kentucky 41071")
+    end
+  end
 end
