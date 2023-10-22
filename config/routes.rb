954833628142777bf0500ad274445fba62b19660
Taylor Pubins <taylor.pubins@me.com>
2023-10-22 09:02:37 -0700

    Remove routes configurations for path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :applications do
-    member do
-      patch :add
-    end
-  end
-
   get "/", to: "application#welcome"
 
   get "/shelters", to: "shelters#index"
@@ -45,8 +39,8 @@ Rails.application.routes.draw do
   get "/veterinary_offices/:veterinary_office_id/veterinarians/new", to: "veterinarians#new"
   post "/veterinary_offices/:veterinary_office_id/veterinarians", to: "veterinarians#create"
 
-  get "/applications/new", to: "applications#new"
   post "/applications", to: "applications#create"
+  get "/applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
   patch "/applications/:id", to: "applications#update"
 
