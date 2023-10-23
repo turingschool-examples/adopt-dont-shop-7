596bbcbdf4070d7f1ecf6cc4a677a65d41399de5
Jimmy Trulock <ttrulock23@gmail.com>
2023-10-23 09:02:49 -0600

    testing for story 3

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -50,6 +50,7 @@ RSpec.describe "As a visitor" do
       expect(page).to have_content("I love dogs")
     end
   end
+  #3 Application error
   describe "When I visit the new application page And I fail to fill in any of the form fields And I click submit" do
     it "I am taken back to the new applications page And I see a message that I must fill in those fields." do
       visit "/applications/new"
