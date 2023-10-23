dabc4bb4fd8b721b0beb5495c0dec18e6d759935
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-23 16:20:44 -0600

    Write test for user story 15, all pets accepted

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "the application show" do
   before :each do
     @shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     @john = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: "Bruno", status: "In Progress")
-    @trevor = Application.create!(name: "Trevor Smith", street_address: "815 Ardsma Ave", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: [], status: "In Progress")  
-    @bruiser = Pet.create!(adoptable: true, age: 1, breed: "huskey", name: "Bruiser", shelter_id: @shelter.id)
+    @trevor = Application.create!(name: "Trevor Smith", street_address: "815 Ardsma Ave", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: [], status: "Pending")  
+    @bruiser = @trevor.pets.create!(adoptable: true, age: 1, breed: "huskey", name: "Bruiser", shelter_id: @shelter.id)
     @bruno = @john.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Bruno", shelter_id: @shelter.id)
-    @trixie = Pet.create!(adoptable: true, age: 7, breed: "pitbull", name: "Trixie", shelter_id: @shelter.id)
+    @trixie = @trevor.pets.create!(adoptable: true, age: 7, breed: "pitbull", name: "Trixie", shelter_id: @shelter.id)
     
   end
 
@@ -15,7 +15,7 @@ RSpec.describe "the application show" do
     # User Story 12, Approving a Pet for Adoption
     it 'has a button to approve the application for a specific pet' do
       visit "/admin/applications/#{@john.id}"
-      # save_and_open_page
+    
       expect(page).to have_content("Approve Adoption")
 
       click_button(@bruno.id)
@@ -26,6 +26,15 @@ RSpec.describe "the application show" do
       expect(page).to_not have_content("Adopt This Pet")
 
       expect(page).to have_content("This Adoption has been Approved")
+    end
+
+    #User Story 15, All Pets Accepted on an Application
+    it 'approves all pets for an application' do
+      visit "/admin/applications/#{@trevor.id}"
+      click_button(@bruiser.id)
+      click_button(@trixie.id)
+
+      expect(@trevor.status).to eq("Approved")
     end
   end
 end
