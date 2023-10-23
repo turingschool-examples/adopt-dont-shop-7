3583b1996eb87bbb65113e62e64bd5703e63a271
Taylor Pubins <tpubz@icloud.com>
2023-10-23 09:16:23 -0700

    Merge commit

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,18 +3,23 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    application = Application.create!({
+    application = Application.new({
       name: params[:name],
       address: params[:address],
       address_s: params[:address_s],
       city: params[:city],
       state: params[:state],
-      zip: params[:zip],
+      zip: params[:zip_code],
       description: params[:description],
-      status: "In Progress"
+      status: params[:status]
     })
 
-    redirect_to "/applications/#{application.id}"
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      flash.alert = "Each field must be complete"
+      redirect_to "/applications/new"
+    end
   end
 
   def show
@@ -25,10 +30,13 @@ class ApplicationsController < ApplicationController
   end
 
   def update
-    # use silent create here so if unique combination isn't entered, then just reloads page
-    PetApplication.create(application_id: params[:id], pet_id: params[:adopt])
-
-    redirect_to "/applications/#{params[:id]}"
+    application = Application.find(params[:id])
+    application.update({
+      status: "Pending",
+      reason_for_good_owner: params[:reason_for_good_owner]
+    })
+    application.save
+    redirect_to "/applications/#{application.id}"
   end
 
   private
