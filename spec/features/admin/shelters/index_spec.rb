9925baed054f8db48099f269610a4638a79babe6
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-23 12:19:16 -0600

    Rewrite testing for user story 11 to include sad and happy paths, within blocks

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'admin#shelters' do
     @shelter_3.pets.create!(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
     @bruno = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Bruno", shelter_id: @shelter_1.id)
     @john = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", pet_names: [@bruno.name], status: "Pending")
+    @john.add_pet(@bruno)
   end
 
   describe 'Admin Shelters#index' do
@@ -27,8 +28,18 @@ RSpec.describe 'admin#shelters' do
     visit '/admin/shelters'
 
     expect(page).to have_content("Shelters with Pending Applications")
-    expect("Shelters with Pending Applications").to appear_before(@shelter_1.name)
-    expect("Shelters with Pending Applications").to_not appear_before(@shelter_2.name)
-    expect("Shelters with Pending Applications").to_not appear_before(@shelter_3.name)
+    within('div', :text => 'Shelters with Pending Applications') do
+      expect(page).to have_content(@shelter_1.name)
+    end
+  end
+
+  it 'will not show shelters with no pending applications' do
+    visit '/admin/shelters'
+
+    expect(page).to have_content("Shelters with Pending Applications")
+    within('div', :text => 'Shelters with Pending Applications') do
+      expect(page).to_not have_content(@shelter_2.name)
+      expect(page).to_not have_content(@shelter_3.name)
+    end
   end
 end
