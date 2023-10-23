08f4bf62102ce1cb824528079fcd6f6bab2fac44
Anthea Yur <acyur6@gmail.com>
2023-10-23 13:05:45 -0500

    Debug and fix code to properly follow US 7

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Application Show Page' do
   before(:each) do
     @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    
     @pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: @shelter_1.id)
     @pet_2 = @shelter_1.pets.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: @shelter_1.id)
     @pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: @shelter_1.id)
-    @application = Application.create!(name: "Stacy Chapman", street_address: "1870 Canopy Rd", city: "Los Angeles", state: "CA", zip_code: 90001, description: "I grew up with dachshunds and felt really connected", status: "In Progress")
     @pet_4 = @shelter_1.pets.create(adoptable: true, age: 5, breed: "catahoula", name: "Chispa", shelter_id: @shelter_1.id)
     @pet_5 = @shelter_1.pets.create(adoptable: true, age: 9, breed: "chihuahua", name: "Tiny", shelter_id: @shelter_1.id)
+
+    @application = Application.create!(name: "Stacy Chapman", street_address: "1870 Canopy Rd", city: "Los Angeles", state: "CA", zip_code: 90001, description: "I grew up with dachshunds and felt really connected", status: "In Progress")
+    @application_2 = Application.create!(name: "Charlie Moon", street_address: "340 Walker St", city: "San Diego", state: "CA", zip_code: 91911, description: "I really am hoping to find a new companion for my parrot", status: "In Progress")
+
   end
 
   it 'displays the attribute of the applicant' do
@@ -84,8 +88,6 @@ RSpec.describe 'Application Show Page' do
       fill_in "Search for Pets", with: "Chispa"
       click_button "Submit"
       click_button "Adopt #{@pet_4.name}"
-
-      @application_2 = Application.create!(name: "Charlie Moon", street_address: "340 Walker St", city: "San Diego", state: "CA", zip_code: 91911, description: "I really am hoping to find a new companion for my parrot", status: "In Progress")
     end
 
     it "can include multiple pets on the application" do
@@ -95,14 +97,9 @@ RSpec.describe 'Application Show Page' do
       expect(page).to have_content(@pet_4.name)
     end
 
-    it "does not have a submit application button if there are no animals" do
-      visit "/applications/#{@application_2.id}"
-      save_and_open_page
-      expect(page).to_not have_content("Submit This Application")
-    end
-
-    xit "has an input section during final submission to explain why user wants to adopt pet(s) and after submission removes option to add more pets" do
+    it "has an input section during final submission to explain why user wants to adopt pet(s) and after submission removes option to add more pets" do
       visit "/applications/#{@application.id}"
+      save_and_open_page
       fill_in "Application input", with: "I work remote and have the resources and financial ability to support an animal"
       save_and_open_page
       click_button "Submit This Application"
@@ -113,11 +110,15 @@ RSpec.describe 'Application Show Page' do
       expect(page).to have_content(@pet_4.name)
       expect(page).to_not have_content("Search for Pets")
     end
+  end
 
+  ## USER STORY 7
+  it "does not have a submit application button if there are no animals" do
+    visit "/applications/#{@application_2.id}"
+    expect(page).to_not have_content("Submit This Application")
   end
 
   # USER STORY 8
-
   it "displays partial matches for pet names in search" do
     visit "/applications/#{@application.id}"
     fill_in "Search for Pets", with: "Bare"
