8e3550df7c257373464e3f2138038bd44bed39e7
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-23 12:30:31 -0600

    Rewrite methods to utilize relationships and create new variable fo shelters with pending apps

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,10 +1,7 @@
 class Admin::SheltersController < ApplicationController
   def index
-    if "All Shelters".present?
-      @shelters = Shelter.order_reverse_alphabetical
-    elsif "Shelters with Pending Applications".present?
-      @shelters = Shelter.shelter_with_pending_apps
-    end
+    @shelters = Shelter.order_reverse_alphabetical
+    @shelters_with_pending = Shelter.with_pending_apps
   end
 
   private
