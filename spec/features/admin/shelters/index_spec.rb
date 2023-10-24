a3c6972b9cb1b380541fe5b1eab24e35ac86b508
Miranda Delaney <delaney.miranda1@gmail.com>
2023-10-24 16:09:47 -0600

    showing shelters with pending apps

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -10,18 +10,26 @@ RSpec.describe "Admin Index Page" do
   # USER STORY 10
   it "lists all the shelter names in reverse alphabetical order" do
     visit "/admin/shelters"
-    save_and_open_page
     first = find("#shelter-#{@shelter_2.id}")
     second = find("#shelter-#{@shelter_3.id}")
     third = find("#shelter-#{@shelter_1.id}")
     expect(first).to appear_before(second)
     expect(second).to appear_before(third)
   end
-  # RAW SQL - in shelter model
-  # def self.order_by_reverse_alphabetical
-  # find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
 
-  # in shelter controller inside of admin subdir
-  # def index 
-  #   @shelters = Shelter.order_by_reverse_alphabetical
+  # USER STORY 11
+  
+  it "displays all shelters in the system that have a pending application" do
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: @shelter_1.id)
+    application = Application.create!(name: "Stacy Chapman", street_address: "1870 Canopy Rd", city: "Los Angeles", state: "CA", zip_code: 90001, description: "I grew up with dachshunds and felt really connected", status: "In Progress")
+    visit "/applications/#{application.id}"
+    fill_in "Search for Pets", with: "Elle"
+    click_button "Submit"
+    click_button "Adopt #{pet_3.name}"
+    visit '/admin/shelters'
+    save_and_open_page
+    expect(page).to have_content("Shelters with Pending Applications")
+    expect(page).to have_content(@shelter_1.name)
+    
+  end
 end
