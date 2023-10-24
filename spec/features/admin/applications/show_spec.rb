86ed3cd6b3b1298b67cbe5a6c8e1ea68e8a15ea4
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-23 18:39:37 -0600

    Rewrite test for user story 12

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 RSpec.describe "the application show" do
   before :each do
     @shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    @john = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "In Progress")
-    @trevor = Application.create!(name: "Trevor Smith", street_address: "815 Ardsma Ave", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "In Progress")  
+    @john = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "Pending")
+    @trevor = Application.create!(name: "Trevor Smith", street_address: "815 Ardsma Ave", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "In Progress") 
+    @regan = Application.create!(name: "Regan Ocean", street_address: "46 N. Old York Road", city: "Willoughby", state: "OH", zip_code: "44094", description: "I like cats.", status: "Pending")
     @bruiser = Pet.create!(adoptable: true, age: 1, breed: "huskey", name: "Bruiser", shelter_id: @shelter.id)
     @bruno = @john.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Bruno", shelter_id: @shelter.id)
     @trixie = Pet.create!(adoptable: true, age: 7, breed: "pitbull", name: "Trixie", shelter_id: @shelter.id)
@@ -15,17 +16,16 @@ RSpec.describe "the application show" do
     # User Story 12, Approving a Pet for Adoption
     it 'has a button to approve the application for a specific pet' do
       visit "/admin/applications/#{@john.id}"
-      # save_and_open_page
-      expect(page).to have_content("Approve Adoption")
-
-      click_button(@bruno.id)
-
+      
+      expect(page).to have_button("Approve Adoption")
+      
+      click_button("Approve Adoption")
+      
       expect(current_path).to eq("/admin/applications/#{@john.id}")
-      expect(@john.status).to eq("Approved")
-      expect(@bruno.adoptable).to eq(false)
-      expect(page).to_not have_content("Adopt This Pet")
-
       expect(page).to have_content("This Adoption has been Approved")
+      expect(page).to_not have_button("Approve Adoption")
+      expect(@john.status).to eq("Accepted")
+      expect(@bruno.adoptable).to eq(false)
     end
   end
 end
