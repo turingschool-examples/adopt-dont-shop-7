9981d3fe18c40092c20fed4bc4080a656284669a
Victor Sanchez <vsanantone@gmail.com>
2023-10-23 20:21:21 -0500

    feat: add the create method and the application_params method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,14 +1,28 @@
 class ApplicationsController < ApplicationController
   def new
-
+    @application = Application.new
   end
 
   def create
-    
+    @application = Application.new(application_params)
+    if @application.save
+      redirect_to "/applications/#{@application.id}"
+    else
+      flash[:alert] = "Please fill in all required fields"
+      # flash[:alert] = "Error: #{error_message(@application.errors)}"
+      redirect_to "/applications/new"
+      # render :new
+    end
   end
   
   def show
     @application = Application.find(params[:id])
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
   end
 
 end
