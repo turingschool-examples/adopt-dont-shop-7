51b8e142420dd7360099900f547e823b32f49273
Victor Sanchez <vsanantone@gmail.com>
2023-10-23 21:06:10 -0500

    fix the new method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationsController < ApplicationController
   def new
-    @application = Application.new
   end
 
   def create
-    @application = Application.new(application_params)
-    if @application.save
-      redirect_to "/applications/#{@application.id}"
+    params[:application_status]= "In Progress"
+    application = Application.new(application_params)
+    if application.save
+      redirect_to "/applications/#{application.id}"
     else
       flash[:alert] = "Please fill in all required fields"
       # flash[:alert] = "Error: #{error_message(@application.errors)}"
@@ -22,7 +22,7 @@ class ApplicationsController < ApplicationController
   private
 
   def application_params
-    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description, :application_status)
   end
 
 end
