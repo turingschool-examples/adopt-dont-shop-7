5252e45b18b391b1141d2075451d8dd1ef320c9b
Eric Belongea <eric.belongea@gmail.com>
2023-10-23 22:13:56 -0600

    Add testing for 22

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -46,13 +46,28 @@ RSpec.describe "the shelter show" do
 
   it "Will only show shelter name and address when visited by admin" do
     shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-
-
+    
+    
     visit "/admin/shelters/#{shelter_1.id}"
-
+    
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_1.city)
     expect(page).to_not have_content(shelter_1.foster_program)
     expect(page).to_not have_content(shelter_1.rank)
+  end
+  
+  it "Has a statistics section showing average age of all pets" do
+    shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    
+    visit "/admin/shelters/#{@shelter_1.id}"
+    
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to_not have_content(shelter_1.foster_program)
+    expect(page).to_not have_content(shelter_1.rank)
+
+    within("div#statistics") do
+      expect(page).to have_content("Average Pet Age:")
+    end
   end
 end
