e8e49ed7d6e0b747e84bcc9b3a295d7cae691106
Scott DeVoss <scottdevoss@yahoo.com>
2023-10-24 15:03:45 -0600

    added user story 11 test

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,5 +1,7 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC;")
+    require 'pry'; binding.pry
+    @shelters_with_pending_applications
   end
 end
--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe "Admin Shelter Page", type: :feature do
         @shelter_1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
         @shelter_2 = Shelter.create!(name: "Lakewood shelter", city: "Lakewood, CO", foster_program: true, rank: 7)
         @shelter_3 = Shelter.create!(name: "Westminster shelter", city: "Westminster, CO", foster_program: false, rank: 2)
-        
+
+        @application_1 = Application.create(name: "Julie Johnson", street_address: "201 Main Street", city: "Seattle", state: "WA", zip_code: "75250", description: "I love dogs!", status: "In Progress")
+
+        @pet_1 = Pet.create!(adoptable: true, age: 1, breed: "doberman", name: "Auggie", shelter_id: @shelter_1.id)
       end 
 
       #User Story 10
@@ -22,6 +25,30 @@ RSpec.describe "Admin Shelter Page", type: :feature do
         expect(@shelter_3.name).to appear_before(@shelter_2.name)
         expect(@shelter_2.name).to appear_before(@shelter_1.name)
       end
+
+      #User Story 11
+      describe "Then I see a section for 'Shelters with Pending Applications'" do
+        it "I see a section with the name of every shelter that has a pending application" do
+          visit "/applications/#{@application_1.id}"
+
+          fill_in "Search for Pets", with: "Auggie"
+
+          click_button "Submit"
+
+          click_button "Adopt this Pet"
+
+          fill_in "Why would you make a good owner for these pet(s)?", with: "I've always loved dogs."
+
+          click_button "Submit Application"
+          
+          expect(page).to have_content("Pending")
+
+          visit "/admin/shelters"
+
+          expect(page).to have_content("Shelters with Pending Applications")
+          expect(page).to have_content("Aurora shelter")
+        end
+      end 
     end 
   end 
 end 
