d53660be0cacb11863cd38d4ce7e983ed493eed7
Blaine Kennedy <blainekennedy@gmail.com>
2023-10-24 15:48:36 -0500

    finish us 12 + 13 + 14

--- app/controllers/admin/application_pets_controller.rb
+++ app/controllers/admin/application_pets_controller.rb
@@ -2,9 +2,9 @@ class Admin::ApplicationPetsController < ApplicationController
   def update
     @application_pet = ApplicationPet.where("pet_id = ? AND application_id = ?", params[:pet_id], params[:application_id])
     if params[:status] == "approved"
-      @application_pet.applications.first.update({status: "Approved"})
+      @application_pet.first.update({status: "Approved"})
     else
-      @application_pet.applications.first.update({status: "Rejected"})
+      @application_pet.first.update({status: "Rejected"})
     end
 
     redirect_to "/admin/applications/#{params[:application_id]}"
