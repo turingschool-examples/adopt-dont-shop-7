d42480bdbecc180ad18c2266c71811aa64f6f933
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-10-25 16:51:10 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,10 @@
 @shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
 @pet = @shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: @shelter.id)
 @pet_2 = @shelter.pets.create!(name: "Scrappy", age: 1, breed: "Good Dane", adoptable: true, shelter_id: @shelter.id)
+@pet_3 = @shelter.pets.create!(name: "Tom", age: 3, breed: "Tom Cat", adoptable: true, shelter_id: @shelter.id)
+@pet_4 = @shelter.pets.create!(name: "Jerry", age: 4, breed: "House Mouse", adoptable: true, shelter_id: @shelter.id)
+
 @application = Application.create!(name: "Jimmy John", street_address: "111 lonely road", city: "John City", state: "AR", zip_code: "90909", description: "I like animals", status: "In Progress")
 @application_2 = Application.create!(name: "Papa John", street_address: "222 lonely road", city: "John City", state: "AR", zip_code: "90909", description: "I like animals", status: "In Progress")
+@application_3 = Application.create!(name: "John the Baptist", street_address: "333 desert road", city: "Bethany", state: "Judea", zip_code: "00001", description: "God sent me", status: "In Progress")
+@application_4 = Application.create!(name: "Noah", street_address: "444 flood street", city: "Flood", state: "World", zip_code: "00002", description: "I have 2 of every other animal", status: "In Progress")
