23581a1c3ab8f1e9bdc120ddf82de6490ca30fcb
Matt Darlington <matthewsdarlington@gmail.com>
2023-10-25 17:37:26 -0600

    Fix arguments to make tests pass

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,10 +1,11 @@
 class PetApplicationsController < ApplicationController
+  
   def create
-    pet_application = PetApplication.new({application: params[:application_id], params[:pet_id]})
+    pet_application = PetApplication.new({application_id: params[:application_id], pet_id: params[:pet_id]})
     if pet_application.save
-      redirect_to "/applications/#{application.id}"
+      redirect_to "/applications/#{params[:application_id]}"
     else
-      redirect_to "/applications/#{application.id}"
+      redirect_to "/applications/#{params[:application_id]}"
       flash[:alert] = "Error: #{error_message(application.errors)}"
     end
   end
