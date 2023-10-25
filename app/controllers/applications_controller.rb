8b4ed8ac403029bcd111200491a73a991715c67b
Matt Darlington <matthewsdarlington@gmail.com>
2023-10-25 17:34:02 -0600

    Added update to ApplicationsController

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,10 +9,16 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
-    @pets = @application.pets
     if params[:pet_name]
       @pets_search = Pet.search(params[:pet_name])
     end
   end
-  
+
+  def update
+    @application = Application.find(params[:id])
+    @application.update({ description: :why_good_owner, application_status: "Pending" })
+
+    redirect_to "/applications/#{@application.id}"
+  end
+
 end
