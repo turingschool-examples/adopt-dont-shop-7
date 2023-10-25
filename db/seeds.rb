7a715d177e2facda208f34d51ea85c2149671192
Nathan Trautenberg <Natester123@mac.com>
2023-10-25 15:36:57 -0600

    added destroy_all to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# WHAT IS THIS?? RENDER SAID WE NEED? ASK INSTRUCTOR!! {Model}.destroy_all
+Shelter.destroy_all
+Pet.destroy_all
+Application.destroy_all
+PetApplication.destroy_all
+Veterinarian.destroy_all
+VeterinaryOffice.destroy_all
 
 @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
 @shelter_2 = Shelter.create(name: "Littleton shelter", city: "Littleton, CO", foster_program: false, rank: 9)
