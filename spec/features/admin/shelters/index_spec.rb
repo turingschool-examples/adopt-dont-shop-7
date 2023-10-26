04b0875e18f82aeb1ae2b40e7d5b8373dec8885d
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-10-25 19:27:51 -0600

    Add testing for US 20

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,12 +5,18 @@ RSpec.describe 'admin#shelters' do
     @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+    @shelter_4 = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+
     @shelter_1.pets.create!(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
     @shelter_1.pets.create!(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
     @shelter_3.pets.create!(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    @shelter_4.pets.create!(adoptable: true, age: 3, breed: "doberman", name: "Bruno", shelter_id: shelter.id)
+    @shelter_4.pets.create!(adoptable: true, age: 7, breed: "pitbull", name: "Trixie", shelter_id: shelter.id)
     @bruno = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Bruno", shelter_id: @shelter_1.id)
+    @trevor = Application.create!(name: "Trevor Smith", street_address: "815 Ardsma Ave", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "Pending")  
     @john = Application.create!(name: "John Smith", street_address: "376 Amherst Street", city: "Providence", state: "RI", zip_code: "02904", description: "I am a good person.", status: "Pending")
     @john.add_pet(@bruno)
+
   end
 
   describe 'Admin Shelters#index' do
@@ -32,6 +38,7 @@ RSpec.describe 'admin#shelters' do
     expect(page).to have_content("Shelters with Pending Applications")
     within('section', :text => 'Shelters with Pending Applications') do
       expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_4.name)
     end
   end
 
@@ -42,6 +49,15 @@ RSpec.describe 'admin#shelters' do
     within('section', :text => 'Shelters with Pending Applications') do
       expect(page).to_not have_content(@shelter_2.name)
       expect(page).to_not have_content(@shelter_3.name)
+    end
+  end
+
+  it 'shows shelters with pending applications in alpha order' do
+    visit '/admin/shelters'
+    
+    expect(page).to have_content("Shelters with Pending Applications")
+    within('section', :text => 'Shelters with Pending Applications') do
+      expect(@shelter_4.name).to appear_before(@shelter_1.name)
     end
   end
 end
