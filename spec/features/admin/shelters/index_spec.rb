7a28353db4fdee06ac4ad99de2d3c7206e3897b0
Matt Darlington <matthewsdarlington@gmail.com>
2023-10-25 20:55:15 -0600

    Remove save_and_open_page

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
-RSpec.describe "Admin Shelter Index" do 
-  before(:each) do 
+RSpec.describe "Admin Shelter Index" do
+
+  before(:each) do
     @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 3) 
     @shelter_2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create!(name: 'Boulder shelter', city: 'Boulder, CO', foster_program: false, rank: 9)
-   end
+  end
 
   it "shows all Shelters in the system in reverse alphabetical order by name" do
     
@@ -13,7 +14,5 @@ RSpec.describe "Admin Shelter Index" do
 
     expect(@shelter_2.name).to appear_before(@shelter_3.name) 
     expect(@shelter_3.name).to appear_before(@shelter.name)
-
-    save_and_open_page
   end
 end 
