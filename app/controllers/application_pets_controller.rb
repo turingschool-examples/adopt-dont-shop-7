85e2b7fe86fd00589eeff9e3171ef7f2547ea54a
Joseph Lee <jhjlee702@gmail.com>
2023-10-26 10:12:34 -0600

    bring in code from another branch that was left out

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -14,9 +14,13 @@ class ApplicationPetsController < ApplicationController
   end
 
   def update_application(application)
+    application = Application.find(params[:id])
+    application_pets = application.application_pets
     if application.has_rejected_pets
       application.update(status: 3)
-    else
+    elsif application.has_pending_pets
+      application.update(status: 1)
+    else 
       application.update(status: 2)
     end
   end
