6ef991a7ed0b9ef98f45f37a3ebe44ebfeecffc3
Sam Puttman <daputter2@gmail.com>
2023-12-08 12:55:53 -0500

    Add index to applications. Update controller to handle index and create actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationsController < ApplicationController
+
+  def index
+    @applications = Application.all
+  end
+  
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.new(application_params)
+    if @application.save
+      redirect_to "/applications/#{@application.id}"   
+    else
+      Rails.logger.debug @application.errors.full_messages
+      render :new
+    end
+  end
+
   def show
         @applicant = Application.find(params[:id])
         @pets = @applicant.pets
   end
 end
 
+private
+
+def application_params
+  params.permit(:applicant_name, :address, :city, :state, :zip_code, :description)
+end
