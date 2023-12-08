5acb8a6e1fd9320c9c690e525bf2b2c7da1a8bdc
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-08 16:58:49 -0700

    Add new and create actions to applications controller to allow applicants to create a new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,21 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new 
+
+  end
+
+  def create 
+    application = Application.create!({
+      name: params[:name],
+      street: params[:street],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      descr: params[:good_owner]
+    })
+    
+    redirect_to "/applications/#{application.id}"
+  end
 end
