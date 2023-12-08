b3c24c314b19876b2a343e31fa5dec57f88843b0
Sam Puttman <daputter2@gmail.com>
2023-12-08 15:14:25 -0500

    Added error to new.html.erb that triggers when form fields are blank.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
+  validates :applicant_name, :address, :city, :state, :zip_code, :description, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 end
