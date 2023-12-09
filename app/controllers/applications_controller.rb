d0f98416f6b9a4a15991db228fd9ff65a71f75db
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-09 15:44:33 -0700

    Refactor applications#create using strong params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,23 +4,8 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    application = Application.new({
-      name: params["name"],
-      street_address: params["Street Address"],
-      city: params["City"],
-      state: params["State"],
-      zip_code: params["Zip Code"],
-      description: params["Description"],
-      application_status: "In Progress"
-    })
-
-    if application.save
-      redirect_to "/applications/#{application.id}"
-    else
-      flash[:alert] = "All fields must be filled in before you can proceed"
-      redirect_to "/applications/new"
-    end
     application = Application.new(application_params)
+    
     if application.save
       redirect_to "/applications/#{application.id}"
     else
