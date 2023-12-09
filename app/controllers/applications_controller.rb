452b72534322e843f6c98fff20de99e96007de26
Nathan Lambertson <lambertones@gmail.com>
2023-12-09 15:24:32 -0600

    Add partial progress of US 3 > > Co-authored-by: lambo1986 <lambertones@gmail.com> Co-authored-by: s2an <A141100397+s2an@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,14 +10,18 @@ class ApplicationsController < ApplicationController
     input_params = application_params
     address = "#{input_params[:street_address]}, city: #{input_params[:city]}, state: #{input_params[:state]}, zip: #{input_params[:zip_code]}"
 
+    if input_params.values.any?(&:blank?)
+      flash[:alert] = "All fields are required."
+      redirect_to "/applications/new"
+      return
+    end
+
     @application = Application.create!(
       name: input_params[:name],
       address: address,
       description: input_params[:description],
       status: "In Progress"
     )
-
-    redirect_to "/applications/#{@application.id}"
   end
 
     private
--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -21,4 +21,20 @@ RSpec.describe "the application new" do
     expect(page).to have_content("Fred Flintstone")
     expect(page).to have_content("In Progress")
   end
+
+  it "returns user to the form if fields not completed" do
+    visit "/applications/new"
+    expect(page).to have_button("Submit Application")
+
+    fill_in("street_address", with: "123 Bedrock St")
+    fill_in("city", with: "Rockville")
+    fill_in("state", with: "Croatan")
+    fill_in("zip_code", with: "12345")
+    fill_in("description", with: "Good with dinos!")
+
+    click_on("Submit Application")
+
+    expect(current_path).to eq("/applications/new")
+    expect(page).to have_content("All fields are required.")
+  end
 end
