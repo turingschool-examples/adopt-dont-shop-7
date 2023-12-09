ed62cb5b386a6853f794d9e26845b3bb5f12a404
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-08 21:49:05 -0700

    add pet search to controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,7 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+    @pets = @application.pet_search(params[:pet_name])
   end
 
   def new 
