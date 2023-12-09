c10dab90cb93d20c0e2ed2d0deba6a1ea17ef186
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-08 21:49:43 -0700

    define new instance method #pet_search

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,9 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
  
+  def pet_search(name)
+    Pet.where('name = ?', name)
+  end
 
 
 end
