b8187b4be97aca2465014e153b978643c0a13a22
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-09 14:39:53 -0500

    Pair Progress on User Story 2 > > Co-authored-by: lambo1986 <lambertones@gmail.com> Co-authored-by: s2an <A141100397+s2an@users.noreply.github.com>

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "the application new" do
+  it "has a form to create a new application" do
+
+    visit "/applications/new"
+    expect(page).to have_button("Submit Application")
+
+    fill_in("name", with: "Fred Flintstone")
+    fill_in("street_address", with: "123 Bedrock St")
+    fill_in("city", with: "Rockville")
+    fill_in("state", with: "Croatan")
+    fill_in("zip_code", with: "12345")
+    fill_in("description", with: "Good with dinos!")
+
+    click_on("Submit")
+
+    expect(current_path).to eq("/applications/:id")
+    expect(page).to have_content("Fred Flinstone")
+  end
+end
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,13 @@ RSpec.describe "the pets index" do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it "has a link to start the application" do
+    visit "/pets"
+
+    click_link("Start an Application")
+
+    expect(page).to have_current_path("/applications/new")
+  end
+
 end
