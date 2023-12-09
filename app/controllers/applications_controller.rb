b191cb62ac2b6ddc5f0bd11cf69cea56df5a6c4f
Nathan Lambertson <lambertones@gmail.com>
2023-12-09 16:29:40 -0600

    Finish US 3 > > Co-authored-by: lambo1986 <lambertones@gmail.com> Co-authored-by: s2an <A141100397+s2an@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,21 +7,22 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    input_params = application_params
-    address = "#{input_params[:street_address]}, city: #{input_params[:city]}, state: #{input_params[:state]}, zip: #{input_params[:zip_code]}"
+    address = "#{application_params[:street_address]}, city: #{application_params[:city]}, state: #{application_params[:state]}, zip: #{application_params[:zip_code]}"
 
-    if input_params.values.any?(&:blank?)
+    if application_params.values.any?(&:blank?)
       flash[:alert] = "All fields are required."
       redirect_to "/applications/new"
       return
     end
 
     @application = Application.create!(
-      name: input_params[:name],
+      name: application_params[:name],
       address: address,
-      description: input_params[:description],
+      description: application_params[:description],
       status: "In Progress"
     )
+
+    redirect_to "/applications/#{@application.id}"
   end
 
     private
