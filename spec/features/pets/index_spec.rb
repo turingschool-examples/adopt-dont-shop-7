0e378346538fc96c8c01ab65392431c86bb4afb7
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-08 17:32:03 -0700

    Remove all feature testing for filling out and submitting a new application to features/applications/create_spec.rb file

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -98,49 +98,5 @@ RSpec.describe "the pets index" do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
-
-  it "has a link to start an application" do 
-    visit "/pets"
-
-    expect(page).to have_link("Start an Application")
-    
-    click_link("Start an Application")
-
-    expect(current_path).to eq("/applications/new")
-
-    expect(page).to have_content("New Application")
-
-    expect(page).to have_content("Name:")
-    expect(page).to have_field(:name)
-    expect(page).to have_content("Street address:")
-    expect(page).to have_field(:street)
-    expect(page).to have_content("City:")
-    expect(page).to have_field(:city)
-    expect(page).to have_content("State:")
-    expect(page).to have_field(:state)
-    expect(page).to have_content("Zip code:")
-    expect(page).to have_field(:zip)
-    expect(page).to have_content("What makes me a good owner:")
-    expect(page).to have_field(:good_owner)
-
-    fill_in(:name, with: "Matt")
-    fill_in(:street, with: "2636 Vrain St.")
-    fill_in(:city, with: "Denver")
-    fill_in(:state, with: "CO")
-    fill_in(:zip, with: "80212")
-    fill_in(:good_owner, with: "B/c I'm awesome!")
-
-    expect(page).to have_button("Submit")
-
-    click_button "Submit" 
-    
-    expect(page).to have_content("Matt")
-    expect(page).to have_content("2636 Vrain St.")
-    expect(page).to have_content("Denver")
-    expect(page).to have_content("CO")
-    expect(page).to have_content("80212")
-    expect(page).to have_content("B/c I'm awesome!")
-    expect(page).to have_content("In Progress")
-  end
-
+  
 end
