b8709f95a7da73733ab71924c54d7345af70f8be
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-08 20:29:56 -0700

    Add not empty validations for all application attributes

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,6 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
  
-
+  validates_presence_of :name, :street, :city, :state, :zip, :descr
 
 end
