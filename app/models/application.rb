99923605050135572304371ea8bc7fb609c82d94
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-09 21:24:38 -0700

    Add enum status

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,9 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
  
-  validates_presence_of :name, :street, :city, :state, :zip, :descr
+  validates_presence_of :name, :street, :city, :state, :zip, :descr, :status
+
+  enum status: {"In progress": 0, "Pending": 1, "Approved": 2, "Rejected": 3}
 
   def pet_search(name) 
     Pet.where("name = ?", name)
