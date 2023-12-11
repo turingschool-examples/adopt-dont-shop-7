2ad7377f5b94ee951f7c405a33a51bf4f36b09d2
Martin Chavez <martinchavez1203@gmail.com>
2023-12-11 15:45:22 -0700

    add validation tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,6 +5,17 @@ RSpec.describe Application, type: :model do
       it { should have_many(:pets) }
     end
 
+    describe 'Validations' do
+      it { should validate_presence_of :name}
+      it { should validate_presence_of :street_address}
+      it { should validate_presence_of :city}
+      it { should validate_presence_of :state}
+      it { should validate_presence_of :zip_code}
+      it { should validate_numericality_of :zip_code}
+      it { should validate_length_of :zip_code}
+      it { should validate_presence_of :description}
+    end
+
     before(:each) do
         @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
         @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
