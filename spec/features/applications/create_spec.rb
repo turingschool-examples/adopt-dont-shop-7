5d3a7cd74981573da8726bccd9ab657bc9e25f39
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 18:57:34 -0800

    US-3: Add validation for all user inputted application fields

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -1,36 +1,47 @@
 require "rails_helper"
 
-RSpec.describe "application create" do
+RSpec.describe "application creation" do
+  describe "the application new" do
+    it "renders the new form" do
 
-  it "renders the new form" do
+      visit "/applications/new"
 
-    visit "/applications/new"
-
-    # save_and_open_page
-
-    expect(page).to have_content("New Application")
-    expect(find("form")).to have_content("Name")
-    expect(find("form")).to have_content("Street Address")
-    expect(find("form")).to have_content("City")
-    expect(find("form")).to have_content("State")
-    expect(find("form")).to have_content("Zip Code")
-    expect(find("form")).to have_content("Description")
+      expect(page).to have_content("New Application")
+      expect(find("form")).to have_content("Name")
+      expect(find("form")).to have_content("Street Address")
+      expect(find("form")).to have_content("City")
+      expect(find("form")).to have_content("State")
+      expect(find("form")).to have_content("Zip Code")
+      expect(find("form")).to have_content("Description")
+    end
   end
 
+  describe "the application create" do
+    context "given valid data" do
+      it "creates the application and redirects to the created application show" do
+        visit "/applications/new"
 
-  it "creates the application and redirects to the created application show" do
-    visit "/applications/new"
+        fill_in "Name", with: "Bumblebee"
+        fill_in "Street Address", with: "123 Calle del Valle"
+        fill_in "City", with: "Puerto Vallarta"
+        fill_in "State", with: "Jalisco"
+        fill_in "Zip Code", with: 48290
+        fill_in "Description", with: "I like animals"
+        click_button "Save"
 
-    fill_in "Name", with: "Bumblebee"
-    fill_in "Street Address", with: "123 Calle del Valle"
-    fill_in "City", with: "Puerto Vallarta"
-    fill_in "State", with: "Jalisco"
-    fill_in "Zip Code", with: 48290
-    fill_in "Description", with: "I like animals"
-    click_button "Save"
-    save_and_open_page
-    expect(page).to have_content("Bumblebee")
-    expect(page).to have_content("Puerto Vallarta")
+        expect(page).to have_content("Bumblebee")
+        expect(page).to have_content("Puerto Vallarta")
+      end
+    end
+
+    context "given invalid data" do
+      it "re-renders the application new form" do
+        visit "/applications/new"
+        click_button "Save"
+
+        expect(page).to have_current_path("/applications/new")
+        expect(page).to have_content("Error: Name can't be blank, Street address can't be blank, City can't be blank, State can't be blank, Zip code can't be blank, Zip code is not a number, Zip code is the wrong length (should be 5 characters), Description can't be blank")
+      end
+    end
   end
-
 end
