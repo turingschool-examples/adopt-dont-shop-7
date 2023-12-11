db0722ec1adc326751f9f75b2ae21fa9f69e15e3
Yain Porter <yainporter@gmail.com>
2023-12-11 14:07:28 -0700

    Add validation and association tests to ApplicationPet

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,7 @@
 class ApplicationPet < ApplicationRecord
+  validates :application_id, presence: true
+  validates :pet_id, presence: true
+
   belongs_to :application
   belongs_to :pet
 
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Application, type: :model do
+RSpec.describe ApplicationPet, type: :model do
   before(:each) do
     @application_1 = Application.create(name: "John", street_address: "1234 ABC Lane", city: "Turing", state: "Backend", zipcode: "54321", description: "I love animals")
 
@@ -11,6 +11,15 @@ RSpec.describe Application, type: :model do
 
     @application_pet_1 = ApplicationPet.create(application_id: @application_1.id, pet_id: @dog.id)
     @application_pet_2 = ApplicationPet.create(application_id: @application_1.id, pet_id: @cat.id)
+  end
+
+  describe "associations" do
+    it {should belong_to :application}
+    it {should belong_to :pet}
+  end
+  describe "validations" do
+    it {should validate_presence_of :application_id}
+    it {should validate_presence_of :pet_id}
   end
 
   describe "#set_application_approved" do
