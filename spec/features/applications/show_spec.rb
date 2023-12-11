588caaf854c1f6059fb2fab6abc1f0d46aa465b9
Nathan Lambertson <lambertones@gmail.com>
2023-12-10 19:24:20 -0600

    Finish US 8 and 9 with tests written for them

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -102,6 +102,7 @@ RSpec.describe "applications show page", type: :feature do
     expect(page).to have_content("Pending")
     expect(page).to have_content(pet2.name)
     expect(page).to_not have_content("Add a Pet to this Application")
+    expect(page).to_not have_content("Search Pets")
   end
 
   it "has a submit application button that is not visible with no pets added to application" do
@@ -115,6 +116,57 @@ RSpec.describe "applications show page", type: :feature do
     expect(current_path).to eq("/applications/#{application1.id}")
     expect(page).to have_content("Add a Pet to this Application")
     expect(page).to_not have_button("Submit Application")
-    save_and_open_page
+  end
+
+  it "can search for pets with partial matches" do
+  application1 = Application.create!(name: "Fred Flintstone", address: "123 Main St, city: New York, state: NY, zip: 70117", description: "Worked with dinosaurs", status: "In Progress")
+    shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    pet1 = shelter.pets.create!(name: "garfield", breed: "shorthair", adoptable: true, age: 1)
+    pet2 = shelter.pets.create!(name: "fido", breed: "mutt", adoptable: true, age: 2)
+
+    visit "/applications/#{application1.id}"
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(application1.name)
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to have_field("Search")
+
+    fill_in "Search", with: "gar"
+    click_button("Search Pets")
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(pet1.name)
+
+    fill_in "Search", with: "fi"
+    click_button("Search Pets")
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(pet2.name)
+  end
+
+  it "can search for pets, case insensitive" do
+    application1 = Application.create!(name: "Fred Flintstone", address: "123 Main St, city: New York, state: NY, zip: 70117", description: "Worked with dinosaurs", status: "In Progress")
+    shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    pet1 = shelter.pets.create!(name: "garfield", breed: "shorthair", adoptable: true, age: 1)
+    pet2 = shelter.pets.create!(name: "fido", breed: "mutt", adoptable: true, age: 2)
+
+    visit "/applications/#{application1.id}"
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(application1.name)
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to have_field("Search")
+
+    fill_in "Search", with: "GArfIeld"
+    click_button("Search Pets")
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(pet1.name)
+
+    fill_in "Search", with: "fidO"
+    click_button("Search Pets")
+
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content(pet2.name)
   end
 end
