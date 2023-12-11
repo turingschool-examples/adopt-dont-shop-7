1a8027da471708b35ad14420646a4110edacfc79
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-11 12:35:57 -0700

    Add route to  create pet application

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,6 @@ Rails.application.routes.draw do
   get "/applications/:id", to: "applications#show"
   post "/applications", to: "applications#create"
   patch "/applications/:id", to: "applications#update"
+
+  post "/pet_applications", to: "pet_applications#create"
 end
