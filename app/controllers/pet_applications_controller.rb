83d9e230c4220f646133835c83c92eb862868b65
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-12 15:58:37 -0700

    Change application to instance variable to be able to call app status in view

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -12,9 +12,9 @@ class PetApplicationsController < ApplicationController
   end
 
   def update 
-    application = Application.find(params[:app_id])
+    @application = Application.find(params[:app_id])
     pet = Pet.find(params[:pet_id])
-    pet_application = application.get_pet_app(pet.id)
+    pet_application = @application.get_pet_app(pet.id)
 
     pet_application.update_pet_status(params[:commit])
 
@@ -31,8 +31,15 @@ class PetApplicationsController < ApplicationController
     # else
     #   flash[:alert] = "There was a problem and #{pet.name}'s adoption has not yet been approved. Please try again later."
     # end
-    
-    redirect_to "/admin/applications/#{application.id}"
+    # if application.pet_applications.all? { |pet_app| pet_app.status == "Approved" }
+    if @application.all_pets_apps_appr?
+      @application.update!(status: "Approved")
+    elsif !@application.all_pets_apps_appr?
+      @application.update!(status: "Rejected")
+    end
+    # end 
+
+    redirect_to "/admin/applications/#{@application.id}"
   end
 
 end
