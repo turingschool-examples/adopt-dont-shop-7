e72f1ab620349af77599c0aa0ed56051549c8967
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-11 17:28:26 -0700

    Add update method to pet applications controller to let admin users approve a pet on an application

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -11,5 +11,18 @@ class PetApplicationsController < ApplicationController
     redirect_to "/applications/#{application.id}"
   end
 
+  def update 
+    application = Application.find(params[:app_id])
+    pet = Pet.find(params[:pet_id])
+    pet_application = PetApplication.find_pet_app(pet.id, application.id)
+
+    if pet_application.update!(status: 2)
+      flash[:notice] = "The request to adopt #{Pet.find(params[:pet_id]).name} has been approved!!"
+    else
+      flash[:alert] = "There was a problem and #{Pet.find(params[:pet_id]).name}'s adoption has not yet been approved. Please try again later."
+    end
+    
+    redirect_to "/admin/applications/#{application.id}"
+  end
 
 end
