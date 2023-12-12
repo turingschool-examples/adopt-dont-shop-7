c1161b4515dc20d517cddb4dc203f037377a8c2b
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-11 17:43:18 -0700

    Put the test to make sure a button exists next to every pet with a 'Pending' status in the specific app being processed into a 'describe' block umbrella that will include the various program executions depending on whether a pet is approved or rejecte on the current app.

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -42,23 +42,38 @@ RSpec.describe "Admin Application Show Page" do
     @pet_4 = Pet.create!(adoptable: true, age: 1, breed: "beagle", name: "Toaster", shelter_id: @shelter_3.id)
     @pet_5 = Pet.create!(adoptable: true, age: 4, breed: "pitbull", name: "Hoser", shelter_id: @shelter_3.id)
 
-    @pet_app_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @app_1.id, status: 3)
+    @pet_app_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @app_1.id, status: 1)
     @pet_app_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: @app_1.id, status: 1)
     @pet_app_3 = PetApplication.create!(pet_id: @pet_4.id, application_id: @app_1.id, status: 1)
-    @pet_app_4 = PetApplication.create!(pet_id: @pet_1.id, application_id: @app_2.id, status: 2)
+    @pet_app_4 = PetApplication.create!(pet_id: @pet_1.id, application_id: @app_2.id, status: 1)
     @pet_app_5 = PetApplication.create!(pet_id: @pet_5.id, application_id: @app_2.id, status: 1)
-    @pet_app_6 = PetApplication.create!(pet_id: @pet_3.id, application_id: @app_3.id, status: 2)
+    @pet_app_6 = PetApplication.create!(pet_id: @pet_3.id, application_id: @app_3.id, status: 1)
     @pet_app_7 = PetApplication.create!(pet_id: @pet_5.id, application_id: @app_3.id, status: 1)
   end
 
-  it "there is a button next to every pet on the application" do 
-    visit "/admin/applications/#{@app_1.id}"
-    @app_1.pets.each do |pet| 
-      save_and_open_page
-      within "#approve-#{pet.id}" do 
-        expect(page).to have_content(pet.name)
-        expect(page).to have_button("Approve")
+  describe "approving a pet on an application" do 
+    it "there is an 'Approve' button next to every pet on the application" do 
+      visit "/admin/applications/#{@app_1.id}"
+      @app_1.pets.each do |pet| 
+        within "#approve-#{pet.id}" do 
+          expect(page).to have_content(pet.name)
+          expect(page).to have_button("Approve")
+        end
+      end
+    end
+
+    it "when the 'Approve' button is clicked it removes the button and indicates pet application was 'approved'" do 
+      visit "/admin/applications/#{@app_1.id}" 
+    
+      @app_1.pets.each do |pet| 
+        within "#approve-#{pet.id}" do 
+          click_button("Approve")
+          expect(current_path).to eq("/admin/applications/#{@app_1.id}")
+          expect(page).to_not have_button("Approve")
+          expect(page).to have_content("Approved")
+        end
       end
     end
   end
+
 end 
