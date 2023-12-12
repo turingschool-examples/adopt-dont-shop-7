03dd6958b7f162fddf1355ffac84abb1866fce8c
Martin Chavez <martinchavez1203@gmail.com>
2023-12-11 20:33:56 -0700

    Added function to show action in application controller using .search

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationsController < ApplicationController
     def show
         @application = Application.find(params[:id])
+        
+        @pets = Pet.search(params[:search])
+        
     end
     
     def new 
