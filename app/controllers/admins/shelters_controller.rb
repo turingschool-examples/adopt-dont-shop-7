974d1e81e28258722a721b82018ffbe36f795f9e
Yain Porter <144638203+yainporter@users.noreply.github.com>
2023-12-12 13:29:15 -0700

    Fix admins/shelters/index.html.erb so that #self.pending_applications is not called

--- app/controllers/admins/shelters_controller.rb
+++ app/controllers/admins/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admins::SheltersController < ApplicationController
   def index
     @shelters = Shelter.order_by_reverse_alphabetically
+    @shelters_pending = Shelter.pending_applications
   end
 end
