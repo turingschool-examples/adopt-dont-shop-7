78eefe05a648fa1ac6cc89f2b3ee8e68e6f4c010
Logan Finnegan <logantfinnegan@protonmail.com>
2023-12-11 19:59:33 -0600

    finish story 5

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,6 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   post "/applications", to: "applications#create" 
   get "/applications/:id", to: "applications#show"
+
+  post "/pet_apps", to: "pet_apps#create"
 end
