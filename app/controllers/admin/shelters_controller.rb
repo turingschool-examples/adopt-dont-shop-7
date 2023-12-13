526ba7935eb92f9215c45bb90e23ab7aad13aaf0
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-12 22:55:22 -0700

    Add sheltername and shelter address from SQL queries into variables

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,4 +2,15 @@ class Admin::SheltersController < ApplicationController
   def index 
     @shelters = Shelter.all
   end
-end
+
+  def show
+    shelter_id = params[:id]
+    @shelter_name = Shelter.find_by_sql("SELECT name FROM shelters WHERE id = #{shelter_id}").first&.name
+    @shelter_address = Shelter.find_by_sql("SELECT city FROM shelters WHERE id = #{shelter_id}").first&.city
+    
+    #wondering which method to put these ^^ in for admin.
+    #I tried making an admin/shelter model
+    #I also tried putting it in the shelter model
+    #neither worked, might not be setting up the new model correctly
+  end
+end 
