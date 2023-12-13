bea6593ea9c8087eb47cfd2aee4d3613add2f381
Meg Roth <megroth2@gmail.com>
2023-12-13 16:02:44 -0700

    Add ! to the end of every .create and .update

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 RSpec.describe ApplicationPet, type: :model do
   before(:each) do
-    @application_1 = Application.create(name: "John", street_address: "1234 ABC Lane", city: "Turing", state: "Backend", zipcode: "54321", description: "I love animals")
+    @application_1 = Application.create!(name: "John", street_address: "1234 ABC Lane", city: "Turing", state: "Backend", zipcode: "54321", description: "I love animals")
 
-    @shelter = Shelter.create(foster_program: true, name: "Turing", city: "Backend", rank: 3)
+    @shelter = Shelter.create!(foster_program: true, name: "Turing", city: "Backend", rank: 3)
 
-    @dog = @shelter.pets.create(adoptable: true, age: 4, breed: "Golden Retriever", name: "Dog")
-    @cat = @shelter.pets.create(adoptable: true, age: 1, breed: "Tabby", name: "cat")
+    @dog = @shelter.pets.create!(adoptable: true, age: 4, breed: "Golden Retriever", name: "Dog")
+    @cat = @shelter.pets.create!(adoptable: true, age: 1, breed: "Tabby", name: "cat")
 
-    @application_pet_1 = ApplicationPet.create(application_id: @application_1.id, pet_id: @dog.id)
-    @application_pet_2 = ApplicationPet.create(application_id: @application_1.id, pet_id: @cat.id)
+    @application_pet_1 = ApplicationPet.create!(application_id: @application_1.id, pet_id: @dog.id)
+    @application_pet_2 = ApplicationPet.create!(application_id: @application_1.id, pet_id: @cat.id)
   end
 
   describe "associations" do
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -12,20 +12,20 @@ RSpec.describe Pet, type: :model do
   end
 
   before(:each) do
-    @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
-    @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
-    @pet_3 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 3, adoptable: false)
+    @shelter_1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    @pet_1 = @shelter_1.pets.create!(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
+    @pet_2 = @shelter_1.pets.create!(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+    @pet_3 = @shelter_1.pets.create!(name: "Ann", breed: "ragdoll", age: 3, adoptable: false)
 
-    @application_1 = Application.create(name: "John", street_address: "1234 ABC Lane", city: "Turing", state: "Backend", zipcode: "54321", description: "I love animals")
+    @application_1 = Application.create!(name: "John", street_address: "1234 ABC Lane", city: "Turing", state: "Backend", zipcode: "54321", description: "I love animals")
 
-    @shelter = Shelter.create(foster_program: true, name: "Turing", city: "Backend", rank: 3)
+    @shelter = Shelter.create!(foster_program: true, name: "Turing", city: "Backend", rank: 3)
 
-    @dog = @shelter.pets.create(adoptable: true, age: 4, breed: "Golden Retriever", name: "Dog")
-    @cat = @shelter.pets.create(adoptable: true, age: 1, breed: "Tabby", name: "cat")
+    @dog = @shelter.pets.create!(adoptable: true, age: 4, breed: "Golden Retriever", name: "Dog")
+    @cat = @shelter.pets.create!(adoptable: true, age: 1, breed: "Tabby", name: "cat")
 
-    @application_pet_1 = ApplicationPet.create(application_id: @application_1.id, pet_id: @dog.id)
-    @application_pet_2 = ApplicationPet.create(application_id: @application_1.id, pet_id: @cat.id)
+    @application_pet_1 = ApplicationPet.create!(application_id: @application_1.id, pet_id: @dog.id)
+    @application_pet_2 = ApplicationPet.create!(application_id: @application_1.id, pet_id: @cat.id)
   end
 
   describe "class methods" do
