996644ca7316261bb27b521b1034bfa196c3344d
Yain Porter <144638203+yainporter@users.noreply.github.com>
2023-12-13 15:45:08 -0700

    Delete tests for set_status and adjust #create

--- app/controllers/admins/applications_controller.rb
+++ app/controllers/admins/applications_controller.rb
@@ -2,13 +2,6 @@ class Admins::ApplicationsController < ApplicationController
 
   def show
     @application = Application.application(params[:id])
-    if params[:filter] == "approved"
-      # pet_application = ApplicationPet.find_application_pet(params[:id], params[:pet_id])
-      # pet_application.set_application_approved
-      Pet.find(params[:pet_id]).set_adoptable_false #is this chaining too much? I figured it's the same thing as: pet = Pet.find(params[:pet_id] -> pet.set_adoptable_false)
-    elsif params[:filter].present?
-      # pet_application = ApplicationPet.find_application_pet(params[:id], params[:pet_id]).set_application_denied
-    end
   end
 
 
