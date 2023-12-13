0cee724ea341abbe5cab24d48628c77503635579
Victor Sanchez <vsanantone@gmail.com>
2023-12-13 12:41:32 -0600

    simple cov modification and test alignment

--- spec/features/applications/search_pets_spec.rb
+++ spec/features/applications/search_pets_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe "SearchPets" do
   it "User searches for a pet by name" do
 
-    victor_app = Application.create!(applicant_name: "Victor Antonio Sanchez", address: "97 Jaffa Road", city: "Jerusalem", state: "Israel", zip_code: "9103401", description: "Because I'm rich! :)")
+    victor_app = Application.create!(applicant_name: "Victor Antonio Sanchez", 
+                                     address: "97 Jaffa Road", 
+                                     city: "Jerusalem", 
+                                     state: "Israel", 
+                                     zip_code: "9103401", 
+                                     description: "Because I'm rich! :)")
     shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = shelter_1.pets.create!(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
     pet_2 = shelter_1.pets.create!(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
