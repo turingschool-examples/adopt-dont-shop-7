dd89dfefd640421c3205c8add3a4cff2e998451b
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-13 10:56:05 -0500

    Alter admin_applications controller to modify application_pet instead of pet

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -5,15 +5,23 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def update
+    # we want it to be approved/rejected at application_pets, not change the pets status? 
     @application = Application.find(params[:id])
-    @pets = @application.pets
+    # @pets = @application.pets
+    @pet = Pet.find(params[:id])
+    @pet_id = params[:pet_id]
+    @application_pet = @application.application_pets.find_by(pet: pet)
     if params[:commit].starts_with?('Approve')
-      approved_pet = @pets.find_by(adoptable: false)
-      approved_pet.update!(adoptable: true, rejected: false) if approved_pet
+      # creating the rejected attribute here, is like a different pet object/only in the admin, but I think it still carries back to the pet. we need the link to stay broken
+      # approved_pet = @pets.find_by(adoptable: false)
+      # approved_pet.update!(adoptable: true, rejected: false) if approved_pet
+      application_pet.update!(approved: true)
       redirect_to "/admin/applications/#{@application.id}"
     elsif params[:commit].starts_with?('Reject')
-      rejected_pet = @pets.find_by(adoptable: false)
-      rejected_pet.update!(adoptable: false, rejected: true) if rejected_pet
+      # no need to make a different pet object now
+      # rejected_pet = @pets.find_by(adoptable: false)
+      # rejected_pet.update!(adoptable: false, rejected: true) if rejected_pet
+      application_pet.update!(approved: true)
       redirect_to "/admin/applications/#{@application.id}"
     end
 
