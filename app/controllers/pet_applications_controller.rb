554412d7f25304482cc3d9608ec511b0082e5877
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-13 17:03:45 -0700

    Finish Project w/some extensions

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -16,33 +16,15 @@ class PetApplicationsController < ApplicationController
     pet = Pet.find(params[:pet_id])
     pet_application = @application.get_pet_app(pet.id)
 
-    
-    # TODO: can we test call to update!
-    #    i.e. if pet_application.update!({pet_id: pet.id, application_id: @application.id})
-    #           "Your update was successful and the change has been processed..."
-    #         else  
-    #           "There was a problem and we could not make the update you requested..."
-    #         end
-
     pet_application.update_pet_status(params[:commit])
 
-    if pet_application.status == "Approved"
-      flash[:notice] = "The request to adopt #{pet.name} has been approved!!"
-    elsif pet_application.status == "Rejected"
-      flash[:notice] = "The request to adopt #{pet.name} has been rejected :("
-    else
-      flash[:alert] = "There was a problem and #{pet.name}'s adoption has not yet been approved. Please try again later."
-    end
-    
-    if @application.get_app_status == "Approved"
+    if @application.all_pets_apps_appr?
       @application.update!(status: "Approved")
-    elsif @application.get_app_status == "Rejected" 
+      flash[:notice] = "The request to adopt #{pet.name} has been approved!!"
+    elsif !@application.all_pets_apps_appr?
       @application.update!(status: "Rejected")
-    else
-      @application.update!(status: "Pending")
+      flash[:notice] = "The request to adopt #{pet.name} has been rejected :("
     end
-
     redirect_to "/admin/applications/#{@application.id}"
   end
-
 end
