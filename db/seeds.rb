55b213193f8d46f538740ad4c4f0e6073df7bd93
Dylan Perry <dylanbperry@gmail.com>
2023-12-13 23:42:17 -0800

    US-14: Approved/Rejected Pets on one Application do not affect other Applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 shelter_1 = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
 shelter_2 = Shelter.create(name: "New Shelter", city: "Irvine CA", foster_program: false, rank: 9)
-applicant_who_has_pets = Application.create(name: "Shaggy", street_address: "123 Mystery Lane", city: "Irvine", state: "CA", zip_code: "91010", description: "Because",status:"Pending")
-applicant_who_has_no_pets = Application.create(name: "Daphne", street_address: "4 North Street", city: "Boulder", state: "CO", zip_code: "80209", description: "I want a pet")
-pet = applicant_who_has_pets.pets.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter_1.id)
-cat = Pet.create(name: "Catdog", age: 4, breed: "Calico", adoptable: true, shelter_id: shelter_1.id)
+shaggy = Application.create(name: "Shaggy", street_address: "123 Mystery Lane", city: "Irvine", state: "CA", zip_code: "91010", description: "Because")
+daphne = Application.create(name: "Daphne", street_address: "4 North Street", city: "Boulder", state: "CO", zip_code: "80209", description: "I want a pet")
+scooby = shaggy.pets.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter_1.id)
+catdog = Pet.create(name: "Catdog", age: 4, breed: "Calico", adoptable: true, shelter_id: shelter_1.id)
 
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -9,20 +9,34 @@ RSpec.describe ApplicationPet, type: :model do
     before(:each) do
       @shelter_1 = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
       @shelter_2 = Shelter.create(name: "New Shelter", city: "Irvine CA", foster_program: false, rank: 9)
-      @applicant_who_has_pets = Application.create(name: "Shaggy", street_address: "123 Mystery Lane", city: "Irvine", state: "CA", zip_code: "91010", description: "Because",status:"Pending")
-      @pet = @applicant_who_has_pets.pets.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: @shelter_1.id)
-
+      @shaggy = Application.create(name: "Shaggy", street_address: "123 Mystery Lane", city: "Irvine", state: "CA", zip_code: "91010", description: "Because", status:"Pending")
+      @daphne = Application.create(name: "Daphne", street_address: "4 North Street", city: "Boulder", state: "CO", zip_code: "80209", description: "I want a pet", status:"Pending")
+      @scooby = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: @shelter_1.id)
+      @shaggy.add_pet_to_application(@scooby.id)
+      @daphne.add_pet_to_application(@scooby.id)
     end
 
     describe "#change_application_pet_status" do 
       it "changes an application pet's status" do 
-        application_pet = ApplicationPet.where({pet_id: @pet.id},{application_id: @applicant_who_has_pets.id}).first
+        application_pet = ApplicationPet.where({pet_id: @scooby.id, application_id: @shaggy.id}).first
 
         expect(application_pet.status).to eq(nil)
 
         application_pet.change_application_pet_status("fart")
 
         expect(application_pet.status).to eq("fart")
+      end
+
+      it "does not change a different application pet's status when there are two or more applications for the same pet" do
+        application_pet_1 = ApplicationPet.where({pet_id: @scooby.id, application_id: @shaggy.id}).first
+        application_pet_2 = ApplicationPet.where({pet_id: @scooby.id, application_id: @daphne.id}).first
+
+        expect(application_pet_1.status).to eq(nil)
+        expect(application_pet_2.status).to eq(nil)
+
+        application_pet_1.change_application_pet_status("fart")
+        expect(application_pet_1.status).to eq("fart")
+        expect(application_pet_2.status).to_not eq("fart")
       end
     end   
 end
