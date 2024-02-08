2d7fea27396dd39211fb0027f1c96f1d3769844f
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-02-08 17:31:22 -0500

    Added {Model}.destroy_all to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+{Model}.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# postgres://adopt_dont_shop_7_lance_nico_user:sMbZKr7Vi7NJGGq3D9vccKk9b0fco6ss@dpg-cn2l2a21hbls739d55k0-a/adopt_dont_shop_7_lance_nico
