0198efcc8e3fb81f03e7ef4444c8aac71d42da15
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-08 17:43:21 -0500

    fix:typo, feat: adoption-app-pet model

--- spec/models/adoption_application_pets_spec.rb
+++ spec/models/adoption_application_pets_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe AdoptionApplicationPets, type: :model do
+RSpec.describe AdoptionApplicationPet, type: :model do
    describe 'relationships' do
       it {should belong_to :adoption_application}
-      it {should belong_to :pets}
+      it {should belong_to :pet}
    end
 end
