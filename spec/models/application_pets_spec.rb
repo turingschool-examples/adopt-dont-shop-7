e49864df95611202caecd05e32823bdce6b66a89
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-08 22:09:47 -0700

    Test: Create tests for Models

---
+++ spec/models/application_pets_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationPet, type: :model do
+
+ describe 'relationships' do
+  it { should belong_to :pet }
+  it { should belong_to :application }
+ end
+
+ describe 'instance methods' do
+ end
+end
