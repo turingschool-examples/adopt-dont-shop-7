bcf1fe46fe1e843633df99fd58e216aaf0a13929
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-02-09 15:48:50 -0500

    Give applicant another column of description and put description in it, and test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Application show" do
     before(:each) do
-        @applicant_1 = Applicant.create!(name: "Lance", street_address: "123 sesame street", city: "Brooklyn", state: "New York", zip_code: "10012")
-        @applicant_2 = Applicant.create!(name: "Nico", street_address: "125 sesame way", city: "Miami", state: "Florida", zip_code: "63016") 
-        #lance database @applicant_2 = id => 1
+        @applicant_1 = Applicant.create!(name: "Lance", street_address: "123 sesame street", city: "Brooklyn", state: "New York", zip_code: "10012", description: "I like dogs")
+        @applicant_2 = Applicant.create!(name: "Nico", street_address: "125 sesame way", city: "Miami", state: "Florida", zip_code: "63016", description: "I have an unlimited supply of poop bags") 
+    
     end
 
     it 'shows the name of the applicant and attributes' do
@@ -15,17 +15,13 @@ RSpec.describe "Application show" do
         expect(page).to have_content(@applicant_1.city)
         expect(page).to have_content(@applicant_1.state)
         expect(page).to have_content(@applicant_1.zip_code)
+        expect(page).to have_content(@applicant_1.desciption)
         expect(page).to_not have_content(@applicant_2.name)
         expect(page).to_not have_content(@applicant_2.street_address)
         expect(page).to_not have_content(@applicant_2.city)
         expect(page).to_not have_content(@applicant_2.state)
         expect(page).to_not have_content(@applicant_2.zip_code)
-    end
+        expect(page).to_not have_content(@applicant_2.desciption)
 
-    it 'shows desciption for why this is a good home' do 
-        visit "/applicant/#{@applicant_1.id}"
-
-        expect(page).to have_content(@applicant_1.desciption)
-        #expect(page).to_not have_content(@applicant_2.desciption)
     end
 end
