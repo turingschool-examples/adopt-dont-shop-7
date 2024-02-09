33cc2e45b214387b5735ae1e7810a2ffe90df185
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-09 17:19:31 -0500

    feat: tests: add pet to application

--- app/models/adoption_application.rb
+++ app/models/adoption_application.rb
@@ -2,4 +2,9 @@ class AdoptionApplication < ApplicationRecord
    has_many :adoption_application_pets
    has_many :pets, through: :adoption_application_pets
    validates :name, :street_address, :city, :state, :zip_code, :description, :status, presence: true
+
+   def add_pet_to_app(pet_id)
+      pet = Pet.find(pet_id)
+      self.pets << pet
+   end
 end
--- spec/models/adoption_application_spec.rb
+++ spec/models/adoption_application_spec.rb
@@ -15,4 +15,15 @@ RSpec.describe AdoptionApplication, type: :model do
       it { should validate_presence_of(:description) }
       it { should validate_presence_of(:status) }
    end
+
+   describe '#add_pet_to_app' do
+      it 'adds the pet to the adoption application' do
+         shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+         pet_1 = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+         pet_2 = Pet.create(name: "Scrappy", age: 1, breed: "Pit Mix", adoptable: true, shelter_id: shelter.id)
+         application = pet_2.adoption_applications.create!(name: "Mel", street_address: "23 Main St", city: "Denver", state: "CO", zip_code: 80303, description: "I have a fenced backyard and love dogs", status: "Pending")
+   
+         expect(application.add_pet_to_app(pet_1.id).first).to eq(pet_1)
+      end
+   end
 end
