8057011f4b45e4296ec4ef34f665f8256676f4c4
Hoa Dam <hoangocdam@gmail.com>
2024-02-09 08:27:10 -0800

    feat:flash message and fix:add instance var in new method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationsController < ApplicationController
+  def new
+    @application = Application.new
+  end
+
+  def create
+    application = Application.new(application_params)
+
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      flash[:notice] = application.errors.full_messages.join(', ')
+      redirect_to "/applications/new"
+    end
+  end
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:name, :street_address, :city, :state, :zip_code, :description)
   end
 end
