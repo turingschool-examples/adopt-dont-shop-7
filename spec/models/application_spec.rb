b0ae6d0db17ed2d367677ccf158309fe858a068e
Hoa Dam <hoangocdam@gmail.com>
2024-02-09 15:25:37 -0800

    add validation test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pets).through(:application_pets) }
     it { should have_many(:application_pets) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_numericality_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_numericality_of(:description) }
+  end
 end
