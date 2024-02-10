eb1e589ef58cc1e3fd86fdbeb4943f90308b034d
lance butler <lancej2004@gmail.com>
2024-02-09 17:48:07 -0700

    Catching back up to where we were addsapplications tests and passing factors

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,25 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Application show" do
     before(:each) do
-        @shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-        @pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: 1)
-        @pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: 1)
-        @applicant_1 = Applicant.create!(name: "Lance", street_address: "123 sesame street", city: "Brooklyn", state: "New York", zip_code: "10012", description: "I like dogs", pet_names: "Lucille Bald", status: "In Progress")
-        @applicant_2 = Applicant.create!(name: "Nico", street_address: "125 sesame way", city: "Miami", state: "Florida", zip_code: "63016", description: "I have an unlimited supply of poop bags", pet_names: "Lobster", status: "In Progress") 
+    #     @shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    #     @pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: 1)
+    #     @pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: 1)
+        @application_1 = Application.create!(name: "Lance", street_address: "123 sesame street", city: "Brooklyn", state: "New York", zip_code: "10012", description: "I like dogs", status: "In Progress")
+        @application_2 = Application.create!(name: "Nico", street_address: "125 sesame way", city: "Miami", state: "Florida", zip_code: "63016", description: "I have an unlimited supply of poop bags", status: "In Progress") 
     
     end
 
     it 'shows the name of the applicant and attributes' do
-        visit "/applicant/#{@applicant_1.id}"
+        visit "/applications/#{@application_1.id}"
 
-        expect(page).to have_content(@applicant_1.name)
-        expect(page).to have_content(@applicant_1.street_address)
-        expect(page).to have_content(@applicant_1.city)
-        expect(page).to have_content(@applicant_1.state)
-        expect(page).to have_content(@applicant_1.zip_code)
-        expect(page).to have_content(@applicant_1.description)
-        expect(page).to have_link("Pet Names")
-        expect(page).to have_content(@applicant_1.status)
+        expect(page).to have_content(@application_1.name)
+        expect(page).to have_content(@application_1.street_address)
+        expect(page).to have_content(@application_1.city)
+        expect(page).to have_content(@application_1.state)
+        expect(page).to have_content(@application_1.zip_code)
+        expect(page).to have_content(@application_1.description)
+        expect(page).to have_content(@application_1.status)
 
        
     end
