63ad8e8fd9a131fa7fa8559a50a5b7bb0c1aed36
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-10 14:32:27 -0700

    feat, user story 11 complete

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -3,7 +3,7 @@ class Admin::SheltersController < ApplicationController
     def index
         @admin_shelters = Shelter.all.reverse_alphabetical_order
 
-        @shelter_with_pending_apps = Shelter.all.sort_by_pending
+        @shelters_with_pending_apps = Shelter.all.sort_by_pending
     end
 end
 
--- spec/models/adoption_application_spec.rb
+++ spec/models/adoption_application_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AdoptionApplication, type: :model do
    describe 'relationships' do
-      # # US 11
-      # it { should belong_to(:shelter) }
-
       it { should have_many :adoption_application_pets }
       it { should have_many(:pets).through(:adoption_application_pets) }
    end
