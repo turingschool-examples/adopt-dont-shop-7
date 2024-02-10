b5ac6142dc7606bade4c3612063f01f1ca8496a5
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-10 11:51:13 -0700

    test, tests pass for partial and case insensitive

--- spec/features/adoption_application/show_spec.rb
+++ spec/features/adoption_application/show_spec.rb
@@ -42,29 +42,55 @@ RSpec.describe 'Adoption Application Show page', type: :feature do
       # When I click one of these buttons
       # Then I am taken back to the application show page
       # And I see the Pet I want to adopt listed on this application
-      it 'has a section to add a Pet to the application' do
-         shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
-         pet_1 = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-         pet_2 = Pet.create(name: "Scrappy", age: 1, breed: "Pit Mix", adoptable: true, shelter_id: shelter.id)
-         application = pet_2.adoption_applications.create!(name: "Mel", street_address: "23 Main St", city: "Denver", state: "CO", zip_code: 80303, description: "I have a fenced backyard and love dogs", status: "Pending")
+   it 'has a section to add a Pet to the application' do
+      shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+      pet_1 = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+      pet_2 = Pet.create(name: "Scrappy", age: 1, breed: "Pit Mix", adoptable: true, shelter_id: shelter.id)
+      application = pet_2.adoption_applications.create!(name: "Mel", street_address: "23 Main St", city: "Denver", state: "CO", zip_code: 80303, description: "I have a fenced backyard and love dogs", status: "Pending")
+
+      visit "/applications/#{application.id}"
+
+      expect(page).to have_content("Add Pets to this Application")
+      expect(page).to have_content("Search Pet by Name")
+
+      fill_in "search", with: "Sc"
+      click_on "Search"
+
+      expect(page).to have_content(pet_1.name)
+      expect(page).to have_content(pet_1.breed)
+      expect(page).to have_content(pet_1.age)
+      expect(page).to have_content(pet_1.adoptable)
+      expect(page).to have_content(pet_2.name)
+      expect(page).to have_content(pet_2.breed)
+      expect(page).to have_content(pet_2.age)
+      expect(page).to have_content(pet_2.adoptable)
+
+      expect(page).to have_button("Adopt this Pet")
+   end
    
-         visit "/applications/#{application.id}"
-   
-         expect(page).to have_content("Add Pets to this Application")
-         expect(page).to have_content("Search Pet by Name")
+   it 'is case insensitive and has partial matches' do
+      shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+      pet_1 = Pet.create(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+      pet_2 = Pet.create(name: "Scrappy", age: 1, breed: "Pit Mix", adoptable: true, shelter_id: shelter.id)
+      application = pet_2.adoption_applications.create!(name: "Mel", street_address: "23 Main St", city: "Denver", state: "CO", zip_code: 80303, description: "I have a fenced backyard and love dogs", status: "Pending")
 
-         fill_in "search", with: "Sc"
-         click_on "Search"
+      visit "/applications/#{application.id}"
 
-         expect(page).to have_content(pet_1.name)
-         expect(page).to have_content(pet_1.breed)
-         expect(page).to have_content(pet_1.age)
-         expect(page).to have_content(pet_1.adoptable)
-         expect(page).to have_content(pet_2.name)
-         expect(page).to have_content(pet_2.breed)
-         expect(page).to have_content(pet_2.age)
-         expect(page).to have_content(pet_2.adoptable)
+      expect(page).to have_content("Add Pets to this Application")
+      expect(page).to have_content("Search Pet by Name")
 
-         expect(page).to have_button("Adopt this Pet")
-      end
+      fill_in "search", with: "sc"
+      click_on "Search"
+
+      expect(page).to have_content(pet_1.name)
+      expect(page).to have_content(pet_1.breed)
+      expect(page).to have_content(pet_1.age)
+      expect(page).to have_content(pet_1.adoptable)
+      expect(page).to have_content(pet_2.name)
+      expect(page).to have_content(pet_2.breed)
+      expect(page).to have_content(pet_2.age)
+      expect(page).to have_content(pet_2.adoptable)
+
+      expect(page).to have_button("Adopt this Pet")
+   end
 end
