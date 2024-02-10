1a22d7ac04bce88e2ad2c716eff087a9a712741b
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-10 13:44:58 -0700

    test,tests written for US 11

--- app/models/adoption_application.rb
+++ app/models/adoption_application.rb
@@ -1,6 +1,8 @@
 class AdoptionApplication < ApplicationRecord
    has_many :adoption_application_pets
    has_many :pets, through: :adoption_application_pets
+   belongs_to :shelter
+   
    validates :name, :street_address, :city, :state, :zip_code, :description, :status, presence: true
 
    def add_pet_to_app(pet_id)
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -4,6 +4,7 @@ class Shelter < ApplicationRecord
   validates :city, presence: true
 
   has_many :pets, dependent: :destroy
+  has_many :adoption_applications, through: :pets
 
   def self.order_by_recently_created
     order(created_at: :desc)
@@ -33,7 +34,7 @@ class Shelter < ApplicationRecord
   end
 
   # admin
-  
+
   def self.reverse_alphabetical_order
     @reverse_alpha = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC;")
   end
