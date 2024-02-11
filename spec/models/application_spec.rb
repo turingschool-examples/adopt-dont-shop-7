daf3b0ac4e0e18c2ce22871b3ad2060dac61f873
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-10 20:39:54 -0600

    fix: validation tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe Application, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:street_address) }
-    it { should validate_numericality_of(:city) }
+    it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip_code) }
-    it { should validate_numericality_of(:description) }
+    it { should  validate_presence_of(:description) }
   end
 end
