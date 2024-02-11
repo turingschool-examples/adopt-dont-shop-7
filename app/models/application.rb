7ed33aa40e8f454ef36b5a9c53e67313df3badff
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-11 01:25:31 -0600

    feat: zip code must be a number

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,7 +3,7 @@ class Application < ApplicationRecord
   validates :street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, numericality: true
   validates :description, presence: true
 
   enum status: {
