60241cdc7ffd9307a4393a26aa14feb36d232f70
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-11 02:12:50 -0600

    refactor: change 'before each' to 'lets'

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -12,33 +12,31 @@ RSpec.describe Shelter, type: :model do
     it { should validate_numericality_of(:rank) }
   end
 
-  before(:each) do
-    @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+  let!(:shelter_1) {Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)}
+  let!(:shelter_2) {Shelter.create!(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)}
+  let!(:shelter_3) {Shelter.create!(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)}
 
-    @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)
-    @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
-    @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
-    @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
-  end
+  let!(:pet_1) {shelter_1.pets.create!(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)}
+  let!(:pet_2) {shelter_1.pets.create!(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)}
+  let!(:pet_3) {shelter_3.pets.create!(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)}
+  let!(:pet_4) {shelter_1.pets.create!(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)}
 
   describe "class methods" do
     describe "#search" do
       it "returns partial matches" do
-        expect(Shelter.search("Fancy")).to eq([@shelter_3])
+        expect(Shelter.search("Fancy")).to eq([shelter_3])
       end
     end
 
     describe "#order_by_recently_created" do
       it "returns shelters with the most recently created first" do
-        expect(Shelter.order_by_recently_created).to eq([@shelter_3, @shelter_2, @shelter_1])
+        expect(Shelter.order_by_recently_created).to eq([shelter_3, shelter_2, shelter_1])
       end
     end
 
     describe "#order_by_number_of_pets" do
       it "orders the shelters by number of pets they have, descending" do
-        expect(Shelter.order_by_number_of_pets).to eq([@shelter_1, @shelter_3, @shelter_2])
+        expect(Shelter.order_by_number_of_pets).to eq([shelter_1, shelter_3, shelter_2])
       end
     end
   end
@@ -46,25 +44,25 @@ RSpec.describe Shelter, type: :model do
   describe "instance methods" do
     describe ".adoptable_pets" do
       it "only returns pets that are adoptable" do
-        expect(@shelter_1.adoptable_pets).to eq([@pet_2, @pet_4])
+        expect(shelter_1.adoptable_pets).to eq([pet_2, pet_4])
       end
     end
 
     describe ".alphabetical_pets" do
       it "returns pets associated with the given shelter in alphabetical name order" do
-        expect(@shelter_1.alphabetical_pets).to eq([@pet_4, @pet_2])
+        expect(shelter_1.alphabetical_pets).to eq([pet_4, pet_2])
       end
     end
 
     describe ".shelter_pets_filtered_by_age" do
       it "filters the shelter pets based on given params" do
-        expect(@shelter_1.shelter_pets_filtered_by_age(5)).to eq([@pet_4])
+        expect(shelter_1.shelter_pets_filtered_by_age(5)).to eq([pet_4])
       end
     end
 
     describe ".pet_count" do
       it "returns the number of pets at the given shelter" do
-        expect(@shelter_1.pet_count).to eq(3)
+        expect(shelter_1.pet_count).to eq(3)
       end
     end
   end
