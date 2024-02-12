d678590162e908e4cad7915f3175874dce43881f
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-12 14:23:05 -0700

    Feat: Shelters with Pending Applications for user story 11

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -21,6 +21,15 @@ RSpec.describe Shelter, type: :model do
     @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
     @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
     @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
+  
+    @application_1 = Application.create!(name: "Selena", street_address: "123 Street", city: "City", state: "State", zip_code: "8888", adopting_reason: "Love for cats, no job", status:"Pending")
+    @application_2 = Application.create!(name: "Laura", street_address: "58 Street", city: "City", state: "State", zip_code: "5555", adopting_reason: "Need company", status:"Rejected")
+    @application_3 = Application.create!(name: "Isaac", street_address: "456 Street", city: "City", state: "State", zip_code: "8878", adopting_reason: "Lots of love to give", status:"Accepted")
+    @application_4 = Application.create!(name: "Mark", street_address: "889 Folsom Ave", city: "Denver", state: "CO", zip_code: "80024", adopting_reason: "Lonely", status:"Pending")
+   
+    @application_pets_1 = ApplicationPet.create!(pet_id: @pet_3.id, application_id: @application_1.id)
+    @application_pets_2 = ApplicationPet.create!(pet_id: @pet_2.id, application_id: @application_4.id)
+    @application_pets_3 = ApplicationPet.create!(pet_id: @pet_2.id, application_id: @application_3.id)
   end
 
   describe "class methods" do
@@ -43,9 +52,15 @@ RSpec.describe Shelter, type: :model do
     end
 
     describe "#order_by_reverse_alpha" do
-    it "orders the shelters by number of pets they have, descending" do
-      expect(Shelter.order_by_reverse_alpha).to eq([@shelter_2, @shelter_3, @shelter_1])
+      it "orders the shelters by number of pets they have, descending" do
+        expect(Shelter.order_by_reverse_alpha).to eq([@shelter_2, @shelter_3, @shelter_1])
+      end
     end
+
+    describe "#find_pending_applications" do
+      it "resturns the shelters with pending applications" do
+        expect(Shelter.find_pending_applications).to eq([@shelter_1, @shelter_3])
+      end
     end
   end
 
