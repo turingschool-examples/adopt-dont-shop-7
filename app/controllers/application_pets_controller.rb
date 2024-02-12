2c01aaa38324521070e54ea28e51ca754735e2bb
Barry Allahyar <barrya@gmail.com>
2024-02-12 13:04:08 -0600

    Create and update application_pets_controller.rb

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationPetsController < ApplicationController
 
-  
+  def create
+    pet = Pet.find(params[:pet_id])
+    application = Application.find(params[:id])
+    ApplicationPet.create(application_id: application.id, pet_id: pet.id)
+
+    redirect_to "/applications/#{application.id}"
+  end
+
 end
+
