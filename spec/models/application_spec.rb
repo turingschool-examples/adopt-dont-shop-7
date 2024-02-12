d7205a6094b7fc0d3946dd13a6852e3883f7f914
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-02-12 15:48:04 -0700

    Test: add and pass test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -7,23 +7,24 @@ RSpec.describe Application, type: :model do
  end
 
  describe 'class methods' do
-  it "returns a correct number of applications" do
+    it "returns a correct number of applications" do
 
-    expect(Application.num_of_applications).to eq(0)
+      expect(Application.num_of_applications).to eq(0)
 
-    @application_1 = Application.create!(name: "Selena", street_address: "123 Street", city: "City", state: "State", zip_code: "8888", adopting_reason: "Love for cats, no job", status:"Pending")
-    @application_2 = Application.create!(name: "Laura", street_address: "58 Street", city: "City", state: "State", zip_code: "5555", adopting_reason: "Need company", status:"Rejected")
-    @application_3 = Application.create!(name: "Isaac", street_address: "456 Street", city: "City", state: "State", zip_code: "78", adopting_reason: "Lots of love to give", status:"Accepted")
+      @application_1 = Application.create!(name: "Selena", street_address: "123 Street", city: "City", state: "State", zip_code: "8888", adopting_reason: "Love for cats, no job", status:"Pending")
+      @application_2 = Application.create!(name: "Laura", street_address: "58 Street", city: "City", state: "State", zip_code: "5555", adopting_reason: "Need company", status:"Rejected")
+      @application_3 = Application.create!(name: "Isaac", street_address: "456 Street", city: "City", state: "State", zip_code: "78", adopting_reason: "Lots of love to give", status:"Accepted")
 
-    expect(Application.num_of_applications).to eq(3)
-  end
+      expect(Application.num_of_applications).to eq(3)
+    end
  end
 
- describe 'instance methods' do
-  it "returns a populated address" do
-    @application_1 = Application.create!(name: "Selena", street_address: "123 Street", city: "City", state: "State", zip_code: "8888", adopting_reason: "Love for cats, no job", status:"Pending")
-    
-    expect(@application_1.populate_address).to eq ("123 Street, City, State, 8888")
+  describe 'instance methods' do
+    it "returns a populated address" do
+      @application_1 = Application.create!(name: "Selena", street_address: "123 Street", city: "City", state: "State", zip_code: "8888", adopting_reason: "Love for cats, no job", status:"Pending")
+      
+      expect(@application_1.populate_address).to eq ("123 Street, City, State, 8888")
+    end
   end
- end
+
 end
