4c14692537256c1ce8db25d73b919318000906c7
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-14 10:40:06 -0500

    model testing

--- app/controllers/admin/adoption_applications_controller.rb
+++ app/controllers/admin/adoption_applications_controller.rb
@@ -19,15 +19,14 @@ class Admin::AdoptionApplicationsController < ApplicationController
         # We want to change the application status rather than if the pet is adoptable, user story 14 requires pet to be still visible
         if params[:approved_pet].present?
             pet_id = params[:approved_pet]
-            adoption_app_pet = AdoptionApplicationPet.where({pet_id: pet_id,adoption_application_id: @adoption_app.id}).first
-            adoption_app_pet.change_app_pet_status("Approved")
+            # adoption_app_pet = AdoptionApplicationPet.where({pet_id: pet_id,adoption_application_id: @adoption_app.id}).first
+            AdoptionApplicationPet.change_status_to_approved(@adoption_app.id, pet_id)
         end
 
         # Reject the application if the pet is rejected. That way we can display different messages in the show page if the pet is approved or rejected
         if params[:rejected_pet].present?
             pet_id = params[:rejected_pet]
-            adoption_app_pet = AdoptionApplicationPet.where({pet_id: pet_id,adoption_application_id: @adoption_app.id}).first
-            adoption_app_pet.change_app_pet_status("Rejected")
+            AdoptionApplicationPet.change_status_to_rejected(@adoption_app.id, pet_id)
         end
     end    
 end
