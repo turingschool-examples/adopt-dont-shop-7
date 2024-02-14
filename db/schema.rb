7364a876c435fbdb220a1bf22f6327aee125df1a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-13 20:05:01 -0700

    Fix: restore column state from applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_025936) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_030340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_025936) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state"
   end
 
   create_table "pets", force: :cascade do |t|
