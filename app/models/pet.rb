cb70a930e4808b2d535485e38e611ba82934c7e8
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-02-14 20:25:48 -0700

    feat: Add scope to Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,13 +5,15 @@ class Pet < ApplicationRecord
   has_many :application_pets, dependent: :destroy
   has_many :applications, through: :application_pets, dependent: :destroy, source: :application
 
+  scope :adoptable, -> { where(adoptable: true) }
+
   def shelter_name
     shelter.name
   end
 
-  def self.adoptable
-    where(adoptable: true)
-  end
+  # def self.adoptable
+  #   where(adoptable: true)
+  # end
 
   def self.check_pet_applications
     Pet.joins(:application_pets).pluck(:application_approved)
